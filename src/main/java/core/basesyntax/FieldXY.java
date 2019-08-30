b6632355f49ee2e1095edde686739222ed352ea6
Vlad <wald.pavliuk@icloud.com>
2019-08-30 23:47:25 +0300

    test

---
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class FieldXY {
+
+    public void moveRobot(Robot robot, int toX, int toY) {
+        //your code
+    }
+}
+
---
+++ src/main/java/core/basesyntax/Robot.java
@@ -0,0 +1,75 @@
+package core.basesyntax;
+
+public class Robot {
+
+    private Direction direction;
+    private int coordinateX;
+    private int coordinateY;
+
+    public Robot(Direction direction, int coordinateX, int coordinateY) {
+        this.direction = direction;
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getX() {
+        return coordinateX;
+    }
+
+    public int getY() {
+        return coordinateY;
+    }
+
+    public void turnLeft() {
+        switch (direction) {
+            case UP:
+                direction = Direction.LEFT;
+                break;
+            case LEFT:
+                direction = Direction.DOWN;
+                break;
+            case DOWN:
+                direction = Direction.RIGHT;
+                break;
+            case RIGHT:
+                direction = Direction.UP;
+        }
+    }
+
+    public void turnRight() {
+        switch (direction) {
+            case UP:
+                direction = Direction.RIGHT;
+                break;
+            case LEFT:
+                direction = Direction.UP;
+                break;
+            case DOWN:
+                direction = Direction.LEFT;
+                break;
+            case RIGHT:
+                direction = Direction.DOWN;
+        }
+    }
+
+    public void stepForward() {
+        switch (direction) {
+            case UP:
+                coordinateY++;
+                break;
+            case LEFT:
+                coordinateX--;
+                break;
+            case DOWN:
+                coordinateY--;
+                break;
+            case RIGHT:
+                coordinateX++;
+        }
+    }
+}
+
