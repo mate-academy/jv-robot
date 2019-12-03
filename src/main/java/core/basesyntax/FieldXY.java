7ce155307c15ef800b04f3fabb641af679a997ec
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-03 19:42:31 +0200

    added moveRobot method

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -13,7 +13,105 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+
+        long distanceToX = Math.abs(robot.getX() - (long) toX);
+        long distanceToY = Math.abs(robot.getY() - (long) toY);
+
+        if (robot.getX() < toX) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    break;
+                default:
+                    System.out.println("Error");
+                    break;
+            }
+
+            for (long i = 0; i < distanceToX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getX() > toX) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    System.out.println("Error");
+                    break;
+            }
+
+            for (long i = 0; i < distanceToX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() < toY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                default:
+                    System.out.println("Error");
+                    break;
+            }
+
+            for (long i = 0; i < distanceToY; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() > toY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                default:
+                    System.out.println("Error!");
+                    break;
+            }
+
+            for (long i = 0; i < distanceToY; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
 
