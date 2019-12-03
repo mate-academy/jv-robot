c2a8b37738380de9f97fae5dcdf09cdf0fa218c7
Vadim Chaika <proxik777@gmail.com>
2019-12-04 01:42:47 +0200

    added a solution for robot homework

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -14,6 +14,74 @@ public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
         //your code
+        int startX = robot.getX();
+        int startY = robot.getY();
+        while (startX != toX && startY != toY) {
+            if (robot.getDirection() == Direction.UP) {
+                if (toY - startY > 0) {
+                    robot.stepForward();
+                    startY = robot.getY();
+                }
+                if (startX - toX > 0) {
+                    robot.turnLeft();
+                }
+                if (toX - startX > 0) {
+                    robot.turnRight();
+                }
+                if (startY - toY > 0) {
+                    robot.turnRight();
+                    robot.turnRight();
+                }
+            }
+            if (robot.getDirection() == Direction.RIGHT) {
+                if (toX - startX > 0) {
+                    robot.stepForward();
+                    startX = robot.getX();
+                }
+                if (toY - startY > 0) {
+                    robot.turnLeft();
+                }
+                if (startY - toY > 0) {
+                    robot.turnRight();
+                }
+                if (startX - toX > 0) {
+                    robot.turnRight();
+                    robot.turnRight();
+                }
+            }
+            if (robot.getDirection() == Direction.DOWN) {
+                if (startY - toY > 0) {
+                    robot.stepForward();
+                    startY = robot.getY();
+                }
+                if (toX - startX > 0) {
+                    robot.turnLeft();
+                }
+                if (startX - toX > 0) {
+                    robot.turnRight();
+                }
+                if (toY - startY > 0) {
+                    robot.turnRight();
+                    robot.turnRight();
+                }
+            }
+            if (robot.getDirection() == Direction.LEFT) {
+                if (startX - toX > 0) {
+                    robot.stepForward();
+                    startX = robot.getY();
+                }
+                if (startY - toY > 0) {
+                    robot.turnLeft();
+                }
+                if (toY - startY > 0) {
+                    robot.turnRight();
+                }
+                if (toX - startX > 0) {
+                    robot.turnRight();
+                    robot.turnRight();
+                }
+            }
+        }
     }
 }
 
