57afaaa9c06a53f35e38b07222919ae596ee0a66
Dmitrii <zinchukdimaa@gmail.com>
2019-12-04 11:56:34 +0200

    optimized, separeted big blocks to smaller methods

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,36 +13,31 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (robot.getX() < toX) {
+        if (robot.getX() <= toX) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-            while (robot.getX() < toX) {
-                robot.stepForward();
-            }
-        } else if (robot.getX() > toX) {
+
+        } else if (robot.getX() >= toX) {
             while (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             }
-            while (robot.getX() > toX) {
-                robot.stepForward();
-            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
         }
 
         if (robot.getY() < toY) {
             while (robot.getDirection() != Direction.UP) {
                 robot.turnLeft();
             }
-            while (robot.getY() < toY) {
-                robot.stepForward();
-            }
         } else if (robot.getY() > toY) {
             while (robot.getDirection() != Direction.DOWN) {
                 robot.turnRight();
             }
-            while (robot.getY() >= toY) {
-                robot.stepForward();
-            }
+        }
+        while (robot.getY() >= toY) {
+            robot.stepForward();
         }
     }
 }
