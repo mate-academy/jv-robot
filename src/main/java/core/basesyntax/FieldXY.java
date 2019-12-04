036a2d80ae68ab4701799a4ff899a9927d1d01db
Serhii Yevtushok <ggazila@gmail.com>
2019-12-04 14:57:48 +0200

    made requirement changes

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -12,29 +12,29 @@ package core.basesyntax;
  */
 public class FieldXY {
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (robot.getX() < toX) {
+        if (robot.getX() <= toX) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-        } else if (robot.getX() > toX) {
+        } else {
             while (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             }
         }
-        for (int i = Math.abs(robot.getX()); i < Math.abs(toX); i++) {
+        while (robot.getX() != toX) {
             robot.stepForward();
         }
 
-        if (robot.getY() < toY) {
-            while (robot.getDirection() != Direction.UP) {
+        if (robot.getY() >= toY) {
+            while (robot.getDirection() != Direction.DOWN) {
                 robot.turnRight();
             }
-        } else if (robot.getY() > toY) {
-            while (robot.getDirection() != Direction.DOWN) {
+        } else {
+            while (robot.getDirection() != Direction.UP) {
                 robot.turnLeft();
             }
         }
-        for (int i = Math.abs(robot.getY()); i < Math.abs(toY); i++) {
+        while (robot.getY() != toY) {
             robot.stepForward();
         }
     }
