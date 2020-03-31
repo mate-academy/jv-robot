1fbbd0f50fe87f0dec3941af8cb450b243a3b8c6
nastya <cctvporsh@gmail.com>
2020-03-31 17:25:48 +0300

    Added solution to the 4th task

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -11,9 +11,25 @@ package core.basesyntax;
  * Реализуйте метод moveRobot()</p>
  */
 public class FieldXY {
-
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        Direction directionX = toX > 0
+                ? Direction.RIGHT : Direction.LEFT;
+
+        while (robot.getDirection() != directionX) {
+            robot.turnRight();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        Direction directionY = toY > 0
+                ? Direction.UP : Direction.DOWN;
+
+        while (robot.getDirection() != directionY) {
+            robot.turnRight();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
 
