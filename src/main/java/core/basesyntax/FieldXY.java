7c6d1c6a31be11d55507a07f8dd48f362cbe53aa
Popovych Yurii <popovychyb@gmail.com>
2020-03-31 17:05:59 +0300

    fixed code duplication

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -17,32 +17,26 @@ public class FieldXY {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnLeft();
             }
-            while (robot.getX() < toX) {
-                robot.stepForward();
-            }
         } else {
             while (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             }
-            while (robot.getX() > toX) {
-                robot.stepForward();
-            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
         }
 
         if (robot.getY() < toY) {
             while (robot.getDirection() != Direction.UP) {
                 robot.turnLeft();
             }
-            while (robot.getY() < toY) {
-                robot.stepForward();
-            }
         } else {
             while (robot.getDirection() != Direction.DOWN) {
                 robot.turnLeft();
             }
-            while (robot.getY() > toY) {
-                robot.stepForward();
-            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
         }
     }
 }
