fa5abbf786f355de8f08dccc072438d3c7a0f3b2
Serhii Maksymov <serhio.maksymov@gmail.com>
2020-03-31 21:30:38 +0300

    added solution hw-2.4

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,7 +13,58 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        if (toX > robot.getX()) {
+            switch (robot.getDirection()) {
+                case UP: robot.turnRight();
+                break;
+                case LEFT: robot.turnRight();
+                           robot.turnRight();
+                break;
+                case DOWN: robot.turnLeft();
+                break;
+                default: break;
+            }
+        } else if (toX < robot.getX()) {
+            switch (robot.getDirection()) {
+                case RIGHT: robot.turnLeft();
+                            robot.turnLeft();
+                    break;
+                case DOWN: robot.turnRight();
+                    break;
+                case UP: robot.turnLeft();
+                    break;
+                default: break;
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if (toY < robot.getY()) {
+            switch (robot.getDirection()) {
+                case LEFT: robot.turnRight();
+                break;
+                case DOWN: robot.turnRight();
+                           robot.turnRight();
+                break;
+                case RIGHT: robot.turnLeft();
+                break;
+                default: break;
+            }
+        } else if (toY > robot.getY()) {
+            switch (robot.getDirection()) {
+                case RIGHT: robot.turnRight();
+                break;
+                case LEFT: robot.turnLeft();
+                break;
+                case UP: robot.turnLeft();
+                         robot.turnLeft();
+                break;
+                default: break;
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
 
