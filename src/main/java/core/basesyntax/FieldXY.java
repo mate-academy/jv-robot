ce019937de37fadf6552f9733dd448817b894669
Andrew <tolstuy1996@gmail.com>
2020-03-31 15:17:36 +0300

    fixed checkstile

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -18,7 +18,7 @@ public class FieldXY {
             robot.turnRight();
         } else if (robot.getDirection() == Direction.RIGHT) {
             robot.turnLeft();
-        } else if(robot.getDirection() == Direction.DOWN) {
+        } else if (robot.getDirection() == Direction.DOWN) {
             robot.turnLeft();
             robot.turnLeft();
         }
@@ -26,12 +26,12 @@ public class FieldXY {
         //move x
         if (robot.getX() < toX) {
             robot.turnRight();
-            while(robot.getX() != toX) {
+            while (robot.getX() != toX) {
                 robot.stepForward();
             }
         } else if (robot.getX() > toX) {
             robot.turnLeft();
-            while(robot.getX() != toX) {
+            while (robot.getX() != toX) {
                 robot.stepForward();
             }
         }
@@ -40,19 +40,19 @@ public class FieldXY {
         if (robot.getY() < toY) {
             if (robot.getDirection() == Direction.LEFT) {
                 robot.turnRight();
-            } else if(robot.getDirection() == Direction.RIGHT){
+            } else if (robot.getDirection() == Direction.RIGHT) {
                 robot.turnLeft();
             }
-            while(robot.getY() != toY) {
+            while (robot.getY() != toY) {
                 robot.stepForward();
             }
-        }else if (toY < robot.getY()) {
+        } else if (toY < robot.getY()) {
             if (robot.getDirection() == Direction.LEFT) {
                 robot.turnLeft();
             } else {
                 robot.turnRight();
             }
-            while(robot.getY() != toY) {
+            while (robot.getY() != toY) {
                 robot.stepForward();
             }
         }
