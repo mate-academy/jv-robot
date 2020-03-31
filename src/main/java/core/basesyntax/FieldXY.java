609d6111c33b1374fc104ef6169e25fe4889856f
woododoCode <woododoCode@users.noreply.github.com>
2020-03-31 16:39:11 +0300

    Solution HW24

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -14,6 +14,34 @@ public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
         //your code
+        if (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnRight();
+                }
+            } else {
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+            }
+            do {
+                robot.stepForward();
+            } while (robot.getX() != toX);
+        }
+        if (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnLeft();
+                }
+            } else {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnLeft();
+                }
+            }
+            do {
+                robot.stepForward();
+            } while (robot.getY() != toY);
+        }
     }
 }
 
