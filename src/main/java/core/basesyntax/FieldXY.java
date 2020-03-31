26ba0761cd21f95126d19f257286cf94d7c7d4e3
Максим Матуляк <maksymmatuliak@gmail.com>
2020-03-31 17:25:12 +0300

    now robot round to the right all the time

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -14,40 +14,24 @@ public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
         if (robot.getX() > toX) {
-            if (robot.getDirection() == Direction.UP) {
-                robot.turnLeft();
-            } else {
-                while (robot.getDirection() != Direction.LEFT) {
-                    robot.turnRight();
-                }
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
             }
         } else {
-            if (robot.getDirection() == Direction.UP) {
+            while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
-            } else {
-                while (robot.getDirection() != Direction.RIGHT) {
-                    robot.turnLeft();
-                }
             }
         }
         while (robot.getX() != toX) {
             robot.stepForward();
         }
         if (robot.getY() > toY) {
-            if (robot.getDirection() == Direction.LEFT) {
-                robot.turnLeft();
-            } else {
-                while (robot.getDirection() != Direction.DOWN) {
-                    robot.turnRight();
-                }
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
             }
         } else {
-            if (robot.getDirection() == Direction.LEFT) {
+            while (robot.getDirection() != Direction.UP) {
                 robot.turnRight();
-            } else {
-                while (robot.getDirection() != Direction.UP) {
-                    robot.turnLeft();
-                }
             }
         }
         while (robot.getY() != toY) {
