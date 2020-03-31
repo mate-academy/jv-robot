628389c4b7908abebb55675ee6e3cfdebbc69503
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-03-31 16:02:59 +0300

    add solution for robot

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -12,8 +12,31 @@ package core.basesyntax;
  */
 public class FieldXY {
 
-    public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        if (robot.getX() < toX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if(robot.getY() < toY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
 
