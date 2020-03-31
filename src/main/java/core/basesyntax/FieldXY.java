23bc975a7dc7460449921bd6f32541bd3e3c64fd
Andrew <tolstuy1996@gmail.com>
2020-03-31 15:16:17 +0300

    solution done

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,7 +13,49 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        //set  position up ;
+        if (robot.getDirection() == Direction.LEFT) {
+            robot.turnRight();
+        } else if (robot.getDirection() == Direction.RIGHT) {
+            robot.turnLeft();
+        } else if(robot.getDirection() == Direction.DOWN) {
+            robot.turnLeft();
+            robot.turnLeft();
+        }
+
+        //move x
+        if (robot.getX() < toX) {
+            robot.turnRight();
+            while(robot.getX() != toX) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() > toX) {
+            robot.turnLeft();
+            while(robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+
+        //move y
+        if (robot.getY() < toY) {
+            if (robot.getDirection() == Direction.LEFT) {
+                robot.turnRight();
+            } else if(robot.getDirection() == Direction.RIGHT){
+                robot.turnLeft();
+            }
+            while(robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }else if (toY < robot.getY()) {
+            if (robot.getDirection() == Direction.LEFT) {
+                robot.turnLeft();
+            } else {
+                robot.turnRight();
+            }
+            while(robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
 
