a074c52aed73fc293bdc78360135f4c5f0de2dd6
nastya <cctvporsh@gmail.com>
2020-03-31 22:13:02 +0300

    Created several functions to move the robot

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -12,18 +12,32 @@ package core.basesyntax;
  */
 public class FieldXY {
     public void moveRobot(Robot robot, int toX, int toY) {
-        Direction directionX = toX > 0
-                ? Direction.RIGHT : Direction.LEFT;
+        Direction directionX = chooseDirectionForX(toX);
+        moveToX(robot, toX, directionX);
+        Direction directionY = chooseDirectionForY(toY);
+        moveToY(robot, toY, directionY);
+    }
 
+    public static Direction chooseDirectionForY(int toY) {
+        return toY > 0
+                ? Direction.UP : Direction.DOWN;
+    }
+
+    public static Direction chooseDirectionForX(int toX) {
+        return toX > 0
+                ? Direction.RIGHT : Direction.LEFT;
+    }
+
+    public static void moveToX(Robot robot, int toX, Direction directionX) {
         while (robot.getDirection() != directionX) {
             robot.turnRight();
         }
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-        Direction directionY = toY > 0
-                ? Direction.UP : Direction.DOWN;
+    }
 
+    public static void moveToY(Robot robot, int toY, Direction directionY) {
         while (robot.getDirection() != directionY) {
             robot.turnRight();
         }
