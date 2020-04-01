5f62add759ffc233406a681567600c6d2d001a5b
Vbortnyk <viacheslav.bortnyk@gmail.com>
2020-04-01 18:02:23 +0300

    changed logic

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -14,36 +14,29 @@ public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
 
-        chooseDirection(robot, toX, toY);
-        while (robot.getY() != toY) {
-            robot.stepForward();
-        }
-        chooseDirection(robot, toX, toY);
-        while (robot.getX() != toX) {
-            robot.stepForward();
-        }
-    }
-
-    public void chooseDirection(Robot robot, int toX, int toY) {
         if (robot.getY() < toY) {
             while (robot.getDirection() != Direction.UP) {
                 robot.turnLeft();
             }
-        }
-        if (robot.getY() > toY) {
+        } else {
             while (robot.getDirection() != Direction.DOWN) {
                 robot.turnLeft();
             }
         }
-        if (robot.getY() == toY && robot.getX() < toX) {
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+        if (robot.getX() < toX) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-        }
-        if (robot.getY() == toY && robot.getX() > toX) {
+        } else {
             while (robot.getDirection() != Direction.LEFT) {
-                robot.turnLeft();
+                robot.turnRight();
             }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
         }
     }
 }
--- src/main/java/core/basesyntax/Robot.java
+++ src/main/java/core/basesyntax/Robot.java
@@ -79,7 +79,5 @@ public class Robot {
             default:
                 break;
         }
-
     }
 }
-
