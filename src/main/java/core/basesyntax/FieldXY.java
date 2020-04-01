da861c05ff518549bb3531fceecb548f35000a8a
ana-dav <opa.super.op@gmail.com>
2020-04-01 12:54:12 +0300

    separated method

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -3,29 +3,19 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (toX > robot.getX()) {
-            while (robot.getDirection() != Direction.RIGHT) {
-                robot.turnRight();
-            }
-        } else {
-            while (robot.getDirection() != Direction.LEFT) {
-                robot.turnLeft();
-            }
-        }
+        newDirection(robot, robot.getX() < toX ? Direction.RIGHT : Direction.LEFT);
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-        if (toY > robot.getY()) {
-            while (robot.getDirection() != Direction.UP) {
-                robot.turnRight();
-            }
-        } else {
-            while (robot.getDirection() != Direction.DOWN) {
-                robot.turnRight();
-            }
-        }
+        newDirection(robot, robot.getY() < toY ? Direction.UP : Direction.DOWN);
         while (robot.getY() != toY) {
             robot.stepForward();
+        }
+    }
+
+    public void newDirection(Robot robot, Direction direction) {
+        while (!robot.getDirection().equals(direction)) {
+            robot.turnLeft();
         }
     }
 }
