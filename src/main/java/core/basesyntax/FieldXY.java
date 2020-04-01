43059b60f29fb9bdb06f91e3409479c7b8bb078d
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-01 10:08:51 +0300

    removed comments.

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -16,96 +16,24 @@ public class FieldXY {
         while (robot.getX() != toX || robot.getY() != toY) {
             if (robot.getX() != toX) {
                 if (robot.getX() > toX) {
-                    //turnToLeft(robot);
                     turnRobot(robot, Direction.LEFT);
                     multiplySteps(robot, robot.getX() - toX);
                 } else {
-                    //turnToRight(robot);
                     turnRobot(robot, Direction.RIGHT);
                     multiplySteps(robot, toX - robot.getX());
                 }
             }
             if (robot.getY() != toY) {
                 if (robot.getY() > toY) {
-                    //turnToDown(robot);
                     turnRobot(robot, Direction.DOWN);
                     multiplySteps(robot, robot.getY() - toY);
                 } else {
-                    //turnToUp(robot);
                     turnRobot(robot, Direction.UP);
                     multiplySteps(robot, toY - robot.getY());
                 }
             }
         }
     }
-
-    /*public void turnToUp(Robot robot) {
-        switch (robot.getDirection()) {
-            case LEFT:
-                robot.turnRight();
-                break;
-            case DOWN:
-                robot.turnRight();
-                robot.turnRight();
-                break;
-            case RIGHT:
-                robot.turnLeft();
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void turnToDown(Robot robot) {
-        switch (robot.getDirection()) {
-            case LEFT:
-                robot.turnLeft();
-                break;
-            case UP:
-                robot.turnRight();
-                robot.turnRight();
-                break;
-            case RIGHT:
-                robot.turnRight();
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void turnToLeft(Robot robot) {
-        switch (robot.getDirection()) {
-            case DOWN:
-                robot.turnRight();
-                break;
-            case RIGHT:
-                robot.turnRight();
-                robot.turnRight();
-                break;
-            case UP:
-                robot.turnLeft();
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void turnToRight(Robot robot) {
-        switch (robot.getDirection()) {
-            case LEFT:
-                robot.turnLeft();
-                robot.turnLeft();
-                break;
-            case UP:
-                robot.turnRight();
-                break;
-            case DOWN:
-                robot.turnLeft();
-                break;
-            default:
-                break;
-        }
-    }*/
 
     public void turnRobot(Robot robot, Direction direction) {
         while (robot.getDirection() != direction) {
