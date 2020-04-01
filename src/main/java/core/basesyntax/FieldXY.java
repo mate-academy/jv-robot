7ca154326595085c7e1767899d5e0ac7a523b5df
serg-ksv <serg.ksv1@gmail.com>
2020-04-01 12:53:01 +0300

    Combined methods

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,50 +13,28 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (robot.getX() != toX) {
-            setDirectionToX(robot, toX);
-            moveToX(robot, toX);
-        }
-        if (robot.getY() != toY) {
-            setDirectionToY(robot, toY);
-            moveToY(robot, toY);
-        }
-    }
-
-    public void setDirectionToX(Robot robot, int toX) {
         if (robot.getX() > toX) {
-            while (robot.getDirection() != Direction.LEFT) {
-                robot.turnRight();
-            }
+            setDirection(robot, Direction.LEFT);
         } else {
-            while (robot.getDirection() != Direction.RIGHT) {
-                robot.turnRight();
-            }
+            setDirection(robot, Direction.RIGHT);
         }
-    }
-
-    public void setDirectionToY(Robot robot, int toY) {
-        if (robot.getY() > toY) {
-            while (robot.getDirection() != Direction.DOWN) {
-                robot.turnLeft();
-            }
-        } else {
-            while (robot.getDirection() != Direction.UP) {
-                robot.turnLeft();
-            }
-        }
-    }
-
-    public void moveToX(Robot robot, int toX) {
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-    }
 
-    public void moveToY(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            setDirection(robot, Direction.DOWN);
+        } else {
+            setDirection(robot, Direction.UP);
+        }
         while (robot.getY() != toY) {
             robot.stepForward();
         }
     }
-}
 
+    public void setDirection(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnRight();
+        }
+    }
+}
