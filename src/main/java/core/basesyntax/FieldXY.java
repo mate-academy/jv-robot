2f66874d6a2a840312c083a3ac1ffb5cfd590967
Maksym Vakuliuk <vakuliukmaksym@mail.com>
2020-04-01 11:55:06 +0300

    logick changes

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,10 +13,15 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        while (robot.getX() != toX || robot.getY() != toY) {
-            goToEnd(robot, toX, toY);
-        }
+        Direction[] trueDirection = trueDirection(robot, toX, toY);
 
+        if (!robot.getDirection().equals(trueDirection[0])
+                || !robot.getDirection().equals(trueDirection[1])) {
+            turnToNeededDirection(robot, trueDirection);
+        }
+        goToEndOfDirection(robot, toX, toY);
+        turnToNeededDirection(robot, trueDirection);
+        goToEndOfDirection(robot, toX, toY);
     }
 
     private Direction[] trueDirection(Robot robot, int toX, int toY) {
@@ -24,32 +29,31 @@ public class FieldXY {
                                 (toY > robot.getY()) ? Direction.UP : Direction.DOWN};
     }
 
-    private void turnToNeededDirection(Robot robot, int toX, int toY) {
-        Direction[] neededDirection = trueDirection(robot, toX, toY);
+    private void turnToNeededDirection(Robot robot, Direction[] trueDirection) {
         switch (robot.getDirection()) {
             case UP:
-                if (neededDirection[0].equals(Direction.RIGHT)) {
+                if (trueDirection[0].equals(Direction.RIGHT)) {
                     robot.turnRight();
                 } else {
                     robot.turnLeft();
                 }
                 break;
             case DOWN:
-                if (neededDirection[0].equals(Direction.LEFT)) {
+                if (trueDirection[0].equals(Direction.LEFT)) {
                     robot.turnRight();
                 } else {
                     robot.turnLeft();
                 }
                 break;
             case LEFT:
-                if (neededDirection[1].equals(Direction.UP)) {
+                if (trueDirection[1].equals(Direction.UP)) {
                     robot.turnRight();
                 } else {
                     robot.turnLeft();
                 }
                 break;
             case RIGHT:
-                if (neededDirection[1].equals(Direction.DOWN)) {
+                if (trueDirection[1].equals(Direction.DOWN)) {
                     robot.turnRight();
                 } else {
                     robot.turnLeft();
@@ -60,19 +64,22 @@ public class FieldXY {
         }
     }
 
-    private void goToEnd(Robot robot, int toX, int toY) {
-        turnToNeededDirection(robot, toX, toY);
-        if (robot.getDirection().equals(Direction.LEFT)
-                || robot.getDirection().equals(Direction.RIGHT)) {
-            while (robot.getX() != toX) {
-                robot.stepForward();
-            }
-        } else {
-            while (robot.getY() != toY) {
-                robot.stepForward();
-            }
+    private void goToEndOfDirection(Robot robot, int toX, int toY) {
+        switch (robot.getDirection()) {
+            case LEFT: ;
+            case RIGHT:
+                while (robot.getX() != toX) {
+                    robot.stepForward();
+                }
+                break;
+            case DOWN:
+            case UP:
+                while (robot.getY() != toY) {
+                    robot.stepForward();
+                }
+                break;
+            default: break;
         }
-
     }
 
 }
