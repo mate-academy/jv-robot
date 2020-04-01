1e9e2ec9f7e76f5127d35464e1a356e85d91a41b
Polina Kulyk <polina.kulyk@protonmail.com>
2020-04-01 11:05:27 +0300

    grouped methods logically

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -11,81 +11,38 @@ package core.basesyntax;
  * Реализуйте метод moveRobot()</p>
  */
 public class FieldXY {
-    //test commit
     public void moveRobot(Robot robot, int toX, int toY) {
         moveX(robot, toX);
         moveY(robot, toY);
-    }
-
-    private void moveX(Robot robot, int toX) {
         while (robot.getX() != toX) {
-            switch (robot.getDirection()) {
-                case UP:
-                    if (robot.getX() > toX) {
-                        robot.turnLeft();
-                    } else {
-                        robot.turnRight();
-                    }
-                    break;
-                case DOWN:
-                    if (robot.getX() < toX) {
-                        robot.turnLeft();
-                    } else {
-                        robot.turnRight();
-                    }
-                    break;
-                case LEFT:
-                    if (robot.getX() < toX) {
-                        robot.turnLeft();
-                        robot.turnLeft();
-                    }
-                    break;
-                case RIGHT:
-                    if (robot.getX() > toX) {
-                        robot.turnRight();
-                        robot.turnRight();
-                    }
-                    break;
-                default:
-                    break;
-            }
+            robot.stepForward();
+        }
+        while (robot.getY() != toY) {
             robot.stepForward();
         }
     }
 
-    private void moveY(Robot robot, int toY) {
-        while (robot.getY() != toY) {
-            switch (robot.getDirection()) {
-                case UP:
-                    if (robot.getY() > toY) {
-                        robot.turnRight();
-                        robot.turnRight();
-                    }
-                    break;
-                case DOWN:
-                    if (robot.getY() < toY) {
-                        robot.turnLeft();
-                        robot.turnLeft();
-                    }
-                    break;
-                case LEFT:
-                    if (robot.getY() > toY) {
-                        robot.turnRight();
-                    } else {
-                        robot.turnLeft();
-                    }
-                    break;
-                case RIGHT:
-                    if (robot.getY() < toY) {
-                        robot.turnLeft();
-                    } else {
-                        robot.turnRight();
-                    }
-                    break;
-                default:
-                    break;
+    private void moveX(Robot robot, int toX) {
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
             }
-            robot.stepForward();
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+    }
+
+    private void moveY(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
         }
     }
 }
