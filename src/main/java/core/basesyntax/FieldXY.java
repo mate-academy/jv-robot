35633ae27904b02d83dfea4cbe91cd368a2bedf2
Filipp <filipp888@gmail.com>
2020-04-01 12:32:02 +0200

    Method has been renamed

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -14,17 +14,17 @@ public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
 
-        newDirection(robot, robot.getX() < toX ? Direction.RIGHT : Direction.LEFT);
+        settingNewDirection(robot, robot.getX() < toX ? Direction.RIGHT : Direction.LEFT);
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-        newDirection(robot, robot.getY() < toY ? Direction.UP : Direction.DOWN);
+        settingNewDirection(robot, robot.getY() < toY ? Direction.UP : Direction.DOWN);
         while (robot.getY() != toY) {
             robot.stepForward();
         }
     }
 
-    public void newDirection(Robot robot, Direction direction) {
+    public void settingNewDirection(Robot robot, Direction direction) {
         while (!robot.getDirection().equals(direction)) {
             robot.turnLeft();
         }
