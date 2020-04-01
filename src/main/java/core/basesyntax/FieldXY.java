c0bca54f5d34bc5744b1f863162b0bd55b3012a5
SpringWay <44027294+SpringWay@users.noreply.github.com>
2020-04-01 09:32:25 +0300

    added solution to hw-2.4

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -14,6 +14,31 @@ public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
         //your code
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnLeft();
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+
     }
 }
 
