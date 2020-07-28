dcbfdb15d74042212e596c27dff6a5a748240b31
ROMASHSX <andriiromash@gmail.com>
2020-07-28 13:42:11 +0300

    mvn is checked

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -5,8 +5,8 @@ import static core.basesyntax.Direction.RIGHT;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -17,7 +17,7 @@ public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
         if (robot.getX() >= toX) {
-            while (robot.getDirection() != LEFT){
+            while (robot.getDirection() != LEFT) {
                 robot.turnLeft();
             }
             while (robot.getX() != toX) {
@@ -32,7 +32,7 @@ public class FieldXY {
                 robot.stepForward();
             }
         } else if (robot.getX() < toX) {
-            while (robot.getDirection() != RIGHT){
+            while (robot.getDirection() != RIGHT) {
                 robot.turnRight();
             }
             while (robot.getX() != toX) {
