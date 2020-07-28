762046b7fc8a5cce2dd73fa7cede3eea9e8d7a45
FYarkiy <flx_jb@yahoo.com>
2020-07-28 16:56:37 +0300

    Not optimised Robot movements

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
+import static core.basesyntax.Direction.*;
+
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -13,7 +15,35 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        if (robot.getX() < toX) {
+            while (robot.getDirection() != RIGHT) {
+                robot.turnRight();
+            }
+        }
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != LEFT) {
+                robot.turnLeft();
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if (robot.getY() < toY) {
+            while (robot.getDirection() != UP) {
+                robot.turnRight();
+            }
+        }
+        if (robot.getY() > toY) {
+            while (robot.getDirection() != DOWN) {
+                robot.turnLeft();
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
+
+    //your code
 }
+
 
