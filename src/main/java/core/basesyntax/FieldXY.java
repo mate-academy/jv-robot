e27ed4f0c2d021eb9f792f3d715c9b7ba85ff62d
Dm_Grt <grytsishin@gmail.com>
2020-07-28 18:18:10 +0300

    Simplification of the solution

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -4,44 +4,27 @@ public class FieldXY {
     public void moveRobot(Robot robot, int toX, int toY) {
         while (toX > robot.getX()) {
             while (robot.getDirection() != Direction.RIGHT) {
-                if (robot.getDirection() == Direction.DOWN) {
-                    robot.turnLeft();
-                } else {
-                    robot.turnRight();
-                }
+                robot.turnRight();
             }
             robot.stepForward();
         }
         while (toX < robot.getX()) {
             while (robot.getDirection() != Direction.LEFT) {
-                if (robot.getDirection() == Direction.DOWN) {
-                    robot.turnRight();
-                } else {
-                    robot.turnLeft();
-                }
+                robot.turnLeft();
             }
             robot.stepForward();
         }
         while (toY > robot.getY()) {
             while (robot.getDirection() != Direction.UP) {
-                if (robot.getDirection() == Direction.RIGHT) {
-                    robot.turnLeft();
-                } else {
-                    robot.turnRight();
-                }
+                robot.turnRight();
             }
             robot.stepForward();
         }
         while (toY < robot.getY()) {
             while (robot.getDirection() != Direction.DOWN) {
-                if (robot.getDirection() == Direction.RIGHT) {
-                    robot.turnRight();
-                } else {
-                    robot.turnLeft();
-                }
+                robot.turnLeft();
             }
             robot.stepForward();
         }
     }
 }
-
