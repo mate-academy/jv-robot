ee4889add93e1565226dbe2a6bae93492bcd8d17
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-07-28 14:45:34 +0300

    added solution

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -13,7 +13,19 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        newDirection(robot, robot.getX() < toX ? Direction.RIGHT : Direction.LEFT);
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        newDirection(robot, robot.getY() < toY ? Direction.UP : Direction.DOWN);
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    public void newDirection(Robot robot, Direction direction) {
+        while (!robot.getDirection().equals(direction)) {
+            robot.turnLeft();
+        }
     }
 }
-
