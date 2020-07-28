a76b099e1d34e2628439c8c7a9f4ca4e3f02b579
Dm_Grt <grytsishin@gmail.com>
2020-07-28 13:31:00 +0300

    Added solution 2.3

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -1,19 +1,47 @@
 package core.basesyntax;
 
-/**
- * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
- * графики функций в школе?)
- * В начальный момент робот находится в некоторой позиции на поле.
- * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
- * Ваша задача — привести робота в заданную точку игрового поля.
- * Реализуйте метод moveRobot()</p>
- */
 public class FieldXY {
-
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        while (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                if (robot.getDirection() == Direction.DOWN) {
+                    robot.turnLeft();
+                } else {
+                    robot.turnRight();
+                }
+            }
+            robot.stepForward();
+        }
+        while (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                if (robot.getDirection() == Direction.DOWN) {
+                    robot.turnRight();
+                } else {
+                    robot.turnLeft();
+                }
+            }
+            robot.stepForward();
+        }
+        while (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.UP) {
+                if (robot.getDirection() == Direction.RIGHT) {
+                    robot.turnLeft();
+                } else {
+                    robot.turnRight();
+                }
+            }
+            robot.stepForward();
+        }
+        while (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                if (robot.getDirection() == Direction.RIGHT) {
+                    robot.turnRight();
+                } else {
+                    robot.turnLeft();
+                }
+            }
+            robot.stepForward();
+        }
     }
 }
 
