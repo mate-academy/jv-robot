2e97bc8ed743e4b845de205335adf217f9d55115
tarkaiev <tarkaievd@gmail.com>
2020-07-28 09:23:42 +0300

    added solution

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,7 +13,22 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+
+        Direction directionX = toX > robot.getX() ? Direction.RIGHT : Direction.LEFT;
+        Direction directionY = toY > robot.getY() ? Direction.UP : Direction.DOWN;
+
+        while (toX != robot.getX()) {
+            while (directionX != robot.getDirection()) {
+                robot.turnRight();
+            }
+            robot.stepForward();
+        }
+        while (toY != robot.getY()) {
+            while (directionY != robot.getDirection()) {
+                robot.turnRight();
+            }
+            robot.stepForward();
+        }
     }
 }
 
