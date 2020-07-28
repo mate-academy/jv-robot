fd5d20842c72290e719c502e01b4e8bf3ecce024
Oleksii Tromsa <tromsaalex@gmail.com>
2020-07-28 15:50:20 +0300

    Add solution

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -1,19 +1,38 @@
 package core.basesyntax;
 
-/**
- * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
- * графики функций в школе?)
- * В начальный момент робот находится в некоторой позиции на поле.
- * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
- * Ваша задача — привести робота в заданную точку игрового поля.
- * Реализуйте метод moveRobot()</p>
- */
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        Direction x;
+        Direction y;
+
+        if (robot.getX() < toX) {
+            x = Direction.RIGHT;
+        } else {
+            x = Direction.LEFT;
+        }
+
+        if (robot.getY() < toY) {
+            y = Direction.UP;
+        } else {
+            y = Direction.DOWN;
+        }
+
+        while (robot.getDirection() != x) {
+            robot.turnRight();
+        }
+
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        while (robot.getDirection() != y) {
+            robot.turnRight();
+        }
+
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
 
