8e9416f0c5c5be2c85a75416307050418d5177c6
tkhambir <tatiana.khambir@gmail.com>
2020-07-29 00:46:42 +0300

    homework 2.3 solution

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,7 +13,37 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        turnRobotToTarget(robot, robot.getX(), toX, Direction.RIGHT, Direction.LEFT);
+        moveRobotToTarget(robot,robot.getX(), toX);
+        turnRobotToTarget(robot, robot.getY(), toY, Direction.UP, Direction.DOWN);
+        moveRobotToTarget(robot,robot.getY(), toY);
+    }
+
+    public void turnRobotToTarget(Robot robot, int position, int target,
+                                  Direction direction1, Direction direction2) {
+        if (position < target) {
+            while (robot.getDirection() != direction1) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != direction2) {
+                robot.turnLeft();
+            }
+        }
+    }
+
+    public void moveRobotToTarget(Robot robot, int position, int target) {
+        while (position != target) {
+            robot.stepForward();
+            position = getPosition(robot);
+        }
+    }
+
+    public int getPosition(Robot robot) {
+        if (robot.getDirection() == Direction.LEFT || robot.getDirection() == Direction.RIGHT) {
+            return robot.getX();
+        }
+        return robot.getY();
     }
 }
 
