e9a369c8f74bce58188e7d00b959e58eafc0fd47
ROMASHSX <andriiromash@gmail.com>
2020-07-28 13:40:06 +0300

    task 3 is done

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -1,5 +1,8 @@
 package core.basesyntax;
 
+import static core.basesyntax.Direction.LEFT;
+import static core.basesyntax.Direction.RIGHT;
+
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
  * целочисленным координатами: X и Y. 
@@ -13,7 +16,37 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        if (robot.getX() >= toX) {
+            while (robot.getDirection() != LEFT){
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+            if (robot.getY() > toY) {
+                robot.turnRight();
+            } else {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() < toX) {
+            while (robot.getDirection() != RIGHT){
+                robot.turnRight();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+            if (robot.getY() > toY) {
+                robot.turnLeft();
+            } else {
+                robot.turnRight();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
 
