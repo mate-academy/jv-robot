826adc3994fc2ec6f6f7abc550b9a789e9ca2a2c
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-07-28 15:43:01 +0300

    task2-3

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -13,7 +13,197 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+        if (robot.getX() < toX && robot.getY() < toY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    break;
+            }
+
+            for (int i = robot.getY(); i < toY; i++) {
+                robot.stepForward();
+            }
+
+            robot.turnRight();
+
+            for (int i = robot.getX(); i < toX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getX() < toX && robot.getY() > toY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+            }
+
+            for (int i = robot.getY(); i > toY; i--) {
+                robot.stepForward();
+            }
+
+            robot.turnLeft();
+
+            for (int i = robot.getX(); i < toX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getX() > toX && robot.getY() > toY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+            }
+
+            for (int i = robot.getY(); i > toY; i--) {
+                robot.stepForward();
+            }
+
+            robot.turnRight();
+
+            for (int i = robot.getX(); i > toX; i--) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getX() > toX && robot.getY() < toY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    break;
+            }
+
+            for (int i = robot.getY(); i < toY; i++) {
+                robot.stepForward();
+            }
+
+            robot.turnLeft();
+
+            for (int i = robot.getX(); i > toX; i--) {
+                robot.stepForward();
+            }
+        }
+
+        if(robot.getX() == toX && robot.getY() < toY){
+            switch (robot.getDirection()) {
+                case UP:
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    break;
+            }
+
+            for (int i = robot.getY(); i < toY; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if(robot.getX() == toX && robot.getY() > toY){
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+            }
+
+            for (int i = robot.getY(); i > toY; i--) {
+                robot.stepForward();
+            }
+        }
+
+        if(robot.getX() < toX && robot.getY() == toY){
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+            }
+
+            for (int i = robot.getX(); i < toX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if(robot.getX() < toX && robot.getY() == toY){
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    break;
+            }
+
+            for (int i = robot.getX(); i > toX; i--) {
+                robot.stepForward();
+            }
+        }
     }
 }
 
