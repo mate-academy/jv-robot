6d38b4c897ccd0e8d90d5e605b9c6bd0ed59932f
Daria <dariyapikul@gmail.com>
2020-07-29 06:45:25 +0300

    added solution

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -13,7 +13,85 @@ package core.basesyntax;
 public class FieldXY {
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+
+        int actualY = robot.getY();
+        Direction actualDirection = robot.getDirection();
+
+        if (toX < robot.getX()) {
+            switch (actualDirection) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+            for (int i = robot.getX(); i >= toX; i--) {
+                robot.stepForward();
+            }
+        } else if (toX > robot.getX()) {
+            switch (actualDirection) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                default:
+                    break;
+            }
+            for (int i = robot.getX(); i <= toX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (toY < actualY) {
+            switch (actualDirection) {
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+            for (int i = robot.getY(); i >= toX; i--) {
+                robot.stepForward();
+            }
+        } else if (toY > actualY) {
+            switch (actualDirection) {
+                case UP:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                default:
+                    break;
+            }
+            for (int i = robot.getX(); i <= toX; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
 
