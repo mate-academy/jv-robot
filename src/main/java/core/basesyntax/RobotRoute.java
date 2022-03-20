ed766b881658f791ca28d1534223bf3b1d6eae64
Sergey Pshenichnyh <serg.pshenichnyh@gmail.com>
2022-03-20 23:35:21 +0200

    I returned the robot home!) Class RobotRoute edit (mvn clean package).

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -4,6 +4,7 @@ public class RobotRoute {
     private Robot robot;
     private int toX;
     private int toY;
+
     public void moveRobot(Robot robot, int toX, int toY) {
         this.robot = robot;
         this.toX = toX;
@@ -13,22 +14,27 @@ public class RobotRoute {
         turnRobot(getDirectionY());
         moveToY();
     }
+
     private Direction getDirectionX() {
         return robot.getX() - toX < 0 ? Direction.RIGHT : Direction.LEFT;
     }
+
     private Direction getDirectionY() {
         return this.robot.getY() - this.toY < 0 ? Direction.UP : Direction.DOWN;
     }
+
     private void turnRobot(Direction direction) {
         while (this.robot.getDirection() != direction) {
             this.robot.turnRight();
         }
     }
+
     private void moveToX() {
         while (this.robot.getX() != toX) {
             this.robot.stepForward();
         }
     }
+
     private void moveToY() {
         while (this.robot.getY() != toY) {
             this.robot.stepForward();
