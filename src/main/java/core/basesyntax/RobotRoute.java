7a1833b8ab8e471b25dcb2d170fad33809d84dc9
l_osyash <los.nazar24@gmail.com>
2022-03-21 17:08:23 +0200

    Method moveRobot in class RobotRoute was implemented.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,41 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        Direction aimDirection;
+        if (toX > robot.getX()) {
+            aimDirection = Direction.RIGHT;
+            while (aimDirection != robot.getDirection()) {
+                robot.turnRight();
+            }
+            do {
+                robot.stepForward();
+            } while (robot.getX() != toX);
+        } else if (toX < robot.getX()) {
+            aimDirection = Direction.LEFT;
+            while (aimDirection != robot.getDirection()) {
+                robot.turnLeft();
+            }
+            do {
+                robot.stepForward();
+            } while (robot.getX() != toX);
+        }
+        if (toY > robot.getY()) {
+            aimDirection = Direction.UP;
+            while (aimDirection != robot.getDirection()) {
+                robot.turnLeft();
+            }
+            do {
+                robot.stepForward();
+            } while (robot.getY() != toY);
+        } else if (toY < robot.getY()) {
+            aimDirection = Direction.DOWN;
+            while (aimDirection != robot.getDirection()) {
+                robot.turnLeft();
+            }
+            do {
+                robot.stepForward();
+            } while (robot.getY() != toY);
+        }
     }
 }
