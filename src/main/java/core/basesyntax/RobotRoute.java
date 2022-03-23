7d7ac195e9014af1ae8ffcf9973de184bdb0dfc1
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-03-23 22:26:21 +0200

    added solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,48 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        if(robot.getY()!=toX) {
+            if (robot.getX() > toX) {
+                if (robot.getDirection() == Direction.DOWN) {
+                    robot.turnRight();
+                }
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+            }
+            if (robot.getX() < toX) {
+                if (robot.getDirection() == Direction.UP) {
+                    robot.turnRight();
+                }
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+        if(robot.getY()!=toY) {
+            if (robot.getY() > toY) {
+                if (robot.getDirection() == Direction.LEFT) {
+                    robot.turnLeft();
+                }
+                while(robot.getDirection()!=Direction.DOWN){
+                    robot.turnRight();
+                }
+            }
+            if (robot.getY() < toY) {
+                if (robot.getDirection() == Direction.LEFT) {
+                    robot.turnRight();
+                }
+                while(robot.getDirection()!=Direction.UP){
+                    robot.turnLeft();
+                }
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
