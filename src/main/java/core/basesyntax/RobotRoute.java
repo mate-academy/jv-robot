3ba8eb69e4c1e2934fbd387979698c638e94a9cd
Vitalii Smahlenko <smaglenko@gmail.com>
2022-03-26 14:22:41 +0200

    Implemented class RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,51 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //Correcting the direction of movement of the robot
+        Direction direction = correctDirectionX(robot.getDirection(), robot.getX(), toX);
+        //Changing the direction of the robot
+        changeDirection(robot, direction);
+        //Move
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        //Correcting the direction of movement of the robot
+        direction = correctDirectionY(robot.getDirection(), robot.getY(), toY);
+        //Changing the direction of the robot
+        changeDirection(robot, direction);
+        //Move
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private Direction correctDirectionX(Direction direction, int startPoint, int toPoint) {
+        if (startPoint == toPoint) {
+            return direction;
+        } else if (startPoint > toPoint) {
+            return Direction.LEFT;
+        } else {
+            return Direction.RIGHT;
+        }
+    }
+
+    private Direction correctDirectionY(Direction direction, int startPoint, int toPoint) {
+        if (startPoint == toPoint) {
+            return direction;
+        } else if (startPoint > toPoint) {
+            return Direction.DOWN;
+        } else {
+            return Direction.UP;
+        }
+    }
+
+    private void changeDirection(Robot robot, Direction direction) {
+        for (int i = 0; i < 4; i++) {
+            if (robot.getDirection() == direction) {
+                break;
+            } else {
+                robot.turnRight();
+            }
+        }
     }
 }
