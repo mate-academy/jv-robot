3a1578859707a73e0bf0ad9068fb180f7bce5b3d
Oleksandr Petrunovskyi <o.petrunovskyi@gmail.com>
2022-04-13 16:25:56 +0300

    the first commit for verification

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,27 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // if I must used all methods RobotRoute class
+        while (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+            } else {
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+            }
+            robot.stepForward();
+        }
+        // if I don't use all available methods, but create setter setDirection for robot
+        while (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                robot.setDirection(Direction.DOWN);
+            } else {
+                robot.setDirection(Direction.UP);
+            }
+            robot.stepForward();
+        }
     }
 }
