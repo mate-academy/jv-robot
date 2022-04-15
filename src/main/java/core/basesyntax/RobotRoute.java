98969670300ae4478bdc74976b34b5dcf049d949
Alex Tyschenko <qwitelite@gmail.com>
2022-04-15 12:20:14 +0300

    code for move the robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,34 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //for coordinates x
+        while (robot.getX() != toX) {
+          if (robot.getX() > toX) {
+              while (robot.getDirection() != Direction.LEFT) {
+                  robot.turnLeft();
+              }
+          }else {
+              while (robot.getDirection() != Direction.RIGHT) {
+                  robot.turnRight();
+              }
+          }
+        robot.stepForward();
+        }
+
+        //for coordinates y
+
+        while (robot.getY() != toY) {
+            if (robot.getY() < toY) {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+
+            }else {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnLeft();
+                }
+            }
+            robot.stepForward();
+        }
     }
 }
