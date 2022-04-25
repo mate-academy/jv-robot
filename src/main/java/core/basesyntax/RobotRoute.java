25c0482299e7740cf91ef03660932ffa0d245b03
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-04-25 15:12:23 +0300

    Created method moveRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,29 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() > toX) {
+            while (Direction.LEFT != robot.getDirection()) {
+                robot.turnLeft();
+            }
+        } else {
+            while (Direction.RIGHT != robot.getDirection()) {
+                robot.turnRight();
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if (robot.getY() > toY) {
+            while (Direction.DOWN != robot.getDirection()) {
+                robot.turnLeft();
+            }
+        } else {
+            while (Direction.UP != robot.getDirection()) {
+                robot.turnRight();
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
