c43e4660f801209bd5a15059ed9713ba0b310d9f
Oleksii Mospan <alexey.mospan@gmail.com>
2022-04-29 01:31:45 +0300

    deleted comments from RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,20 +2,15 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
-        //find right rotation for X move
         while((robot.getX() < toX ? Direction.RIGHT : Direction.LEFT) != robot.getDirection()){
             robot.turnLeft();
         }
-        //going by X
         while (robot.getX() != toX){
             robot.stepForward();
         }
-        //find right rotation for Y move
         while((robot.getY() < toY ? Direction.UP : Direction.DOWN) != robot.getDirection()){
             robot.turnLeft();
         }
-        //going by Y
         while (robot.getY() != toY){
             robot.stepForward();
         }
