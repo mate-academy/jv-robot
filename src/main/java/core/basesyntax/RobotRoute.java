6c351204b85e835d816c3b7474582a05b42fcd45
Oleksii Mospan <alexey.mospan@gmail.com>
2022-04-29 01:34:17 +0300

    added some spaces for more nice code

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,16 +2,16 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        while((robot.getX() < toX ? Direction.RIGHT : Direction.LEFT) != robot.getDirection()){
+        while ((robot.getX() < toX ? Direction.RIGHT : Direction.LEFT) != robot.getDirection()) {
             robot.turnLeft();
         }
-        while (robot.getX() != toX){
+        while (robot.getX() != toX) {
             robot.stepForward();
         }
-        while((robot.getY() < toY ? Direction.UP : Direction.DOWN) != robot.getDirection()){
+        while ((robot.getY() < toY ? Direction.UP : Direction.DOWN) != robot.getDirection()) {
             robot.turnLeft();
         }
-        while (robot.getY() != toY){
+        while (robot.getY() != toY) {
             robot.stepForward();
         }
     }
