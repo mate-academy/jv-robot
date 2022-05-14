2e668ee995fb143fdaa01b679fc609a1cdffec02
Viacheslav Bondar <mcx.bondar@outlook.com>
2022-05-14 20:23:31 +0300

    second fixes robot solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         RobotRoute robotRoute = new RobotRoute();
-        Robot testRobot = new Robot(Direction.LEFT, 6, 3);
+        Robot testRobot = new Robot(Direction.LEFT, 7, 3);
         robotRoute.moveRobot(testRobot, -4, 0);
         System.out.println(testRobot.getX() + " " + testRobot.getY());
     }
