4a4081ba24ed487c61b7349d54ceb9104826de02
nazarbaklanov <nazarbaklanov@gmail.com>
2022-05-15 21:08:37 +0300

    Added if and while to class RobotRoute.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,32 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        }
+        if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+
+        if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        }
+        if (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+        }
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
