85f84289d514b333ec6f1369d85ab0e3d9c79c01
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-05-15 10:23:42 +0300

    method moveRobot was implemented / BUILD SUCCESS

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,24 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction buildDirection;
+        if (robot.getX() != toX) {
+            buildDirection = robot.getX() - toX > 0 ? Direction.LEFT : Direction.RIGHT;
+            while (robot.getDirection() != buildDirection) {
+                robot.turnRight();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getY() != toY) {
+            buildDirection = robot.getY() - toY > 0 ? Direction.DOWN : Direction.UP;
+            while (robot.getDirection() != buildDirection) {
+                robot.turnRight();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
