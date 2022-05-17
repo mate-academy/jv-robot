f9ecf76be10a64e023d6bcdde6ffe7eb57f74d05
Ilya <zimmboco@gmail.com>
2022-05-17 17:03:18 +0300

    reflection

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import java.lang.reflect.Field;
+
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        try {
+            Field f1 = robot.getClass().getField("coordinateX");
+            f1.setAccessible(true);
+            f1.set(robot, toX);
+            Field f2 = robot.getClass().getField("coordinateY");
+            f2.setAccessible(true);
+            f2.set(robot, toY);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
