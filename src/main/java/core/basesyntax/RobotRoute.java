4a8b1cd82d9ce30e3494d9bab9aa5b761563e734
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-17 12:27:12 +0300

    first time commit.checkflow

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,29 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX <= robot.getX()) {
+            robot.turnRight();
+            while (toX != robot.getX()) {
+                robot.stepForward();
+            }
+        }
+        if (toX >= robot.getX()) {
+            robot.turnLeft();
+            while (toX != robot.getX()) {
+                robot.stepForward();
+            }
+        }
+        if (toY <= robot.getY()) {
+            robot.turnLeft();
+            while (toY != robot.getY()) {
+                robot.stepForward();
+            }
+        }
+        if (toY >= robot.getY()) {
+            robot.turnRight();
+            while (toY != robot.getY()) {
+                robot.stepForward();
+            }
+        }
     }
 }
