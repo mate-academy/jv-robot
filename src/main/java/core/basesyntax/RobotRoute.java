3d8de5b63a8c068f09bd93b496cf144e56ffcc46
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-17 17:05:42 +0300

    Robot is moving!

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,26 +2,34 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (toX <= robot.getX()) {
-            robot.turnRight();
+        if (toX < robot.getX()) {
+            while (!Direction.LEFT.equals(robot.getDirection())) {
+                robot.turnRight();
+            }
             while (toX != robot.getX()) {
                 robot.stepForward();
             }
         }
-        if (toX >= robot.getX()) {
-            robot.turnLeft();
+        if (toX > robot.getX()) {
+            while (!Direction.RIGHT.equals(robot.getDirection())) {
+                robot.turnLeft();
+            }
             while (toX != robot.getX()) {
                 robot.stepForward();
             }
         }
-        if (toY <= robot.getY()) {
-            robot.turnLeft();
+        if (toY < robot.getY()) {
+            while (!Direction.DOWN.equals(robot.getDirection())) {
+                robot.turnLeft();
+            }
             while (toY != robot.getY()) {
                 robot.stepForward();
             }
         }
         if (toY >= robot.getY()) {
-            robot.turnRight();
+            while (!Direction.UP.equals(robot.getDirection())) {
+                robot.turnRight();
+            }
             while (toY != robot.getY()) {
                 robot.stepForward();
             }
