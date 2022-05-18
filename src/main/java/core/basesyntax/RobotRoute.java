ef67d1f5baf41aae4cc3a30c9b3ced38ded4c231
Ilya <zimmboco@gmail.com>
2022-05-18 11:43:08 +0300

    reflection

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -4,34 +4,20 @@ public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         //write your solution here
         //1. ищем по координатам x.
-        if (toX < robot.getX()) {
-            while (!Direction.LEFT.equals(robot.getDirection())) {
+        Direction direction = toX < robot.getX() ? Direction.LEFT : Direction.RIGHT;
+        if (toX != robot.getX()) {
+            while (!direction.equals(robot.getDirection())) {
                 robot.turnLeft();
-            }
-            while (toX != robot.getX()) {
-                robot.stepForward();
-            }
-        }
-        if (toX > robot.getX()) {
-            while (!Direction.RIGHT.equals(robot.getDirection())) {
-                robot.turnRight();
             }
             while (toX != robot.getX()) {
                 robot.stepForward();
             }
         }
         //2. ищем по координатам y.
-        if (toY < robot.getY()) {
-            while (!Direction.DOWN.equals(robot.getDirection())) {
+        direction = toY < robot.getY() ? Direction.DOWN : Direction.UP;
+        if (toY != robot.getY()) {
+            while (!direction.equals(robot.getDirection())) {
                 robot.turnLeft();
-            }
-            while (toY != robot.getY()) {
-                robot.stepForward();
-            }
-        }
-        if (toY > robot.getY()) {
-            while (!Direction.UP.equals(robot.getDirection())) {
-                robot.turnRight();
             }
             while (toY != robot.getY()) {
                 robot.stepForward();
