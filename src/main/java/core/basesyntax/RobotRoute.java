2095e6d0c114a0df73ef9884ffd922f80f3d716b
Mykola Yakymiv <mykola8426@gmail.com>
2022-05-18 19:09:28 +0300

    method RobotRoute was created

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,16 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getDirection() != Direction.LEFT) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        robot.turnLeft();
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+
     }
 }
