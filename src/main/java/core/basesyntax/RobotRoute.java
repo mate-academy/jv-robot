520da915135ed245617d4df48de97213645cc971
Karina Tanasiichuk <k.tanasiichuk18@gmail.com>
2022-05-21 19:52:09 +0300

    robot test

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,59 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+            if (robot.getY() > toY) {
+                robot.turnLeft();
+                while (robot.getY() > toY) {
+                    robot.stepForward();
+                }
+            } else {
+                robot.turnRight();
+                while (robot.getY() < toY) {
+                    robot.stepForward();
+                }
+            }
+        } else if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (toX > robot.getX()) {
+                robot.stepForward();
+            }
+            if (robot.getY() > toY) {
+                robot.turnRight();
+                while (robot.getY() > toY) {
+                    robot.stepForward();
+                }
+            } else {
+                robot.turnLeft();
+                while (robot.getY() < toY) {
+                    robot.stepForward();
+                }
+            }
+        } else if (toX == robot.getX()) {
+            if (robot.getY() > toY) {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnRight();
+                }
+                while (robot.getY() > toY) {
+                    robot.stepForward();
+                }
+            } else {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnLeft();
+                }
+                while (robot.getY() < toY) {
+                    robot.stepForward();
+                }
+            }
+        }
     }
 }
+
