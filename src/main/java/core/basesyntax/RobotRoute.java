35e199f0442c7aec4a83565d84309977653345c7
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-05-22 15:13:08 +0300

    RobotRoute class was written

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,45 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() > toX) {
+            lookLeft(robot);
+        } else if (robot.getX() < toX) {
+            lookRight(robot);
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if (robot.getY() > toY) {
+            lookDown(robot);
+        } else if (robot.getY() < toY) {
+            lookUp(robot);
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private void lookLeft(Robot robot) {
+        while (!robot.getDirection().equals(Direction.LEFT)) {
+            robot.turnLeft();
+        }
+    }
+
+    private void lookRight(Robot robot) {
+        while (!robot.getDirection().equals(Direction.RIGHT)) {
+            robot.turnRight();
+        }
+    }
+
+    private void lookUp(Robot robot) {
+        while (!robot.getDirection().equals(Direction.DOWN)) {
+            robot.turnLeft();
+        }
+    }
+
+    private void lookDown(Robot robot) {
+        while (!robot.getDirection().equals(Direction.UP)) {
+            robot.turnRight();
+        }
     }
 }
