f4454786088c6d8d64e23b9fa175283c9c5b88ab
Sasha Vradiy <alexvradii@gmail.com>
2022-06-08 16:09:20 +0300

    My solution of RobotRoute.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,25 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        System.out.println("Start position " + robot.getX() + " " + robot.getY() + ". Direction of view: " + robot.getDirection());
+
+        if (robot.getY() != toY) {
+            while (robot.getDirection() != ((robot.getY() < toY) ? Direction.UP : Direction.DOWN)) {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getX() != toX) {
+            while (robot.getDirection() != ((robot.getX() < toX) ? Direction.RIGHT : Direction.LEFT)) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+
+        System.out.println("End position " + robot.getX() + " " + robot.getY() + ". Direction of view: " + robot.getDirection());
     }
 }
