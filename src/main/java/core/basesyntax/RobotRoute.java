c6bb7e94bec6f37343736e3c3683d2facc1bde6b
Azarov Artur <azarovartyr@gmail.com>
2022-06-23 18:10:32 +0300

    requested changes applied

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         if (robot.getX() < toX) {
-            while(robot.getDirection() != Direction.RIGHT) {
+            while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-            while(robot.getX() != toX) {
+            while (robot.getX() != toX) {
                 robot.stepForward();
             }
         } else if (robot.getX() > toX) {
