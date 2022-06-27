07d3340c0870bc97429bdc59a2af4d0173b4999b
Andrii Zakhaarchenko <C:\Progs\BatBase>
2022-06-27 07:56:33 +0300

    jv-robot-hw3-done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Robot Johny = new Robot(Direction.RIGHT, 2, 5);
+        Robot johny = new Robot(Direction.RIGHT, 2, 5);
         RobotRoute robotRoute = new RobotRoute();
-        robotRoute.moveRobot(Johny, -4, -13);
+        robotRoute.moveRobot(johny, -4, -13);
     }
 }
--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,35 +2,34 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
         //horizontal move
-        if (robot.getX() > toX) {                               //Set direction for X
-            while (robot.getDirection() != Direction.LEFT) {    //Turn to Left direction from current direction
+        if (robot.getX() > toX) { //Set direction for X
+            while (robot.getDirection() != Direction.LEFT) { //Turn to Left direction
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) { //Turn to Right direction
                 robot.turnLeft();
             }
         }
-        else {
-            while (robot.getDirection() != Direction.RIGHT) {   //Turn to Right direction from current direction
-                robot.turnLeft();
-            }
-        }
-        while (robot.getX() != toX) {                           //move step by step to final X position
+        while (robot.getX() != toX) { //move step by step to final X position
             robot.stepForward();
         }
 
         //Vertical move Y
-        if (robot.getY() > toY) {                               //Set direction for Y
-            while (robot.getDirection() != Direction.DOWN) {    //Turn to Left direction from current direction
+        if (robot.getY() > toY) { //Set direction for Y
+            while (robot.getDirection() != Direction.DOWN) { //Turn to Left direction
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.UP) { //Turn to Right direction
                 robot.turnLeft();
             }
         }
-        else {
-            while (robot.getDirection() != Direction.UP) {      //Turn to Right direction from current direction
-                robot.turnLeft();
-            }
-        }
-        while (robot.getY() != toY) {                           //move step by step to final X position
+        while (robot.getY() != toY) { //move step by step to final X position
             robot.stepForward();
         }
     }
 }
+
