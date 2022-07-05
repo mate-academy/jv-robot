4b71c3d15cdef026a2cc8add3155c3382d39ba89
Vlad Andriichuk <vlad150096@gmail.com>
2022-07-05 21:32:52 +0200

    Realized the method moveRobot and 4 additional methods with logic to solve the task.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,69 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        RobotRoute route = new RobotRoute();
+        if (toX > 0 && toY > 0) {
+            route.moveRobotToFirstQuadrant(robot, toX, toY);
+        } else if (toX < 0 && toY > 0) {
+            route.moveRobotToSecondQuadrant(robot, toX, toY);
+        } else if (toX > 0 && toY < 0) {
+            route.moveRobotToThirdQuadrant(robot, toX, toY);
+        } else if (toX < 0 && toY < 0) {
+            route.moveRobotToFourthQuadrant(robot, toX, toY);
+        }
+        System.out.println(robot.getX() + " " + robot.getY());
+    }
+
+    private void moveRobotToFirstQuadrant(Robot robot, int coordinateX, int coordinateY) {
+        while (robot.getDirection() != Direction.RIGHT) {
+            robot.turnRight();
+        }
+        while (robot.getX() != coordinateX) {
+            robot.stepForward();
+        }
+        robot.turnLeft();
+        while (robot.getY() != coordinateY) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveRobotToSecondQuadrant(Robot robot, int coordinateX, int coordinateY) {
+        while (robot.getDirection() != Direction.LEFT) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != coordinateX) {
+            robot.stepForward();
+        }
+        robot.turnRight();
+        while (robot.getY() != coordinateY) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveRobotToThirdQuadrant(Robot robot, int coordinateX, int coordinateY) {
+        while (robot.getDirection() != Direction.RIGHT) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != coordinateX) {
+            robot.stepForward();
+        }
+        robot.turnRight();
+        while (robot.getY() != coordinateY) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveRobotToFourthQuadrant(Robot robot, int coordinateX, int coordinateY) {
+        while (robot.getDirection() != Direction.LEFT) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != coordinateX) {
+            robot.stepForward();
+        }
+        robot.turnLeft();
+        while (robot.getY() != coordinateY) {
+            robot.stepForward();
+        }
     }
 }
