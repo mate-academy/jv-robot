ee61f327b368786f3e0d20b017581a34fc5beb2f
ssplot56 <ssplot56@gmail.com>
2022-07-08 14:03:36 +0300

    created robotRoute logic

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,42 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX > robot.getX()) {
+            while(robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+            if (toY > robot.getY()) {
+                robot.turnLeft();
+                while (robot.getY() != toY) {
+                    robot.stepForward();
+                }
+            } else {
+                robot.turnRight();
+                while (robot.getY() != toY) {
+                    robot.stepForward();
+                }
+            }
+        } else {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+            if (toY > robot.getY()) {
+                robot.turnRight();
+                while (robot.getY() != toY) {
+                    robot.stepForward();
+                }
+            } else {
+                robot.turnLeft();
+                while (robot.getY() != toY) {
+                    robot.stepForward();
+                }
+            }
+        }
     }
 }
