be85dcac4a84b57fdeab57cf843984c42cda382a
Alexander <liero2D@gmail.com>
2022-07-17 16:52:07 +0300

    Created if-else, while statements. Task solved.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentLocationX = robot.getX();
+        int currentLocationY = robot.getY();
+
+        if (currentLocationX > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else if (currentLocationX < toX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+
+        if (currentLocationY > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        } else if (currentLocationY < toY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+        }
+
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
+
