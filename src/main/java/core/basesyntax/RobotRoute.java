aaa09862afeac6b268c4c5a342d9babbe3698a7f
Nazar <litvik.12@gmail.com>
2022-07-26 16:27:10 +0300

    added code for robot movement

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        robot.turnLeft();
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+        robot.turnRight();
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
