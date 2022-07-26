7db7f0bc565f40fd78f5c72a2e7e817fcae7a645
asha <ashajanebrown@gmail.com>
2022-07-27 00:45:41 +0300

    the robot has found his home route

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,99 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int x = robot.getX();
+        int y = robot.getY();
+        Direction direction = robot.getDirection();
+
+        //Standing on the home x coordinate
+        if (x == toX) {
+            if (y > toY) {
+                while (!robot.getDirection().equals(Direction.DOWN)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (y - toY); step++) {
+                    robot.stepForward();
+                }
+            } else if (y < toY) {
+                while (!robot.getDirection().equals(Direction.UP)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (toY - y); step++) {
+                    robot.stepForward();
+                }
+            }
+        }
+
+        //Standing on the home y coordinate
+        if (y == toY) {
+            if (x > toX) {
+                while (!robot.getDirection().equals(Direction.LEFT)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (x - toX); step++) {
+                    robot.stepForward();
+                }
+            } else if (x < toX) {
+                while (!robot.getDirection().equals(Direction.RIGHT)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (toX - x); step++) {
+                    robot.stepForward();
+                }
+            }
+        }
+
+        if (x > toX) {
+            if (y > toY) {
+                while (!robot.getDirection().equals(Direction.DOWN)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (y - toY); step++) {
+                    robot.stepForward();
+                }
+            } else if (y < toY) {
+                while (!robot.getDirection().equals(Direction.UP)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (toY - y); step++) {
+                    robot.stepForward();
+                }
+            }
+
+            while (!robot.getDirection().equals(Direction.LEFT)) {
+                robot.turnLeft();
+            }
+
+            for (int step = 1; step <= (x - toX); step++) {
+                robot.stepForward();
+            }
+        }
+
+        if (x < toX) {
+            if (y > toY) {
+                while (!robot.getDirection().equals(Direction.DOWN)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (y - toY); step++) {
+                    robot.stepForward();
+                }
+            } else if (y < toY) {
+                while (!robot.getDirection().equals(Direction.UP)) {
+                    robot.turnLeft();
+                }
+                for (int step = 1; step <= (toY - y); step++) {
+                    robot.stepForward();
+                }
+            }
+
+            while (!robot.getDirection().equals(Direction.RIGHT)) {
+                robot.turnLeft();
+            }
+
+            for (int step = 1; step <= (toX - x); step++) {
+                robot.stepForward();
+            }
+        }
+
     }
 }
