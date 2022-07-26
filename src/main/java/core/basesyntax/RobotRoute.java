62cf387bd2842f5f2d33afa7555fb53d16850b5e
Veers <raadch3011@gmail.com>
2022-07-26 22:55:43 +0300

    task solved by suggested algorithm

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,29 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        if (toX == robot.getX() && toY == robot.getY()) {}
+        else {
+            Direction neededDirX;
+            neededDirX = robot.getX() > toX ? Direction.LEFT : Direction.RIGHT;
+
+            while (neededDirX != robot.getDirection()) {
+                robot.turnLeft();
+            }
+
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+
+            Direction neededDirY;
+            neededDirY = robot.getY() > toY ? Direction.DOWN : Direction.UP;
+
+            while (neededDirY != robot.getDirection()) {
+                robot.turnLeft();
+            }
+
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
