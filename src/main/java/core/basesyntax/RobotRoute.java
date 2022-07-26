66b3f3a442e3791c263422c3414b3de9475f898b
Nazar <litvik.12@gmail.com>
2022-07-26 23:10:48 +0300

    added moveable realisation for the robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,29 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getY() < toY) {
+            do {
+                robot.turnRight();
+            } while (robot.getDirection() != Direction.UP);
+        } else if (robot.getY() > toY) {
+            do {
+                robot.turnLeft();
+            } while (robot.getDirection() != Direction.DOWN);
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+        if (robot.getX() < toX) {
+            do {
+                robot.turnLeft();
+            } while (robot.getDirection() != Direction.RIGHT);
+        } else if (robot.getX() > toX) {
+            do {
+                robot.turnRight();
+            } while (robot.getDirection() != Direction.LEFT);
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
     }
 }
