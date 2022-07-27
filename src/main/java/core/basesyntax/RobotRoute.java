eb2ad7af5a5b7f7ca74751123a1c4f7fd6240df9
Misha Brovkin <haasiakira@gmail.com>
2022-07-27 15:34:38 +0300

    robotRouteMethodImplemented

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,24 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        Robot amigo = new Robot(robot.getDirection(), robot.getX(), robot.getY());
+
+        if (amigo.getX() > toX) {
+            amigo.turnLeft();
+        }   amigo.turnRight();
+
+        int xDistance = amigo.getX() - toX;
+        while (xDistance > 0) {
+            amigo.stepForward();
+        }
+
+        if (amigo.getY() > toY) {
+            amigo.turnLeft();
+        }   amigo.turnRight();
+
+        int yDistance = amigo.getY() - toY;
+        while (yDistance > 0) {
+            amigo.stepForward();
+        }
     }
 }
