b1b0a814efc8d29de20bd9b5670593a8bb6e8330
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-07-27 21:02:38 +0300

    1) Robot take right position (turn on) 2) Robot move to needed position

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,47 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // Розвертаємо робота по осі Х в правильному напрямку
+        if (robot.getX() < toX) {
+            while (!robot.getDirection().equals(Direction.RIGHT)) {
+                robot.turnRight();
+            }
+
+            // Рухаємо робота до потрібної точки по осі Х коли ми в мінусовій позиції
+            while (toX != robot.getX()) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() > toX) {
+            while (!robot.getDirection().equals(Direction.LEFT)) {
+                robot.turnRight();
+            }
+
+            // Рухаємо робота до потрібної точки по осі Х коли ми в плюсовій позиції
+            while (toX != robot.getX()) {
+                robot.stepForward();
+            }
+        }
+
+        // Розвертаємо робота по осі Y в правильному напрямку
+        if (robot.getY() < toY) {
+            while (!robot.getDirection().equals(Direction.UP)) {
+                robot.turnRight();
+            }
+
+            // Рухаємо робота до потрібної точки по осі Y коли ми в мінусовій позиції
+            while (toY != robot.getY()) {
+                robot.stepForward();
+            }
+        } else if (robot.getY() > toY) {
+            while (!robot.getDirection().equals(Direction.DOWN)) {
+                robot.turnRight();
+            }
+
+            // Рухаємо робота до потрібної точки по осі Y коли ми в плюсовій позиції
+            while (toY != robot.getY()) {
+                robot.stepForward();
+            }
+        }
+
     }
 }
