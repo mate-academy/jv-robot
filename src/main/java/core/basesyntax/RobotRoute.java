f40cb2ae939bc3aa0d3a35483434923ae974495b
LitvinOleksandr <sanek.litvin@gmail.com>
2022-07-28 13:05:48 +0300

    crete methods how must robot go to home. changes int toX toY for my opinion

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,48 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+    public static void moveRobot(Robot robot, int finishX, int finishY) {
+        while (robot.getX() != finishX || robot.getY() != finishY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    if (robot.getY() < finishY) {
+                        robot.stepForward();
+                    } else if (robot.getX() > finishX) {
+                        robot.turnLeft();
+                    } else {
+                        robot.turnRight();
+                    }
+                    break;
+                case DOWN:
+                    if (robot.getY() > finishY) {
+                        robot.stepForward();
+                    } else if (robot.getX() < finishX) {
+                        robot.turnLeft();
+                    } else {
+                        robot.turnRight();
+                    }
+                    break;
+                case LEFT:
+                    if (robot.getX() > finishX) {
+                        robot.stepForward();
+                    } else if (robot.getY() > finishY) {
+                        robot.turnLeft();
+                    } else {
+                        robot.turnRight();
+                    }
+                    break;
+                case RIGHT:
+                    if (robot.getX() < finishX) {
+                        robot.stepForward();
+                    } else if (robot.getY() < finishY) {
+                        robot.turnLeft();
+                    } else {
+                        robot.turnRight();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
