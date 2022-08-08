6ef3ff4ff8fc4ccc21dc28ddaeefac8064928f20
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-08 17:38:01 +0200

    Solution has been added. Maybe with bad code style((

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,59 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        setCorrectDirectionOnX(robot,toX);
+        checkDirectionOnRightWay(robot);
+        goToTheX(robot, toX);
+        setCorrectDirectionOnY(robot, toY);
+        checkDirectionOnUpWay(robot);
+        goToTheY(robot, toY);
+    }
+
+    public void setCorrectDirectionOnX(Robot robot, int toX) {
+        if(robot.getX() < toX) {
+            robot.setDirection(Direction.RIGHT);
+        } else {
+            robot.setDirection(Direction.LEFT);
+        }
+    }
+
+    public void setCorrectDirectionOnY(Robot robot, int toY) {
+        if(robot.getY() < toY) {
+            robot.setDirection(Direction.UP);
+        } else {
+            robot.setDirection(Direction.DOWN);
+        }
+    }
+
+    public boolean checkDirectionOnRightWay(Robot robot) {
+        return robot.getDirection() == Direction.RIGHT;
+    }
+
+    public boolean checkDirectionOnUpWay(Robot robot) {
+        return robot.getDirection() == Direction.UP;
+    }
+
+    public void goToTheX(Robot robot, int toX) {
+        if(checkDirectionOnRightWay(robot)) {
+            while(robot.getX() < toX) {
+                robot.stepForward();
+            }
+        } else  {
+            while(robot.getX() > toX) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    public void goToTheY(Robot robot, int toY) {
+        if(checkDirectionOnUpWay(robot)) {
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        } else {
+            while (robot.getY() > toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
