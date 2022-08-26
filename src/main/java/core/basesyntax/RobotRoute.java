502352a1e426fdc8445cfdffbb35457a6cca2d59
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-08-26 13:24:17 +0300

    Implement moveRobot method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,73 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        this.moveRobotX(robot, toX);
+        this.moveRobotY(robot, toY);
+    }
+
+    private void moveRobotX(Robot robot, int toX) {
+
+        int steps = Math.abs(robot.getX() - toX);
+        Direction direction = robot.getDirection();
+
+        if (robot.getX() > toX) {
+            switch (direction) {
+                case UP -> robot.turnLeft();
+                case RIGHT -> {
+                    for (int i = 0; i < 2; i++) {
+                        robot.turnLeft();
+                    }
+                }
+                case DOWN -> robot.turnRight();
+            }
+
+        } else if (robot.getX() < toX) {
+            switch (direction) {
+                case UP -> robot.turnRight();
+                case LEFT -> {
+                    for (int i = 0; i < 2; i++) {
+                        robot.turnLeft();
+                    }
+                }
+                case DOWN -> robot.turnLeft();
+            }
+        }
+
+        for (int i = 0; i < steps; i++) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveRobotY(Robot robot, int toY) {
+
+        int steps = Math.abs(robot.getY() - toY);
+        Direction direction = robot.getDirection();
+
+        if (robot.getY() > toY) {
+            switch (direction) {
+                case RIGHT -> robot.turnRight();
+                case UP -> {
+                    for (int i = 0; i < 3; i++) {
+                        robot.turnLeft();
+                    }
+                }
+                case LEFT -> robot.turnLeft();
+            }
+
+        } else if (robot.getY() < toY) {
+            switch (direction) {
+                case RIGHT -> robot.turnLeft();
+                case DOWN -> {
+                    for (int i = 0; i < 3; i++) {
+                        robot.turnLeft();
+                    }
+                }
+                case LEFT -> robot.turnRight();
+            }
+        }
+
+        for (int i = 0; i < steps; i++) {
+            robot.stepForward();
+        }
     }
 }
