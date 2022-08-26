c307cad813409f186dbcf9ad893cc6924203962f
promahos <a.lukavenko@gmail.com>
2022-08-26 21:53:54 +0300

    robot moves added

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // move horizontally
+        int movesX = toX - robot.getX();
+        if (movesX > 0) {
+            turn(robot, Direction.RIGHT);
+        }
+        if (movesX < 0) {
+            turn(robot, Direction.LEFT);
+        }
+        move(movesX, robot);
+
+        // move vertically
+        int movesY = toY - robot.getY();
+        if (movesY > 0) {
+            turn(robot, Direction.UP);
+        }
+        if (movesY < 0) {
+            turn(robot, Direction.DOWN);
+        }
+        move(movesY, robot);
+    }
+
+    private void move(int moves, Robot robot) {
+        for (int i = 0; i < (moves < 0 ? -moves : moves); i++) {
+            robot.stepForward();
+        }
+    }
+
+    private void turn(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnLeft();
+        }
     }
 }
