0c98a0436ac09821d177d8c4770509084e3e9039
Dima Senko <d.senko@i.ua>
2022-08-29 09:32:53 +0300

    Robot is home

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        String message = "Robot is home";
+        while (robot.getX() < toX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            robot.stepForward();
+        }
+        while (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+            robot.stepForward();
+        }
+        if (robot.getX() == toX) {
+            if (robot.getY() == toY) {
+                System.out.println(message);
+            }
+            while (robot.getY() < toY) {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            }
+            while (robot.getY() > toY) {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            }
+            System.out.println(message);
+        }
     }
 }
