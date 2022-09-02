07f123ff15a73646229f8217d20e9800e9ae8df1
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-03 00:31:19 +0300

    Robot v3

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,79 +2,79 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-          while (robot.getX() != toX) {
-              switch (robot.getDirection()) {
-                  case UP: {
-                      if (robot.getX() < toX) {
-                            robot.turnRight();
-                      }
-                      else {
-                          robot.turnLeft();
-                      }
-                      break;
+        while (robot.getX() != toX) {
+            switch (robot.getDirection()) {
+                case UP: {
+                    if (robot.getX() < toX) {
+                        robot.turnRight();
                     }
-                    case DOWN: {
-                        if (robot.getX() > toX) {
-                            robot.turnRight();
-                        }
-                        else {
-                            robot.turnLeft();
-                        }
-                        break;
+                    else {
+                      robot.turnLeft();
                     }
-                    case LEFT: {
-                        if (robot.getX() < toX) {
-                            robot.turnRight();
-                            robot.turnRight();
-                        }
-                        break;
-                    }
-                    case RIGHT: {
-                        if (robot.getX() > toX) {
-                            robot.turnLeft();
-                            robot.turnLeft();
-                        }
-                        break;
-                    }
+                    break;
                 }
-            robot.stepForward();
-          }
-          while (robot.getY() != toY) {
-              switch (robot.getDirection()) {
-                  case UP: {
-                      if (robot.getY() < toY) {
-                          robot.turnRight();
-                          robot.turnRight();
-                      }
-                      break;
-                  }
-                  case DOWN: {
-                      if (robot.getY() > toY) {
-                          robot.turnLeft();
-                          robot.turnLeft();
-                      }
-                      break;
-                  }
-                  case LEFT: {
-                      if (robot.getY() < toY) {
-                          robot.turnRight();
-                      }
-                      else {
-                          robot.turnLeft();
-                      }
-                      break;
-                  }
-                  case RIGHT: {
-                      if (robot.getY() > toY) {
-                          robot.turnRight();
-                      }
-                      else {
-                          robot.turnLeft();
-                      }
-                      break;
-                  }
-              }
-              robot.stepForward();
+                case DOWN: {
+                    if (robot.getX() > toX) {
+                        robot.turnRight();
+                    }
+                    else {
+                        robot.turnLeft();
+                    }
+                    break;
+                }
+                case LEFT: {
+                    if (robot.getX() < toX) {
+                        robot.turnRight();
+                        robot.turnRight();
+                    }
+                    break;
+                }
+                case RIGHT: {
+                    if (robot.getX() > toX) {
+                        robot.turnLeft();
+                        robot.turnLeft();
+                    }
+                    break;
+                }
             }
+            robot.stepForward();
+        }
+        while (robot.getY() != toY) {
+            switch (robot.getDirection()) {
+                case UP: {
+                  if (robot.getY() < toY) {
+                      robot.turnRight();
+                      robot.turnRight();
+                  }
+                  break;
+                }
+                case DOWN: {
+                  if (robot.getY() > toY) {
+                      robot.turnLeft();
+                      robot.turnLeft();
+                  }
+                  break;
+                }
+                case LEFT: {
+                  if (robot.getY() < toY) {
+                      robot.turnRight();
+                  }
+                  else {
+                      robot.turnLeft();
+                  }
+                  break;
+                }
+                case RIGHT: {
+                  if (robot.getY() > toY) {
+                      robot.turnRight();
+                  }
+                  else {
+                      robot.turnLeft();
+                  }
+                  break;
+                }
+            }
+            robot.stepForward();
+        }
     }
 }
