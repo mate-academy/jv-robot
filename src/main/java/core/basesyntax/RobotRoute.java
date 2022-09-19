3aa857f21440027698e405222019a1915c550617
SomniumDZ <mshveda28@gmail.com>
2022-09-19 17:18:23 +0300

    Implement robot path find logic in RobotRoute class

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,21 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        Direction xDirection = robot.getX() > toX ? Direction.LEFT : Direction.RIGHT;
+        turnUntilCorrectDirection(robot, xDirection);
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        Direction yDirection = robot.getY() > toY ? Direction.DOWN : Direction.UP;
+        turnUntilCorrectDirection(robot, yDirection);
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private void turnUntilCorrectDirection(Robot robot, Direction correctDirection) {
+        while (robot.getDirection() != correctDirection) {
+            robot.turnLeft();
+        }
     }
 }
