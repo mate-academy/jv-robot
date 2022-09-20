a9c3779e08bd41187c3f724c52e0614824c7b34b
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-09-20 23:11:28 +0200

    Final version

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,41 @@
 package core.basesyntax;
 
+import org.jetbrains.annotations.NotNull;
+
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int x = robot.getX();
+        int y = robot.getY();
+
+        if (toX < x) {
+            while (robot.getDirection() != Direction.LEFT) {
+              robot.turnLeft();
+            }
+            for (int i = x; i > toX; i--) {
+              robot.stepForward();
+            }
+        }else if (toX > x) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            for (int i = x; i < toX; i++) {
+                robot.stepForward();
+            }
+        }
+        if (toY < y) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            for (int i = y; i > toY; i--) {
+                robot.stepForward();
+            }
+        } else if (toY > y) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            for (int i = y; i < toY; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
