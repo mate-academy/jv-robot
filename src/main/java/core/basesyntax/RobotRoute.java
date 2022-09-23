861324d3292570a0458ccf76bbe9ed50a6a0ca9f
o-shyshkan <o.shyshkan@gmail.com>
2022-09-23 20:41:28 +0300

    Fixed comment

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
         if (toX > robot.getX()) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
