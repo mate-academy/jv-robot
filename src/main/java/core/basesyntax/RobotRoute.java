0ca46b2e5ad246143b6e950efa9d9283f1a2cf2a
Sidir0m <romangolovatyi@yahoo.com>
2022-09-27 22:42:28 +0300

    Added three methods for resolved move robot problems

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    private void moveRight(Robot robot, int toX) {
+        if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveLeft(Robot robot, int toY) {
+        if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+        }
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
+    }
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveRight(robot, toX);
+        moveLeft(robot, toY);
     }
 }
