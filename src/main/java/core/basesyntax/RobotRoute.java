5d1680e8e39b5cb788560ae155e5a52489d7468d
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-09-27 16:23:19 +0300

    Imlement moveRobot method. Pass all tests.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,43 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        alignedXPos(robot, toX);
+        alignedYPos(robot, toY);
+    }
+
+    private void alignedYPos(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            moveRobotOnY(robot, toY, Direction.DOWN);
+        }
+        if (robot.getY() < toY) {
+            moveRobotOnY(robot, toY, Direction.UP);
+        }
+    }
+
+    private void moveRobotOnY(Robot robot, int toY, Direction direction) {
+        while (!robot.getDirection().equals(direction)) {
+            robot.turnLeft();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private void alignedXPos(Robot robot, int toX) {
+        if (robot.getX() > toX) {
+            moveRobotOnX(robot, toX, Direction.LEFT);
+        }
+        if (robot.getX() < toX) {
+            moveRobotOnX(robot, toX, Direction.RIGHT);
+        }
+    }
+
+    private void moveRobotOnX(Robot robot, int toX, Direction direction) {
+        while (!robot.getDirection().equals(direction)) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
     }
 }
