1293b3d6053edbd91d28218cfdc8d8456c604311
Nazarii Pertsak <pertsak98@gmail.com>
2022-09-27 15:41:32 +0300

    changed for with while

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,29 +2,29 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        for (int i = 0; robot.getX() != toX; i++) {
+        while (robot.getX() != toX) {
             if (robot.getX() < toX) {
-                for (int j = 0; !robot.getDirection().equals(Direction.RIGHT); j++) {
+                while (!robot.getDirection().equals(Direction.RIGHT)) {
                     robot.turnRight();
                 }
                 robot.stepForward();
             }
             if (robot.getX() > toX) {
-                for (int j = 0; !robot.getDirection().equals(Direction.LEFT); j++) {
+                while (!robot.getDirection().equals(Direction.LEFT)) {
                     robot.turnLeft();
                 }
                 robot.stepForward();
             }
         }
-        for (int i = 0; robot.getY() != toY; i++) {
+        while (robot.getY() != toY) {
             if (robot.getY() < toY) {
-                for (int j = 0; !robot.getDirection().equals(Direction.UP); j++) {
+                while (!robot.getDirection().equals(Direction.UP)) {
                     robot.turnRight();
                 }
                 robot.stepForward();
             }
             if (robot.getY() > toY) {
-                for (int j = 0; !robot.getDirection().equals(Direction.DOWN); j++) {
+                while (!robot.getDirection().equals(Direction.DOWN)) {
                     robot.turnLeft();
                 }
                 robot.stepForward();
