a480766b6b68818df0029eb2f967d32a4ff646dd
MuzykaVP <muzykavp.work@gmail.com>
2022-09-29 02:04:36 +0300

    Implemented moveRobot method, so Robot Johny now can come back home

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,21 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction turnDirX = robot.getX() < toX ? Direction.RIGHT : Direction.LEFT;
+        Direction turnDirY = robot.getY() < toY ? Direction.UP : Direction.DOWN;
+
+        while (robot.getX() != toX || robot.getY() != toY) {
+            if (turnDirX == robot.getDirection()) {
+                while (robot.getX() != toX) {
+                    robot.stepForward();
+                }
+            }
+            if (turnDirY == robot.getDirection()) {
+                while (robot.getY() != toY) {
+                    robot.stepForward();
+                }
+            }
+            robot.turnRight();
+        }
     }
 }
