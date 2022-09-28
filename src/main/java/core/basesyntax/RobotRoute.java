de082ff46605d85ab77a5f3ab8e5757b54f89447
Danyl Kniaziev <kdanil0401@gmail.com>
2022-09-28 19:18:00 +0300

    Implemented methods moveRobot, turnToX, turnToY, stepes

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,47 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        turnToX(robot, toX);
+        stepes(robot, toX);
+        turnToY(robot, toY);
+        stepes(robot, toY);
+    }
+
+    private void turnToX(Robot robot, int toX) {
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else if (robot.getX() < toX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnLeft();
+            }
+        }
+    }
+
+    private void turnToY(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        } else if (robot.getY() < toY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+        }
+    }
+
+    private void stepes(Robot robot, int cord) {
+        if (robot.getDirection() == Direction.RIGHT || robot.getDirection() == Direction.LEFT) {
+            while (robot.getX() != cord) {
+                robot.stepForward();
+            }
+        } else {
+            while (robot.getY() != cord) {
+                robot.stepForward();
+            }
+        }
     }
 }
+
+
