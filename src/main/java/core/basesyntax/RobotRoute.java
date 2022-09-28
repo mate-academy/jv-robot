46f3a1aceddcd817fc60b2a637551be37ae605e7
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-09-28 09:52:55 +0300

    horizontal move implemented

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,57 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() != toX) {
+            moveHorizontally(robot, toX);
+        }
+        if (robot.getY() != toY) {
+            moveVertically(robot, toY);
+        }
+    }
+
+    private void moveHorizontally(Robot robot, int toX) {
+
+        Direction correctHorizontal;
+
+        if (toX > robot.getX()) {
+            correctHorizontal = Direction.RIGHT;
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    for (int i = 0; i < 2; i++) {
+                        robot.turnLeft();
+                    }
+                    break;
+            }
+        } else {
+            correctHorizontal = Direction.LEFT;
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    for (int i = 0; i < 2; i++) {
+                        robot.turnLeft();
+                    }
+                    break;
+            }
+        }
+
+        int counter = Math.abs(toX - robot.getX());
+        for (int i = counter; i > 0; i--) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveVertically(Robot robot, int toY) {
+
     }
 }
