e015251a5ce6d4a65d76cf5bd632ea2ac7dfc8ff
Andriy <Andriy@email.com>
2022-10-17 18:16:55 +0300

    1st commit - logic movement

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,22 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) { robot.turnRight(); }
+            while (toX > robot.getX()) { robot.stepForward(); }
+        }
+        if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) { robot.turnLeft(); }
+            while (toX < robot.getX()) { robot.stepForward(); }
+        }
+
+        if (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.UP) { robot.turnRight(); }
+            while (toY > robot.getY()) { robot.stepForward(); }
+        }
+        if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) { robot.turnLeft(); }
+            while (toY < robot.getY()) { robot.stepForward(); }
+        }
     }
 }
