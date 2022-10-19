f4f86d2d33bcd7f48b95f19f0726183f52598354
Petro Korets <petrkorets@gmail.com>
2022-10-19 11:50:55 +0300

    Implemented moveRobot() method by determing direction and setting the path according to x & y coordinates.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,45 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX < robot.getX() && toX >= 0) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            for (int i = robot.getX(); i > toX; i++) {
+                robot.stepForward();
+            }
+        }
+        if (toX < robot.getX() && toX < 0) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            for (int i = robot.getX(); i > toX; i--) {
+                robot.stepForward();
+            }
+        }
+        if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            for (int i = robot.getX(); i < toX; i++) {
+                robot.stepForward();
+            }
+        }
+        if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            for (int i = robot.getY(); i > toY; i++) {
+                robot.stepForward();
+            }
+        }
+        if (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            for (int i = robot.getY(); i < toY; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
