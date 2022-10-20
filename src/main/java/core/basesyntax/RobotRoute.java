35b9931d8b080030fd73d296a56024ca11fd8f10
Bohdan Shevtsov <shevtsov.b@gmail.com>
2022-10-20 18:52:02 +0300

    After common mistakes changes

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -5,6 +5,16 @@ import static java.lang.Math.abs;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         //write your solution here
+        turnXRay(robot, toX);
+        moveXRay(robot, toX);
+        turnYRay(robot, toY);
+        moveYRay(robot, toY);
+        System.out.println("Robot reached point of destination. X = "
+                + robot.getX()
+                + " Y = " + robot.getY());
+    }
+
+    private static void turnXRay(Robot robot, int toX) {
         int distanceX1 = abs(toX - robot.getX());
         if (distanceX1 != 0) {
             robot.stepForward();
@@ -21,14 +31,20 @@ public class RobotRoute {
             if (distanceX1 > distanceX2 && distanceX2 != 0) {
                 robot.stepForward();
             }
-            for (int i = 0; i < 1000; i++) { //moving X ray
-                distanceX2 = abs(toX - robot.getX());
-                if (distanceX2 == 0) {
-                    break;
-                }
-                robot.stepForward();
-            }
         }
+    }
+
+    private static void moveXRay(Robot robot, int toX) {
+        for (int i = 0; i < 1000; i++) { //moving X ray
+            int distanceX = abs(toX - robot.getX());
+            if (distanceX == 0) {
+                break;
+            }
+            robot.stepForward();
+        }
+    }
+
+    private static void turnYRay(Robot robot, int toY) {
         int distanceY1 = abs(toY - robot.getY());
         if (distanceY1 != 0) {
             robot.turnLeft();
@@ -48,16 +64,17 @@ public class RobotRoute {
             if (distanceY1 > distanceY2 && distanceY2 != 0) {
                 robot.stepForward();
             }
-            for (int i = 0; i < 1000; i++) { //moving X ray
-                distanceY2 = abs(toY - robot.getY());
-                if (distanceY2 == 0) {
-                    break;
-                }
-                robot.stepForward();
-            }
         }
-        System.out.println("Robot reached point of destination. X = "
-                + robot.getX()
-                + " Y = " + robot.getY());
+    }
+
+    private static void moveYRay(Robot robot, int toY) {
+        for (int i = 0; i < 1000; i++) { //moving X ray
+            int distanceY = abs(toY - robot.getY());
+            if (distanceY == 0) {
+                break;
+            }
+            robot.stepForward();
+        }
     }
 }
+
