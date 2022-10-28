e76d7c9c9b2b2d9be759af2613e39c0911ff25dc
Viacheslav Bondar <mcx.bondar@outlook.com>
2022-10-28 21:57:56 +0300

    deleted extra checks

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,29 +2,22 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-
         if (robot.getX() > toX) {
             while (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             }
-        } else if (robot.getX() < toX) {
+        } else {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnLeft();
             }
         }
-
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-
         if (robot.getY() > toY) {
-            while (robot.getDirection() != Direction.DOWN) {
-                robot.turnLeft();
-            }
-        } else if (robot.getY() < toY) {
-            while (robot.getDirection() != Direction.UP) {
-                robot.turnLeft();
-            }
+            robot.turnLeft();
+        } else {
+            robot.turnRight();
         }
         while (robot.getY() != toY) {
             robot.stepForward();
