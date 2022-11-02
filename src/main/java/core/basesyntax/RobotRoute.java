81a28066fb41fc66f6c0b857e355a2b031c49941
Iskamele <iskamele@gmail.com>
2022-11-02 09:37:08 +0200

    Added robot rotation logic using if statement. Added a universal getMove method to move the robot along the X and Y axes.
    There are plans to move the rotation of the robot into a separate method and optimize it.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,47 @@
 package core.basesyntax;
 
+import static core.basesyntax.Direction.UP;
+
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+
+        if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            getMove(robot, toX, robot.getX());
+        }
+
+        if (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            getMove(robot, toY, robot.getY());
+        }
+
+        if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+            getMove(robot, toX, robot.getX());
+        }
+
+        if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+            getMove(robot, toY, robot.getY());
+        }
+    }
+
+    public void getMove(Robot robot, int to, int from) {
+        while (to != from) {
+            robot.stepForward();
+            if (from < to) {
+                from++;
+            } else {
+                from--;
+            }
+        }
     }
 }
