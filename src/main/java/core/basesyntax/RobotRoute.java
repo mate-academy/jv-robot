8aa2d2ba97d38ccfee3e8afcf3549063a102a18f
Dima Byrka <fobossen@gmail.com>
2022-11-07 21:01:38 +0200

    First version of jv-robot, all tests are successful

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,37 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        int coordinateX = robot.getX();
+        int coordinateY = robot.getY();
+        if (toX < coordinateX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            for (int i = coordinateX; i > toX; i--) {
+                robot.stepForward();
+            }
+        } else if (toX > coordinateX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            for (int i = coordinateX; i < toX; i++) {
+                robot.stepForward();
+            }
+        }
+        if (toY < coordinateY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            for (int i = coordinateY; i > toY; i--) {
+                robot.stepForward();
+            }
+        } else if (toY > coordinateY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            for (int i = coordinateY; i < toY; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
