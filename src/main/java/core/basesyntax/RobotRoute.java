a5c1f3259ed77766ff422dc9117fbf3a7625dfae
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2022-11-09 21:24:25 +0200

    first var of solution was added

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import static core.basesyntax.Direction.*;
+
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int numberOfStepsX = robot.getX() - toX;
+        int numberOfStepsY = robot.getY() - toY;
+        if (numberOfStepsX > 0) {
+            while (robot.getDirection() != RIGHT) {
+                robot.turnRight();
+            }
+        }
+        if (numberOfStepsX < 0) {
+            while (robot.getDirection() != LEFT) {
+                robot.turnLeft();
+            }
+        }
+        for (int i = 0; i < Math.abs(numberOfStepsX); i++) {
+            robot.stepForward();
+        }
+        if (numberOfStepsY > 0) {
+            while (robot.getDirection() != DOWN) {
+                robot.turnRight();
+            }
+            if (numberOfStepsY < 0) {
+                while (robot.getDirection() != UP) {
+                    robot.turnLeft();
+                }
+            for (int j = 0; j < Math.abs(numberOfStepsY); j++) {
+                robot.stepForward();
+            }
+
+            }
+        }
     }
 }
