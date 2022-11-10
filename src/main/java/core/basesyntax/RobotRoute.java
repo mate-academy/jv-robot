b3b06953ed3268eb954fd53184996ec65e74a813
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-11-10 20:42:49 +0200

    location values were declared. All methods were created

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,24 +3,68 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+
+
         int xLocation = robot.getX();
 
         int yLocation = robot.getY();
 
-        Direction direction = robot.getDirection();
 
-        System.out.println(direction);
 
-//        while (xLocation > toX) {
-//
-//
-//
-//        }
-//
-//        while (xLocation == toX && yLocation > toY) {
-//
-//
-//        }
+        if (xLocation > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+
+                robot.turnLeft();
+            }
+
+
+            for (int i = xLocation; i > toX; i--) {
+
+                robot.stepForward();
+
+            }
+        }
+
+        if (yLocation > toY) {
+
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+
+            for (int i = yLocation; i > toY; i--) {
+
+                    robot.stepForward();
+
+            }
+        }
+
+        if (xLocation < toX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+
+                robot.turnRight();
+            }
+
+
+            for (int i = xLocation; i < toX; i++) {
+
+                robot.stepForward();
+
+            }
+        }
+
+        if (yLocation < toY) {
+
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+
+            for (int i = yLocation; i < toY; i++) {
+
+                robot.stepForward();
+
+            }
+        }
+
 
     }
 }
