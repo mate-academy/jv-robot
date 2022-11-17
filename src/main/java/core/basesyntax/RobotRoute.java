753819dbd71d08e6dd72975f00bf8df9057e1f40
Tymofii Skrypko <skrypko270901@gmail.com>
2022-11-17 13:26:12 +0100

    completed jv-robot task

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,72 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if(toX > robot.getX()) {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    robot.turnRight();
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                default:
+                    break;
+            }
+        }
+        if(toX < robot.getX()) {
+            switch (robot.getDirection()) {
+                case RIGHT:
+                    robot.turnRight();
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                default:
+                    break;
+            }
+        }
+        while(robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if(toY < robot.getY()) {
+            switch (robot.getDirection()) {
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                case UP:
+                    robot.turnLeft();
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                default:
+                    break;
+            }
+        }
+        if(toY > robot.getY()) {
+            switch (robot.getDirection()) {
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case UP:
+                default:
+                    break;
+            }
+        }
+        while(robot.getY() != toY) {
+            robot.stepForward();
+        }
+        System.out.println("I don`t want to be moving anymore, I just want to be happy");
     }
 }
