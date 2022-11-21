2756d64ce3eb1e6352ace7b418e62f8e99955f00
Yevhenii Kilovyi <jenya.kilevoy@gmail.com>
2022-11-21 19:03:15 +0200

    third try of solution JV-Robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
+        //write your solution here
         if (robot.getX() < toX) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
