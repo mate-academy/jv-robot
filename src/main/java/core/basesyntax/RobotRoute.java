ca5246e555c77d06c0080d6f383b3dd9d8dadc30
Mykola Radzivon <kolian201497@gmail.com>
2022-12-05 23:44:47 +0200

    robot in the home task solved

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,39 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int numberX = robot.getX();
+        int numberY = robot.getY();
+        if (numberX > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+            for (int i = toX; i < numberX; i++) {
+                robot.stepForward();
+            }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            for (int i = numberX; i < toX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (numberY > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+            for (int i = toY; i < numberY; i++) {
+                robot.stepForward();
+            }
+        } else {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            for (int i = numberY; i < toY; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
+
