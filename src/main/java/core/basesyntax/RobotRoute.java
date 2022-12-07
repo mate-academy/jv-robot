7205a754c014d22f52144938ca0c9f0e3bf22f7b
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2022-12-08 00:51:35 +0200

    robot at home

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,33 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Robot bender = new Robot(Direction.UP, 5,5);
+        int stepX = 5;
+        for (int i = 0; stepX == toX; i++) {
+            if (toX > 5) {
+                bender.turnRight();
+                bender.stepForward();
+                stepX++;
+            }
+            if (toX < 5) {
+                bender.turnLeft();
+                bender.stepForward();
+                stepX--;
+            }
+        }
+        Robot newBender = new Robot(Direction.UP, stepX,5);
+        int stepY = 5;
+        for (int i = 0; stepY == toY; i++) {
+            if (toY > 5) {
+                bender.stepForward();
+                stepX++;
+            }
+            if (toX < 5) {
+                bender.turnLeft();
+                bender.turnLeft();
+                bender.stepForward();
+                stepX--;
+            }
+        }
     }
 }
