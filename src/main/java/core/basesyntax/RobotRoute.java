852eddb8f6fc77946f5f58708fe34eded3984b59
Taras Zhyravetskyi <tarik.zhyravetskyy@gmail.com>
2022-12-07 16:55:24 +0200

    moverobot method is completed and robot reaxhed home

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,24 @@
 package core.basesyntax;
 
+import org.jetbrains.annotations.NotNull;
+
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+    public void moveRobot(@NotNull Robot robot, int toX, int toY) {
+        Direction xdirection = toX > robot.getX() ? Direction.RIGHT : Direction.LEFT;
+        Direction ydirection = toY > robot.getY() ? Direction.UP : Direction.DOWN;
+
+        while (robot.getDirection() != xdirection) {
+            robot.turnRight();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        while (robot.getDirection() != ydirection) {
+            robot.turnRight();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+        System.out.println("Robot reached home");
     }
 }
