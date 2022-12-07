f155990bc051cebd3c575d32ad3001e63dcff89f
Polina Lysytska <nikolavna100500@gmail.com>
2022-12-07 23:40:15 +0200

    task was implemented

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,29 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        if (toX - robot.getX >= 0) {
+            while (robot.getDirection() != RIGHT) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != LEFT) {
+                robot.turnLeft();
+            }
+        }
+        while (toX != robot.getX) {
+            robot.stepForward();
+        }
+        if (toY - robot.getY >= 0) {
+            while (robot.getDirection() != UP) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != DOWN) {
+                robot.turnLeft();
+            }
+        }
+        while (toY != robot.getY) {
+            robot.stepForward();
+        }
     }
 }
