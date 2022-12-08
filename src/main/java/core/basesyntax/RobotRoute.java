10ebaa17591785573d48755ba94f3b93ae8d4e0a
Your NameMykola <mykola.osolinskyi@gmail.com>
2022-12-08 16:34:26 +0100

    welcome to the spot mvn clean package git status
    clear
    git remote
    mvn clean package
    ..
    >>

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX > robot.getX()) {
+            while (!robot.getDirection().equals(Direction.RIGHT)) {
+                robot.turnRight();
+            }
+        }
+
+        if (toX < robot.getX()) {
+            while (!robot.getDirection().equals(Direction.LEFT)) {
+                robot.turnLeft();
+            }
+        }
+
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        if (toY > robot.getY()) {
+            while (!robot.getDirection().equals(Direction.UP)) {
+                robot.turnLeft();
+            }
+        }
+
+        if (toY < robot.getY()) {
+            while (!robot.getDirection().equals(Direction.DOWN)) {
+                robot.turnLeft();
+            }
+        }
+
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
