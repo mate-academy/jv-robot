e373df449cf7b7b9d5c779e4b909d4c28d8a7a2f
Mik911 <911Sokol@gmail.com>
2022-12-14 15:20:45 +0200

    this is the my solution learning lesson JAVA CORE-OOP - jv-robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,41 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+
+        if(toX < robot.getX()) {
+            while(robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            while(toX != robot.getX()){
+                robot.stepForward();
+            }
+
+        }
+        else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (toX != robot.getX()) {
+                robot.stepForward();
+            }
+        }
+
+            if(toY < robot.getY()) {
+                while(robot.getDirection() != Direction.DOWN) {
+                    robot.turnLeft();
+                }
+                while(toY != robot.getY()){
+                    robot.stepForward();
+                }
+
+            }
+            else{
+                while(robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+                while(toY != robot.getY()){
+                    robot.stepForward();
+                }
+        }
     }
 }
