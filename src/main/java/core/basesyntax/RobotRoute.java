a1835dd885d0a37ef27aed2d2e3c3dbdff8be516
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2022-12-18 16:53:22 +0200

    RobotRoute v1.1 no optimal, working model

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -8,15 +8,13 @@ public class RobotRoute {
     }
 
     public void moveRobot(Robot robot, int toX, int toY) {
-        System.out.println("Direction " + robot.getDirection().name());
-        System.out.println("X= " + robot.getX() + " -> " + toX + ", Y = " + robot.getY() + " -> " + toY);
         if (robot.getX() > toX) {
             setRobotDirection(robot, "LEFT");
         } else setRobotDirection(robot, "RIGHT");
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-         if (robot.getY() < toY) {
+        if (robot.getY() < toY) {
             setRobotDirection(robot, "UP");
         } else {
             setRobotDirection(robot, "DOWN");
@@ -24,8 +22,5 @@ public class RobotRoute {
         while (robot.getY() != toY) {
             robot.stepForward();
         }
-        System.out.println("Direction " + robot.getDirection().name());
-        System.out.println("X= " + robot.getX() + " -> " + toX + ", Y = " + robot.getY() + " -> " + toY);
-        System.out.println("------------------------------------------------------");
     }
 }
