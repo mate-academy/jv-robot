6a016cf037367e80a7e4ee725f5aa5d7dbeb0a1b
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2022-12-18 16:49:23 +0200

    no optimal working model

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,31 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    public void setRobotDirection(Robot robot, String setDirection) {
+        while (!robot.getDirection().name().equals(setDirection)) {
+            robot.turnRight();
+        }
+    }
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        System.out.println("Direction " + robot.getDirection().name());
+        System.out.println("X= " + robot.getX() + " -> " + toX + ", Y = " + robot.getY() + " -> " + toY);
+        if (robot.getX() > toX) {
+            setRobotDirection(robot, "LEFT");
+        } else setRobotDirection(robot, "RIGHT");
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+         if (robot.getY() < toY) {
+            setRobotDirection(robot, "UP");
+        } else {
+            setRobotDirection(robot, "DOWN");
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+        System.out.println("Direction " + robot.getDirection().name());
+        System.out.println("X= " + robot.getX() + " -> " + toX + ", Y = " + robot.getY() + " -> " + toY);
+        System.out.println("------------------------------------------------------");
     }
 }
