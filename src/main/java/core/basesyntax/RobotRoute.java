014f582e88f9541644c8163c76b12cb7479bab39
Alexander Karpenko <karalexander@gmail.com>
2023-01-10 01:58:56 +0200

    created moveRobot method added blank Main class

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,104 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int differenceX = robot.getX() - toX;
+        int differenceY = robot.getY() - toY;
+        int diffX = Math.abs(robot.getX() - toX);
+        int diffY = Math.abs(robot.getY() - toY);
+
+        if (differenceX > 0) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN: case RIGHT:
+                    while (!robot.getDirection().equals(Direction.LEFT)) {
+                        robot.turnRight();
+                    }
+                    break;
+                case LEFT: default:
+                    break;
+            }
+            for (int i = 0; i < diffX; i++) {
+                robot.stepForward();
+            }
+
+            if (differenceY > 0) {
+                robot.turnLeft();
+                for (int i = 0; i < diffY; i++) {
+                    robot.stepForward();
+                }
+            } else if (differenceY < 0) {
+                robot.turnRight();
+                for (int i = 0; i < diffY; i++) {
+                    robot.stepForward();
+                }
+            }
+        } else if (differenceX < 0) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN: case LEFT:
+                    while (!robot.getDirection().equals(Direction.RIGHT)) {
+                        robot.turnLeft();
+                    }
+                    break;
+                case RIGHT: default:
+                    break;
+            }
+            for (int i = 0; i < diffX; i++) {
+                robot.stepForward();
+            }
+
+            if (differenceY > 0) {
+                robot.turnRight();
+                for (int i = 0; i < diffY; i++) {
+                    robot.stepForward();
+                }
+            } else if (differenceY < 0) {
+                robot.turnLeft();
+                for (int i = 0; i < diffY; i++) {
+                    robot.stepForward();
+                }
+            }
+
+        } else {
+            if (differenceY < 0) {
+                switch (robot.getDirection()) {
+                    case LEFT:
+                        robot.turnRight();
+                        break;
+                    case DOWN: case RIGHT:
+                        while (!robot.getDirection().equals(Direction.LEFT)) {
+                            robot.turnLeft();
+                        }
+                        break;
+                    case UP: default:
+                        break;
+                }
+                for (int i = 0; i < diffY; i++) {
+                    robot.stepForward();
+                }
+            } else if (differenceY > 0) {
+                switch (robot.getDirection()) {
+                    case RIGHT:
+                        robot.turnLeft();
+                        break;
+                    case UP: case LEFT:
+                        while (!robot.getDirection().equals(Direction.RIGHT)) {
+                            robot.turnLeft();
+                        }
+                        break;
+                    case DOWN: default:
+                        break;
+                }
+                for (int i = 0; i < diffY; i++) {
+                    robot.stepForward();
+                }
+            }
+        }
     }
 }
