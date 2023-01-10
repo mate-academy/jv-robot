98dadff5f9494c2444b7155c9a3dadd5624c2cce
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-01-10 14:14:02 +0200

    Robot reached his goal

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,68 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        if (toX > robot.getX()) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                default:
+                    break; }
+        } else if (toX < robot.getX()) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        if (toY < robot.getY()) {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                default:
+                    break; }
+        } else if (toY > robot.getY()) {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
+
+
+
