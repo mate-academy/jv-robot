d2120e981c9b8e334b66b3b0ebf674c7ac7fae68
Alexander Karpenko <karalexander@gmail.com>
2023-01-11 21:53:15 +0200

    created moveRobot method added blank Main class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,6 +5,6 @@ public class Main {
         /*Robot robot = new Robot(Direction.LEFT, 1, 1);
         RobotRoute route = new RobotRoute();
         route.moveRobot(robot, 7, -3);
-          */
+        System.out.println(robot.getX());*/
     }
 }
