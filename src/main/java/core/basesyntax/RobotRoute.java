0e6f9885c69cf5844044c5034e7cedda928b8800
Maxim Skory <devisrush@gmail.com>
2023-01-13 00:37:29 +0200

    turnToRightXDirection() and turnToRightYDirection() methods were added. getDistance() method was fixed.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,13 +2,80 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        //write your solution here
     }
 
     public int getDistance(int positionCoordinate, int toCoordinate) {
         if (positionCoordinate < 0 ^ toCoordinate < 0) {
             return Math.abs(positionCoordinate) + Math.abs(toCoordinate);
         }
-        return toCoordinate - positionCoordinate;
+        return Math.abs(toCoordinate - positionCoordinate);
+    }
+
+    public void turnToRightYDirection(Robot robot, int toY) {
+        if (robot.getY() < toY) {
+            switch (robot.getDirection()) {
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            switch (robot.getDirection()) {
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case UP:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void turnToRightXDirection(Robot robot, int toX) {
+        if (robot.getX() < toX) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
