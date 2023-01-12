bba48af7de2da4b0aa4b9763f9934c453b70f340
Maxim Skory <devisrush@gmail.com>
2023-01-12 23:25:57 +0200

    getDistance() method was added

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+    }
+
+    public int getDistance(int positionCoordinate, int toCoordinate) {
+        if (positionCoordinate < 0 ^ toCoordinate < 0) {
+            return Math.abs(positionCoordinate) + Math.abs(toCoordinate);
+        }
+        return toCoordinate - positionCoordinate;
     }
 }
