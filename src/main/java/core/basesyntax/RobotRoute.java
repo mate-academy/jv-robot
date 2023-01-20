e78ecf367612842e4d919893e165b404c8bf6d13
Serhii Shyian <serhii.shiyan@ukr.net>
2023-01-20 11:40:52 +0200

    solved the problem with code verification on branch hw-3-solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,24 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() != toX) {
+            while (robot.getDirection() != ((robot.getX() < toX) ? Direction.RIGHT
+                    : Direction.LEFT)) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() != toY) {
+            while (robot.getDirection() != ((robot.getY() < toY) ? Direction.UP
+                    : Direction.DOWN)) {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
