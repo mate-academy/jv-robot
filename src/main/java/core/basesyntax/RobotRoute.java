41803b10c91fffc4ab51123c8dab81ec5727ea4b
Kostyantyn Savchenko <kostyantyn.savchenko@gmail.com>
2023-01-28 01:18:29 +0200

    Done

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -8,91 +8,33 @@ public class RobotRoute {
 
     public void moveRobot(@NotNull Robot robot, int toX, int toY) {
         //write your solution here
-        Direction direction = robot.getDirection();
-        int coordinateX = robot.getX();
-        int coordinateY = robot.getY();
-        if (direction == UP && coordinateX < toX) {
-            robot.turnRight();
-//            direction = RIGHT;
+        while (robot.getX() < toX) {
+            if (robot.getDirection() != RIGHT) {
+                robot.turnRight();
+            } else {
+                robot.stepForward();
+            }
         }
-        if (direction == DOWN && coordinateX < toX) {
-            robot.turnLeft();
-        }
-        if (direction == RIGHT && coordinateX < toX) {
-            robot.stepForward();
-        }
-        if (direction == LEFT && coordinateX < toX) {
-            robot.turnRight();
-            robot.turnRight();
-        }
-        while (coordinateX < toX) {
-            robot.turnRight();
-            robot.stepForward();
-            coordinateX++;
-        }
-            if (direction == UP && coordinateX > toX) {
+        while (robot.getX() > toX) {
+            if (robot.getDirection() != LEFT) {
                 robot.turnLeft();
-            }
-            if (direction == DOWN && coordinateX > toX) {
-                robot.turnRight();
-            }
-            if (direction == LEFT && coordinateX > toX) {
+            } else {
                 robot.stepForward();
-            }
-            if (direction == RIGHT && coordinateX > toX) {
-                robot.turnRight();
-                robot.turnRight();
-            }
-            while (coordinateX > toX) {
-                robot.turnLeft();
-                robot.stepForward();
-                coordinateX--;
-            }
-            if (direction == UP && coordinateY < toY) {
-                robot.stepForward();
-            }
-            if (direction == DOWN && coordinateY < toY) {
-                robot.turnLeft();
-            }
-            if (direction == RIGHT && coordinateY < toY) {
-                robot.turnLeft();
-            }
-            if (direction == LEFT && coordinateY < toY) {
-                robot.turnRight();
-            }
-            while (coordinateY < toY) {
-                robot.stepForward();
-                coordinateY++;
-                if (coordinateY == toY) {
-//                    coordinateY = toY;
-                    break;
-                }
-            }
-            if (direction == UP && coordinateY > toY) {
-                robot.stepForward();
-            }
-            if (direction == DOWN && coordinateY > toY) {
-                robot.turnRight();
-                robot.turnRight();
-            }
-            if (direction == LEFT && coordinateY > toY) {
-                robot.turnRight();
-            }
-            if (direction == RIGHT && coordinateY > toY) {
-                robot.turnLeft();
-            }
-            while (coordinateY > toY) {
-                robot.stepForward();
-                coordinateY--;
-                }
             }
         }
-
-
-
-
-
-
-
-
-
+        while (robot.getY() < toY) {
+            if (robot.getDirection() != UP) {
+                robot.turnRight();
+            } else {
+                robot.stepForward();
+            }
+        }
+        while (robot.getY() > toY) {
+            if (robot.getDirection() != DOWN) {
+                robot.turnLeft();
+            } else {
+                robot.stepForward();
+            }
+        }
+    }
+}
