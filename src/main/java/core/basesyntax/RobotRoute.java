ce4b75e3ebb2238f0250eb26c2be19a3397240bb
Kostyantyn Savchenko <kostyantyn.savchenko@gmail.com>
2023-01-28 01:23:51 +0200

    Done

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,35 +2,33 @@ package core.basesyntax;
 
 import org.jetbrains.annotations.NotNull;
 
-import static core.basesyntax.Direction.*;
-
 public class RobotRoute {
 
     public void moveRobot(@NotNull Robot robot, int toX, int toY) {
         //write your solution here
         while (robot.getX() < toX) {
-            if (robot.getDirection() != RIGHT) {
+            if (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             } else {
                 robot.stepForward();
             }
         }
         while (robot.getX() > toX) {
-            if (robot.getDirection() != LEFT) {
+            if (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             } else {
                 robot.stepForward();
             }
         }
         while (robot.getY() < toY) {
-            if (robot.getDirection() != UP) {
+            if (robot.getDirection() != Direction.UP) {
                 robot.turnRight();
             } else {
                 robot.stepForward();
             }
         }
         while (robot.getY() > toY) {
-            if (robot.getDirection() != DOWN) {
+            if (robot.getDirection() != Direction.DOWN) {
                 robot.turnLeft();
             } else {
                 robot.stepForward();
