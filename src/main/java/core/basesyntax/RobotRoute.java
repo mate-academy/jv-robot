34229cc0b3f16e29632b29cfdd76c1cea6d13ad3
Skrudj <ssinilov@gmail.com>
2023-02-05 12:08:17 +0200

    implemendet moveRobot method and wrote describtion to logic

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,40 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // x
+        int subtractionX = robot.getX() - toX;
+        if (subtractionX < 0) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < Math.abs(subtractionX); i++) {
+                robot.stepForward();
+            }
+        } else if (subtractionX > 0) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < subtractionX; i++) {
+                robot.stepForward();
+            }
+        }
+
+        // y
+        int subtractionY = robot.getY() - toY;
+        if (subtractionY < 0) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < Math.abs(subtractionY); i++) {
+                robot.stepForward();
+            }
+        } else if (subtractionY > 0) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < subtractionY; i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
