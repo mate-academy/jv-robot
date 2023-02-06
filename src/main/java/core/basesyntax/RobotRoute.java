3035f4a648584bab71abcf86d079bcbb6fd44c9c
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-02-07 00:01:28 +0200

    The movement of the robot to the required coordinates is organized

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,42 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        while (robot.getX() != toX && robot.getY() != toY) {
+            int x = robot.getX();
+            int y = robot.getY();
+            if (x > toX) {
+                if (!robot.getDirection().equals(Direction.LEFT)) {
+                    robot.turnLeft();
+                }
+                if (robot.getDirection().equals(Direction.LEFT)) {
+                    robot.stepForward();
+                }
+            }
+            if (x < toX) {
+                if (!robot.getDirection().equals(Direction.RIGHT)) {
+                    robot.turnLeft();
+                }
+                if (robot.getDirection().equals(Direction.RIGHT)) {
+                    robot.stepForward();
+                }
+            }
+            if (y < toY) {
+                if (!robot.getDirection().equals(Direction.UP)) {
+                    robot.turnRight();
+                }
+                if (robot.getDirection().equals(Direction.UP)) {
+                    robot.stepForward();
+                }
+            }
+            if (y > toY) {
+                if (!robot.getDirection().equals(Direction.DOWN)) {
+                    robot.turnRight();
+                }
+                if (robot.getDirection().equals(Direction.DOWN)) {
+                    robot.stepForward();
+                }
+            }
+        }
     }
 }
