9ce2aafd6945b1fda9d51dc07ff9968fa838e952
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-06 22:08:53 +0200

    Solution for walk home was added

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,33 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction wolkDirection;
+        if (robot.getX() > toX) {
+            wolkDirection = Direction.LEFT;
+        } else {
+            wolkDirection = Direction.RIGHT;
+        }
+        turnRobotUntil(wolkDirection, robot);
+        while ((robot.getX() != toX)) {
+            robot.stepForward();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        if (robot.getY() > toY) {
+            wolkDirection = Direction.DOWN;
+        } else {
+            wolkDirection = Direction.UP;
+        }
+        turnRobotUntil(wolkDirection, robot);
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    public void turnRobotUntil(Direction d, Robot r) {
+        while (d != r.getDirection()) {
+            r.turnLeft();
+        }
     }
 }
