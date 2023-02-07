daadcc4289644fe7241bbf0df8837932bb8c4b5b
ivan <kiohi2000@gmail.com>
2023-02-07 17:44:04 +0200

    the robot has found its home

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,32 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() < toX) {
+            turn(robot, Direction.RIGHT);
+        } else {
+            turn(robot, Direction.LEFT);
+        }
+
+        int distanceX = Math.abs(toX - robot.getX());
+        for (int i = 0; i < distanceX; i++) {
+            robot.stepForward();
+        }
+
+        if (robot.getY() < toY) {
+            turn(robot, Direction.UP);
+        } else {
+            turn(robot, Direction.DOWN);
+        }
+
+        int distanceY = Math.abs(toY - robot.getY());
+        for (int i = 0; i < distanceY; i++) {
+            robot.stepForward();
+        }
+    }
+
+    private void turn(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnLeft();
+        }
     }
 }
