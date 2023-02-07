5f0277558e13de995c9a5cbdefc5b838e5da80bf
De1eF <jatfun58@gmail.com>
2023-02-07 19:55:23 +0200

    Robot now finds his way home, with full logs

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,42 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        System.out.println("Final destination: " + toX + " : " + toY);
+        Direction requiredDir = Direction.UP;
+        boolean turnedCorrectWay;
+        int cycle = 0;
+
+        while (robot.getX() != toX || robot.getY() != toY) {
+            cycle++;
+            System.out.println();
+            System.out.println("cycle: " + cycle);
+            int currentX = robot.getX();
+            int currentY = robot.getY();
+            Direction currentDir = robot.getDirection();
+            System.out.println("Current X: " + currentX
+                    + "/n"
+                    + "Current Y: " + currentY
+                    + "/n"
+                    + "Current direction: " + currentDir);
+            if (currentX > toX) {
+                requiredDir = Direction.LEFT;
+            } else if (currentX < toX) {
+                requiredDir = Direction.RIGHT;
+            } else if (currentY > toY) {
+                requiredDir = Direction.DOWN;
+            } else if (currentY < toY) {
+                requiredDir = Direction.UP;
+            }
+            if (currentDir != requiredDir) {
+                robot.turnLeft();
+                turnedCorrectWay = false;
+            } else {
+                turnedCorrectWay = true;
+            }
+            if (turnedCorrectWay) {
+                robot.stepForward();
+            }
+        }
+        System.out.println("Destination reached");
     }
 }
