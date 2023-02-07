829f47e132dce18a49ab6954023435809fb31404
De1eF <jatfun58@gmail.com>
2023-02-07 20:14:22 +0200

    Single complex method has been split into smaller ones

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,42 +2,53 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        System.out.println("Final destination: " + toX + " : " + toY);
-        Direction requiredDir = Direction.UP;
-        boolean turnedCorrectWay;
+        Direction requiredDir;
         int cycle = 0;
 
+        System.out.println("\nFinal destination: " + toX + " : " + toY);
+
         while (robot.getX() != toX || robot.getY() != toY) {
-            cycle++;
-            System.out.println();
-            System.out.println("cycle: " + cycle);
-            int currentX = robot.getX();
-            int currentY = robot.getY();
-            Direction currentDir = robot.getDirection();
-            System.out.println("Current X: " + currentX
-                    + "/n"
-                    + "Current Y: " + currentY
-                    + "/n"
-                    + "Current direction: " + currentDir);
-            if (currentX > toX) {
-                requiredDir = Direction.LEFT;
-            } else if (currentX < toX) {
-                requiredDir = Direction.RIGHT;
-            } else if (currentY > toY) {
-                requiredDir = Direction.DOWN;
-            } else if (currentY < toY) {
-                requiredDir = Direction.UP;
-            }
-            if (currentDir != requiredDir) {
-                robot.turnLeft();
-                turnedCorrectWay = false;
-            } else {
-                turnedCorrectWay = true;
-            }
-            if (turnedCorrectWay) {
+            //logging cycle and robot state
+            log(robot, cycle);
+            //move robot
+            requiredDir = findCorrectDirection(robot.getX(), robot.getY(), toX, toY);
+            if (turnToRequiredDirection(robot,requiredDir)) {
                 robot.stepForward();
             }
         }
         System.out.println("Destination reached");
+    }
+
+    private Direction findCorrectDirection(int currentX, int currentY, int toX, int toY) {
+        if (currentX > toX) {
+            return Direction.LEFT;
+        } else if (currentX < toX) {
+            return Direction.RIGHT;
+        } else if (currentY > toY) {
+            return Direction.DOWN;
+        } else if (currentY < toY) {
+            return Direction.UP;
+        } else {
+            return null;
+        }
+    }
+
+    private boolean turnToRequiredDirection(Robot robot, Direction requiredDir) {
+        if (robot.getDirection() != requiredDir) {
+            robot.turnLeft();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void log(Robot robot, int cycle) {
+        cycle++;
+        System.out.println("\ncycle: " + cycle);
+        System.out.println("Current X: " + robot.getX()
+                + "/n"
+                + "Current Y: " + robot.getY()
+                + "/n"
+                + "Current direction: " + robot.getDirection());
     }
 }
