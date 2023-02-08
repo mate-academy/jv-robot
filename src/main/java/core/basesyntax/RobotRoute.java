093f423af8dd412f00aa731ec76d0e8dcb709f58
Maksym <maxim.yashyn@gmail.com>
2023-02-08 22:52:38 +0200

    implement better solution in moveRobot method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -8,30 +8,38 @@ public class RobotRoute {
             while (robot.getDirection() == Direction.LEFT) {
                 robot.turnLeft();
             }
-            while (startX == toX) {
-                robot.stepForward();
+            if (robot.getDirection() == Direction.LEFT) {
+                while (startX == toX) {
+                    robot.stepForward();
+                }
             }
         } else {
             while (robot.getDirection() == Direction.RIGHT) {
                 robot.turnRight();
             }
-            while (startX == toX) {
-                robot.stepForward();
+            if (robot.getDirection() == Direction.RIGHT) {
+                while (startX == toX) {
+                    robot.stepForward();
+                }
             }
         }
         if (startY > toY) {
             while (robot.getDirection() == Direction.UP) {
                 robot.turnLeft();
             }
-            while (startY == toY) {
-                robot.stepForward();
+            if (robot.getDirection() == Direction.UP) {
+                while (startY == toY) {
+                    robot.stepForward();
+                }
             }
         } else {
             while (robot.getDirection() == Direction.DOWN) {
                 robot.turnLeft();
             }
-            while (startY == toY) {
-                robot.stepForward();
+            if (robot.getDirection() == Direction.DOWN) {
+                while (startY == toY) {
+                    robot.stepForward();
+                }
             }
         }
     }
