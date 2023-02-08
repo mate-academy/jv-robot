cad3636f138ba7eb84e6f26697cfc56b03bc26a7
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-02-08 11:41:28 +0200

    Realize moveRobot, create method changeDirection, create methods goToCorrectX, goToCorrectY

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,35 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        goToCorrectX(robot, toX);
+        goToCorrectY(robot, toY);
+    }
+
+    public void changeDirection(Direction correctDirection, Robot robot) {
+        while (robot.getDirection() != correctDirection){
+            robot.turnRight();
+        }
+    }
+
+    public void goToCorrectX(Robot robot, int toX) {
+        if (toX < robot.getX()) {
+            changeDirection(Direction.LEFT, robot);
+        } else {
+            changeDirection(Direction.RIGHT, robot);
+        }
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+    }
+
+    public void goToCorrectY(Robot robot, int toY) {
+        if (toY < robot.getY()) {
+            changeDirection(Direction.DOWN, robot);
+        } else {
+            changeDirection(Direction.UP, robot);
+        }
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
