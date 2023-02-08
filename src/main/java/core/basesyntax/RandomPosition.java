67e11b708ed0a58db1de61ac66ce074f41d372c2
Maksym <maxim.yashyn@gmail.com>
2023-02-08 22:14:16 +0200

    implement getRandomStartEndX, getRandomStartEndY methods

--- src/main/java/core/basesyntax/RandomPosition.java
+++ src/main/java/core/basesyntax/RandomPosition.java
@@ -3,12 +3,20 @@ package core.basesyntax;
 import java.util.Random;
 
 public class RandomPosition {
-    public int getRandomX() {
-        return new Random().nextInt(100);
+    public int getRandomStartX() {
+        return new Random().nextInt(10);
     }
 
-    public int getRandomY() {
-        return new Random().nextInt(100);
+    public int getRandomEndX() {
+        return new Random().nextInt(10);
+    }
+
+    public int getRandomStartY() {
+        return new Random().nextInt(10);
+    }
+
+    public int getRandomEndY() {
+        return new Random().nextInt(10);
     }
 
     public Direction getRandomDirection() {
