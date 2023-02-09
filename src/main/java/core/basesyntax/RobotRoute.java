02965cea3d2fe9fa9b005e66a5941ee79ac8bb11
Oleksandr Lutsenko <sashagrek03@gmail.com>
2023-02-09 12:47:58 +0200

    added decomposition

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,20 +2,11 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (robot.getX() > toX) {
-            while (robot.getDirection() != Direction.LEFT) {
-                robot.turnLeft();
-            }
-        } else {
-            while (robot.getDirection() != Direction.RIGHT) {
-                robot.turnLeft();
-            }
-        }
+        robotMovesHorizontal(robot, toX, toY);
+        robotMovesVertical(robot, toX, toY);
+    }
 
-        while (robot.getX() != toX) {
-            robot.stepForward();
-        }
-
+    private void robotMovesVertical(Robot robot, int toX, int toY) {
         if (robot.getY() > toY) {
             while (robot.getDirection() != Direction.DOWN) {
                 robot.turnLeft();
@@ -27,6 +18,22 @@ public class RobotRoute {
         }
 
         while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    public void robotMovesHorizontal(Robot robot, int toX, int toY) {
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnLeft();
+            }
+        }
+
+        while (robot.getX() != toX) {
             robot.stepForward();
         }
     }
