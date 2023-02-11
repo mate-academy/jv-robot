fc7522734bf39045b37e5202cd86ade34e433ebf
Yaroslav Chornyi <chorniy.j@gmail.com>
2023-02-11 16:50:20 +0200

    Solve robot route ex with few loop

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,45 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+
+    public void turnRightTwoTimes() {
+
     }
-}
+
+
+    public void moveRobot(Robot robot, int toX, int toY) {
+        //write your solution here
+        while (robot.getY() != toY) {
+            while (!robot.getDirection().equals(Direction.DOWN) && robot.getY() > toY) {
+                robot.turnLeft();
+            }
+            while (!robot.getDirection().equals(Direction.UP) && robot.getY() < toY) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < Math.abs(robot.getY() - toY); i++) {
+                robot.stepForward();
+
+            }
+
+
+        }
+
+        while (robot.getX() != toX) {
+            while ((!robot.getDirection().equals(Direction.LEFT)) && robot.getX() > toX ) {
+                robot.turnLeft();
+
+            }
+           while ((!robot.getDirection().equals(Direction.RIGHT)) && robot.getX() < toX ) {
+               robot.turnRight();
+           }
+                for (int i = 0; i < Math.abs(robot.getX() - toX); i++) {
+                   robot.stepForward();
+
+               }
+
+                }
+            }
+        }
+
+
+
