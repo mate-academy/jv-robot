9ff2e9dc0a1adc2a368760af9b4db103cb3afbdb
Yaroslav Chornyi <chorniy.j@gmail.com>
2023-02-11 16:58:05 +0200

    reformat my cod to the current code style

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,13 +2,7 @@ package core.basesyntax;
 
 public class RobotRoute {
 
-    public void turnRightTwoTimes() {
-
-    }
-
-
     public void moveRobot(Robot robot, int toX, int toY) {
-        //write your solution here
         while (robot.getY() != toY) {
             while (!robot.getDirection().equals(Direction.DOWN) && robot.getY() > toY) {
                 robot.turnLeft();
@@ -18,28 +12,21 @@ public class RobotRoute {
             }
             for (int i = 0; i < Math.abs(robot.getY() - toY); i++) {
                 robot.stepForward();
-
             }
-
-
         }
-
         while (robot.getX() != toX) {
-            while ((!robot.getDirection().equals(Direction.LEFT)) && robot.getX() > toX ) {
+            while ((!robot.getDirection().equals(Direction.LEFT)) && robot.getX() > toX) {
                 robot.turnLeft();
-
             }
-           while ((!robot.getDirection().equals(Direction.RIGHT)) && robot.getX() < toX ) {
-               robot.turnRight();
-           }
-                for (int i = 0; i < Math.abs(robot.getX() - toX); i++) {
-                   robot.stepForward();
-
-               }
-
-                }
+            while ((!robot.getDirection().equals(Direction.RIGHT)) && robot.getX() < toX) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < Math.abs(robot.getX() - toX); i++) {
+                robot.stepForward();
             }
         }
+    }
+}
 
 
 
