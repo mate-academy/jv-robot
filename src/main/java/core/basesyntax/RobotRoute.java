5398e208422e349a329e0994e4d7006a2eb58835
Roman Yuskevych <yuskevych@gmail.com>
2023-02-12 19:18:52 +0200

    added jv-robot homework solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,46 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                if (robot.getDirection() == Direction.UP) {
+                    robot.turnRight();
+                } else {
+                    robot.turnLeft();
+                }
+            }
+        } else if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                if (robot.getDirection() == Direction.DOWN) {
+                    robot.turnRight();
+                } else {
+                    robot.turnLeft();
+                }
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        if (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.UP) {
+                if (robot.getDirection() == Direction.LEFT) {
+                    robot.turnRight();
+                } else {
+                    robot.turnLeft();
+                }
+            }
+        } else if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                if (robot.getDirection() == Direction.RIGHT) {
+                    robot.turnRight();
+                } else {
+                    robot.turnLeft();
+                }
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
