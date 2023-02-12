f837a9a3a891221f286d48a849e203224de79f37
Nikita <nsalogub96@gmail.com>
2023-02-12 19:17:23 +0200

    implementation of robot task

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,37 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        getCorrectX(robot, toX);
+        getCorrectY(robot, toY);
+    }
+
+    public void getCorrectX(Robot robot, int toX) {
+        if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        }
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+    }
+
+    public void getCorrectY(Robot robot, int toY) {
+        if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else if (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        }
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
