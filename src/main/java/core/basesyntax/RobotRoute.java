4899cb367e1c55ae654236aacfd985d4cab41987
Nikita Salogub <120669194+Nikitos787@users.noreply.github.com>
2023-02-12 22:49:26 +0200

    Update RobotRoute.java

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (robot.getX() > toX) {
+        if (robot.getX() < toX) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
