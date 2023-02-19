c4346f58383a9f7fe14ef6dd11b5c32708e229c1
dreo <mishkin878@gmail.com>
2023-02-19 15:21:38 +0200

    Revert "finished"
    This reverts commit 508f03152ab8c76aaae29c559b728d30cb76369f.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,36 +3,5 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
-        int currentX = robot.getX();
-        int currentY = robot.getY();
-        if (toX < 0) {
-            robot.turnRight();
-        } else {
-            robot.turnLeft();
-        }
-        if (currentX > toX) {
-            for (int x = currentX; x > toX; x--) {
-                robot.stepForward();
-            }
-        }  else {
-            for (int x = currentX; x < toX; x++) {
-                robot.stepForward();
-            }
-        }
-
-    if (toY<currentY) {
-        robot.turnLeft();
-    } else {
-        robot.turnRight();
-    }
-        if (currentY > toY) {
-            for (int y = currentY; y > toY; y--) {
-                robot.stepForward();
-            }
-        }  else {
-            for (int y = currentY; y < toY; y++) {
-                robot.stepForward();
-            }
-        }
     }
 }
