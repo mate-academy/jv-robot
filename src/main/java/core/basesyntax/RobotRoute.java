01741ec09552d838276e8072d166ae5dff197b1b
Alex-Solod <solodyhin_2@icloud.com>
2023-02-23 14:07:35 +0200

    Create solution for moveRobot method in RobotRoute class.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,33 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+
+        int diffX = toX - currentX;
+        int diffY = toY - currentY;
+
+        while (diffX != 0 || diffY != 0 ) {
+            if (diffX > 0) {
+                robot.turnRight();
+                robot.stepForward();
+                diffX--;
+            }
+            else if (diffX < 0) {
+                robot.turnLeft();
+                robot.stepForward();
+                diffY++;
+            }
+
+            if (diffY > 0) {
+                robot.stepForward();
+                diffX--;
+            }
+            else if (diffY < 0) {
+                robot.turnLeft();
+                robot.stepForward();
+                diffY++;
+            }
+        }
     }
 }
