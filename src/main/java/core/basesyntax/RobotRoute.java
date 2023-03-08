0753424bb0d7a4b034ff3cd8c61f8242c6f4314e
Alex Drapikovsky <drapikovskyan@gmail.com>
2023-03-08 23:11:17 +0200

    all test passed

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,21 +3,16 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         Direction direction = robot.getX() < toX ? Direction.RIGHT : Direction.LEFT;
-
         while (robot.getX() != toX && robot.getDirection() != direction) {
             robot.turnLeft();
         }
-
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-
         direction = robot.getY() < toY ? Direction.UP : Direction.DOWN;
-
         while (robot.getY() != toY && robot.getDirection() != direction) {
             robot.turnLeft();
         }
-
         while (robot.getY() != toY) {
             robot.stepForward();
         }
