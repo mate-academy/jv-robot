7e91d5ad350f54758639a87fc99d6d69c60af5f7
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-03-09 15:43:31 +0300

    fixed cycles for the method to work correctly

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -4,33 +4,33 @@ public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
         if (robot.getY() < toY) {
-            do {
+            while (robot.getDirection() != Direction.UP){
                 robot.turnRight();
-            } while (robot.getDirection().equals(Direction.UP));
+            }
             for (int i = robot.getY(); i < toY ; i++) {
                 robot.stepForward();
             }
         }
         if (robot.getY() > toY) {
-            do {
+            while (robot.getDirection() != Direction.DOWN){
                 robot.turnRight();
-            } while (robot.getDirection().equals(Direction.DOWN));
+            }
             for (int i = robot.getY(); i > toY ; i--) {
                 robot.stepForward();
             }
         }
         if (robot.getX() < toX) {
-            do {
+            while (robot.getDirection() != Direction.RIGHT){
                 robot.turnRight();
-            } while (robot.getDirection().equals(Direction.RIGHT));
+            }
             for (int i = robot.getX(); i < toX ; i++) {
                 robot.stepForward();
             }
         }
         if (robot.getX() > toX) {
-            do {
+            while (robot.getDirection() != Direction.LEFT){
                 robot.turnRight();
-            } while (robot.getDirection().equals(Direction.LEFT));
+            }
             for (int i = robot.getX(); i > toX ; i--) {
                 robot.stepForward();
             }
