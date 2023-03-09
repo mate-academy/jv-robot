699669066ff276ae073ee2616bbbf744340e1b7b
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-03-09 17:07:14 +0300

    Update RobotRoute.java
    Some chenges in main()

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -4,34 +4,34 @@ public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
         if (robot.getY() < toY) {
-            while (robot.getDirection() != Direction.UP){
+            while (robot.getDirection() != Direction.UP) {
                 robot.turnRight();
             }
-            for (int i = robot.getY(); i < toY ; i++) {
+            while (robot.getY() != toY){
                 robot.stepForward();
             }
         }
         if (robot.getY() > toY) {
-            while (robot.getDirection() != Direction.DOWN){
+            while (robot.getDirection() != Direction.DOWN) {
                 robot.turnRight();
             }
-            for (int i = robot.getY(); i > toY ; i--) {
+            while (robot.getY() != toY){
                 robot.stepForward();
             }
         }
         if (robot.getX() < toX) {
-            while (robot.getDirection() != Direction.RIGHT){
+            while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-            for (int i = robot.getX(); i < toX ; i++) {
+            while (robot.getX() != toX){
                 robot.stepForward();
             }
         }
         if (robot.getX() > toX) {
-            while (robot.getDirection() != Direction.LEFT){
+            while (robot.getDirection() != Direction.LEFT) {
                 robot.turnRight();
             }
-            for (int i = robot.getX(); i > toX ; i--) {
+            while (robot.getX() != toX){
                 robot.stepForward();
             }
         }
