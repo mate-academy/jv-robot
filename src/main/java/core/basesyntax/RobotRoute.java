86bea63af08124671ed1ae4deeddf47bf1ab1e86
Nikita Sachik <nikitasachik@gmail.com>
2023-03-13 11:47:10 +0200

    It works, but need to fix some things

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,43 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        if (toX == robot.getX() && toY == robot.getY()) {
+            return;
+        }
+        int xStart = robot.getX();
+        int yStart = robot.getY();
+
+        if (toX == robot.getX() && toY == robot.getY()) {
+            return;
+        }
+
+        robot.stepForward();
+        if (robot.getX() != xStart) {
+            if (Math.abs(toX - robot.getX()) > Math.abs(xStart - toX)) {
+                robot.turnRight();
+                robot.turnRight();
+            }
+            for (; toX != robot.getX(); ) {
+                robot.stepForward();
+            }
+            robot.turnRight();
+        }
+
+        if (Math.abs(toY - robot.getY()) > Math.abs(yStart - toY)) {
+            robot.turnRight();
+            robot.turnRight();
+        }
+        for (; toY != robot.getY(); ) {
+            robot.stepForward();
+        }
+
+        robot.turnRight();
+            if (Math.abs(toX - robot.getX()) > Math.abs(xStart - toX)) {
+                robot.turnRight();
+                robot.turnRight();
+            }
+            for (; toX != robot.getX(); ) {
+                robot.stepForward();
+            }
     }
 }
