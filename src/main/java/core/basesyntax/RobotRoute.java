fd7b4c925e1ac22a21fc0631d616c41acca6e1ce
Maxym <maxi2ua@gmail.com>
2023-03-15 03:57:15 +0200

    add three methods to RobotRoute class, one for horizintal moving, second for vertical moving and last one for changing direction

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,43 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveHorizontally(robot, toX);
+        moveVertically(robot, toY);
+    }
+
+    private void moveHorizontally(Robot robot, int toX) {
+        if (robot.getX() > toX) {
+            turnIfFacingDirection(robot, Direction.RIGHT, Direction.UP, Direction.DOWN);
+        } else if (robot.getX() < toX) {
+            turnIfFacingDirection(robot, Direction.LEFT, Direction.UP, Direction.DOWN);
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveVertically(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            turnIfFacingDirection(robot, Direction.UP, Direction.RIGHT, Direction.LEFT);
+        } else if (robot.getY() < toY) {
+            turnIfFacingDirection(robot, Direction.DOWN, Direction.RIGHT, Direction.LEFT);
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private void turnIfFacingDirection(Robot robot, Direction direction,
+                                       Direction leftDirection, Direction rightDirection) {
+        if (robot.getDirection() == direction) {
+            robot.turnRight();
+            robot.turnRight();
+        } else if (robot.getDirection() == leftDirection) {
+            robot.turnLeft();
+        } else if (robot.getDirection() == rightDirection) {
+            robot.turnRight();
+        }
     }
 }
+
+
