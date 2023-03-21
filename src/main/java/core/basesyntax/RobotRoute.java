627c113ee30750ab8d10eec20447d10e692e5ec4
Serhii Serdiuk <serhii.serdiuk@gmail.com>
2023-03-22 00:19:46 +0100

    Change robotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -6,89 +6,28 @@ public class RobotRoute {
             System.out.println("Robot on the position");
         }
         if (robot.getX() > toX) {
-            switch (robot.getDirection()) {
-                case UP:
-                    robot.turnLeft();
-                    break;
-                case DOWN:
-                    robot.turnRight();
-                    break;
-                case RIGHT:
-                    for (int i = 0; i <= 2; i++) {
-                        robot.turnRight();
-                    }
-                    break;
-                default:
-                    break;
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
             }
-            for (int position = robot.getX(); position >= toX; position--) {
-                while (robot.getX() != toX) {
-                    robot.stepForward();
-                }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
             }
-        } else if (robot.getX() < toX) {
-            switch (robot.getDirection()) {
-                case UP:
-                    robot.turnRight();
-                    break;
-                case DOWN:
-                    robot.turnLeft();
-                    break;
-                case LEFT:
-                    for (int i = 0; i <= 2; i++) {
-                        robot.turnRight();
-                    }
-                    break;
-                default:
-                    break;
+        }
+            while (robot.getX() != toX) {
+                robot.stepForward();
             }
-            for (int position = robot.getX(); position <= toX; position++) {
-                while (robot.getX() != toX) {
-                    robot.stepForward();
-                }
+        if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
             }
-        } else if (robot.getY() > toY) {
-            switch (robot.getDirection()) {
-                case LEFT:
-                    robot.turnLeft();
-                    break;
-                case RIGHT:
-                    robot.turnRight();
-                    break;
-                case UP:
-                    for (int i = 0; i <= 2; i++) {
-                        robot.turnRight();
-                    }
-                    break;
-                default:
-                    break;
+        } else {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
             }
-            for (int position = robot.getY(); position >= toY; position--) {
-                while (robot.getY() != toY) {
-                    robot.stepForward();
-                }
-            }
-        } else if (robot.getY() < toY) {
-            switch (robot.getDirection()) {
-                case LEFT:
-                    robot.turnRight();
-                    break;
-                case RIGHT:
-                    robot.turnLeft();
-                    break;
-                case DOWN:
-                    for (int i = 0; i <= 2; i++) {
-                        robot.turnRight();
-                    }
-                    break;
-                default:
-                    break;
-            }
-            for (int position = robot.getY(); position <= toY; position++) {
-                while (robot.getY() != toY) {
-                    robot.stepForward();
-                }
+        }
+            while (robot.getY() != toY) {
+                robot.stepForward();
             }
         }
     }
-}
