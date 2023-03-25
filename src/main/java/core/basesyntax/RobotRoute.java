866d05be3ace5252b6a862636ed3b2d6fd0ca1c6
Anna Poplavska <semachka100201@gmail.com>
2023-03-25 13:55:14 +0100

    made a X movement

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,51 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    static private int stepsX;
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        turnXRobot(robot, toX);
+        moveXRobot(robot, toX);
+
+    }
+
+    public void turnXRobot(Robot robot, int toX) {
+        if (robot.getX() > toX) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+        } else if (robot.getX() < toX) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void moveXRobot(Robot robot, int toX) {
+        stepsX = Math.abs(robot.getX() - toX);
+        for (int i = 0; i < stepsX; i++) {
+            robot.stepForward();
+        }
     }
 }
