b2b64e624c88de02c54086ba14ea6661bced613d
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-04-21 19:48:28 +0300

    task completed

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,38 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+
+        int diffX = toX - robot.getX();
+        int diffY = toY - robot.getY();
+
+        // Turn the robot and move to correct x direction
+        if (diffX < 0) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else if (diffX > 0) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        //Turn the robot and move to correct y direction
+        if (diffY < 0) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        } else if (diffY > 0) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+        }
+
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
