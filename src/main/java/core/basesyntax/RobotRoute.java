e8cacb4c697380edf8ba2934075246b4b2a4a250
VladSchweps <vladschweps1@gmail.com>
2023-04-21 22:33:49 +0300

    Change

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -5,22 +5,20 @@ public class RobotRoute {
         while (robot.getX() != toX) {
             if (robot.getX() > toX) {
                 robot.turnLeft();
-                robot.stepForward();
             }
             if (robot.getX() < toX) {
                 robot.turnRight();
-                robot.stepForward();
             }
-            while (robot.getY() != toY) {
-                if (robot.getY() > toY) {
-                    robot.turnLeft();
-                    robot.stepForward();
-                }
-                if (robot.getY() < toY) {
-                    robot.turnRight();
-                    robot.stepForward();
-                }
+            robot.stepForward();
+        }
+        while (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                robot.turnLeft();
             }
+            if (robot.getY() < toY) {
+                robot.turnRight();
+            }
+            robot.stepForward();
         }
     }
 }
