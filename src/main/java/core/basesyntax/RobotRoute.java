c4afd4b6d93bc2801cc94c2ea7678c4a9fc88363
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-04-21 17:55:56 +0300

    complete hw_v0.1

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,19 +2,101 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        if (toX < 0) {
-            for (int i = 0; i > toX; i--) {
-                robot.turnLeft();
-            }
+        int x = robot.getX();
+        int y = robot.getY();
+        if (toX != x) {
+            moveX(robot, toX);
         }
-        if (toX > 0) {
-            for (int i = 0; i < toX; i++) {
-                robot.turnRight();
-            }
+        if (toY != y) {
+            moveY(robot, toY);
         }
-        if (toY < 0) {
-            for (int i = 0; i > toY; i--) {
+    }
+
+    private void moveX(Robot robot, int toX) {
+        Direction direction = robot.getDirection();
+        int x = robot.getX();
+        if (x < toX) {
+            switch (direction) {
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+            while (x < toX) {
                 robot.stepForward();
+                x++;
+            }
+
+        } else if (x > toX) {
+            switch (direction) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+            while (x > toX) {
+                robot.stepForward();
+                x--;
+            }
+        }
+    }
+
+    private void moveY(Robot robot, int toY) {
+        Direction direction = robot.getDirection();
+        int y = robot.getY();
+        if (y < toY) {
+            switch (direction) {
+                case DOWN:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+            while (y < toY) {
+                robot.stepForward();
+                y++;
+            }
+        } else if (y > toY) {
+            switch (direction) {
+                case UP:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                default:
+                    break;
+            }
+            while (y > toY) {
+                robot.stepForward();
+                y--;
             }
         }
     }
