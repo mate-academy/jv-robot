42e8d7380e1592bd44e4fcb745d96ee3ace87097
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-04-23 12:51:46 +0300

    added robot move solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,45 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int diffOX = toX - robot.getX();
+        int diffOY = toY - robot.getY();
+        boolean move = true;
+        while (move) {
+            if (diffOY < 0 && robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+                if (robot.getDirection() == Direction.DOWN) {
+                    move = false;
+                }
+            } else if (diffOY > 0 && robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+                if (robot.getDirection() == Direction.UP) {
+                    move = false;
+                }
+            } else {
+                move = false;
+            }
+        }
+        for (int i = 0; i < Math.abs(diffOY); i++) {
+            robot.stepForward();
+        }
+        move = true;
+        while (move) {
+            if (diffOX < 0 && robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+                if (robot.getDirection() == Direction.LEFT) {
+                    move = false;
+                }
+            } else if (diffOX > 0 && robot.getDirection() != Direction.RIGHT) {
+                robot.turnLeft();
+                if (robot.getDirection() == Direction.RIGHT) {
+                    move = false;
+                }
+            } else {
+                move = false;
+            }
+        }
+        for (int i = 0; i < Math.abs(diffOX); i++) {
+            robot.stepForward();
+        }
     }
 }
