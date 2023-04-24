9a648b76d46bf63de9c4cd6df4a07d7b7f12c47b
Oleksandr Balan <litecloud.nohax@gmail.com>
2023-04-24 16:54:03 +0300

    checkstyle

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,46 +3,33 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         Direction direction = robot.getDirection();
-        int x = robot.getX();
-        int y = robot.getY(); //
+        final int x = robot.getX();
+        final int y = robot.getY(); //
         if (x > toX) {
-            while(direction != Direction.LEFT) {
+            while (direction != Direction.LEFT) {
                 robot.turnLeft();
-            }
-            while(x != toX) {
-                robot.stepForward();
             }
         }
         if (x < toX) {
-            while(direction != Direction.RIGHT) {
+            while (direction != Direction.RIGHT) {
                 robot.turnLeft();
-            }
-            while(x != toX) {
-                robot.stepForward();
             }
         }
+        while (x != toX) {
+            robot.stepForward();
+        }
         if (y > toY) {
-            while(direction != Direction.DOWN) {
+            while (direction != Direction.DOWN) {
                 robot.turnLeft();
-            }
-            while(y != toY) {
-                robot.stepForward();
             }
         }
         if (y < toY) {
-            while(direction != Direction.UP) {
+            while (direction != Direction.UP) {
                 robot.turnLeft();
             }
-            while(y != toY) {
-                robot.stepForward();
-            }
         }
-
-
-
-
-
-
-      //write your solution here
+        while (y != toY) {
+            robot.stepForward();
+        }
     }
 }
