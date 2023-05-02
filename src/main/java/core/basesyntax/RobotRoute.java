d910796ffefd7683e8f238e74e4e69069fd6b976
OLYA <Ole4kastar79@gmail.com>
2023-05-03 00:04:14 +0300

    create a method moveRobot with two inner methods

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,53 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
+    private int robotX;
+    private int robotY;
+    private Direction robotDirection;
+    private Robot robot;
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        this.robot = robot;
+        moveToX(toX);
+        moveToY(toY);
+    }
+    public void moveToX(int toX) {
+        robotX = robot.getX();
+        robotDirection = robot.getDirection();
+        if (toX < robotX) {
+            while (robotDirection != Direction.LEFT) {
+                robot.turnLeft();
+                robotDirection = robot.getDirection();
+            }
+        } else {
+            while (robotDirection != Direction.RIGHT) {
+                robot.turnRight();
+                robotDirection = robot.getDirection();
+            }
+        }
+        while (toX != robotX) {
+            robot.stepForward();
+            robotX = robot.getX();
+        }
+    }
+
+    public void moveToY(int toY) {
+        robotY = robot.getY();
+        if (toY < robotY) {
+            while(robotDirection != Direction.DOWN) {
+                robot.turnLeft();
+                robotDirection = robot.getDirection();
+            }
+        } else {
+            while(robotDirection != Direction.UP) {
+                robot.turnRight();
+                robotDirection = robot.getDirection();
+            }
+        }
+        while(toY != robotY) {
+           robot.stepForward();
+           robotY = robot.getY();
+        }
     }
 }
