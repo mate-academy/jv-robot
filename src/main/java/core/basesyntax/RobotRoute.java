e03864efa74d0ec48b4eeaea121ee1bd85133a36
pochtalon <pochtalon1010@gmail.com>
2023-05-06 23:42:01 +0300

    Add whitespaces to if

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,14 +2,14 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        if(robot.getX() != toX) {
+        if (robot.getX() != toX) {
             Direction way = (robot.getX() < toX) ? Direction.RIGHT : Direction.LEFT;
             writeWay(robot, way);
             while (robot.getX() != toX) {
                 robot.stepForward();
             }
         }
-        if(robot.getY() != toY) {
+        if (robot.getY() != toY) {
             Direction way = (robot.getY() < toY) ? Direction.UP : Direction.DOWN;
             writeWay(robot, way);
             while (robot.getY() != toY) {
@@ -19,7 +19,7 @@ public class RobotRoute {
     }
 
     private void writeWay(Robot robot, Direction way) {
-        if(robot.getDirection() != way) {
+        if (robot.getDirection() != way) {
             robot.turnRight();
             writeWay(robot, way);
         }
