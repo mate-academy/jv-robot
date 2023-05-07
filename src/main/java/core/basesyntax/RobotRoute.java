653a152def53159057013a984971769e5e137e08
Stanislav Golubkov <golubkovstas83@gmail.com>
2023-05-07 14:33:42 +0300

    add hello Ira

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,6 +3,6 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
-        System.out.println("Hello Vlad!");
+        System.out.println("Hello Ira!");
     }
 }
