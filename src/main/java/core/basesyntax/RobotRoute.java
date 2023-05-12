ba7ed5743f597944de02ef4049e8f49c4aa51e55
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-05-12 10:22:01 +0300

    test solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    private int startX;
+    private int startY;
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        startX = robot.getX();
+        startY = robot.getY();
+
+        if (startX == toX && startY == toY) {
+            return;
+        }
+        Direction neededDirection = calculateDirection(toX, toY);
+
+        turnWhileDirectionsNotMatch(robot, neededDirection);
+        robot.stepForward();
+        moveRobot(robot, toX, toY);
+    }
+
+    private void turnWhileDirectionsNotMatch(Robot robot, Direction neededDirection) {
+        while (robot.getDirection() != neededDirection) {
+            robot.turnLeft();
+        }
+    }
+
+    private Direction calculateDirection(int toX, int toY) {
+        if (toX != startX) {
+            return toX - startX < 0 ? Direction.LEFT : Direction.RIGHT;
+        }
+        return toY - startY < 0 ? Direction.DOWN : Direction.UP;
     }
 }
