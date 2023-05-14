89a58f956c327b7976868f8b88ec4853ed55a42f
Mykhailo <mykhailokuryk16@gmail.com>
2023-05-14 14:24:43 +0300

    add my solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,46 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        int robotXCoordinate = robot.getX();
+        int robotYCoordinate = robot.getY();
+        Direction robotDirection = robot.getDirection();
+
+        //first part
+        if (robotXCoordinate > toX) {
+            while (robotDirection != Direction.LEFT) {
+                robot.turnLeft();
+                robotDirection = robot.getDirection();
+            }
+        } else if (robotXCoordinate < toX) {
+            while (robotDirection != Direction.RIGHT) {
+                robot.turnRight();
+                robotDirection = robot.getDirection();
+            }
+        }
+
+        while (robotXCoordinate != toX) {
+            robot.stepForward();
+            robotXCoordinate = robot.getX();
+        }
+
+        // Second, move to the target Y-coordinate
+        if (robotYCoordinate > toY) {
+            while (robotDirection != Direction.DOWN) {
+                robot.turnLeft();
+                robotDirection = robot.getDirection();
+            }
+        } else if (robotYCoordinate < toY) {
+            while (robotDirection != Direction.UP) {
+                robot.turnRight();
+                robotDirection = robot.getDirection();
+            }
+        }
+
+        while (robotYCoordinate != toY) {
+            robot.stepForward();
+            robotYCoordinate = robot.getY();
+        }
+
     }
 }
