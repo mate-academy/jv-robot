489ae93ba7e930ab46c0da06853ba0fc7e75c728
Viktor Budivskyi <meyson443@gmail.com>
2023-05-15 19:26:51 +0300

    implemented the moveRobot method according to the task

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,69 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX > robot.getX() && toY > robot.getY()) {
+            turnRobotRight(robot);
+            for (int i = robot.getX(); i < toX; i++) {
+                robot.stepForward();
+            }
+            turnRobotUp(robot);
+            for (int i = robot.getY(); i < toY; i++) {
+                robot.stepForward();
+            }
+        } else if (toX < robot.getX() && toY < robot.getY()) {
+            turnRobotLeft(robot);
+            for (int i = robot.getX(); i > toX; i--) {
+                robot.stepForward();
+            }
+            turnRobotDown(robot);
+            for (int i = robot.getY(); i > toY; i--) {
+                robot.stepForward();
+            }
+        } else if (toX >= robot.getX() && toY <= robot.getY()) {
+            turnRobotRight(robot);
+            for (int i = robot.getX(); i < toX; i++) {
+                robot.stepForward();
+            }
+            turnRobotDown(robot);
+            for (int i = robot.getY(); i > toY; i--) {
+                robot.stepForward();
+            }
+        } else if (toX <= robot.getX() && toY >= robot.getY()) {
+            turnRobotLeft(robot);
+            for (int i = robot.getX(); i > toX; i--) {
+                robot.stepForward();
+            }
+            turnRobotUp(robot);
+            for (int i = robot.getY(); i < toY; i++) {
+                robot.stepForward();
+            }
+        }
+        System.out.println(robot.getX() + " " + robot.getY());
+    }
+
+    private void turnRobotRight(Robot robot) {
+        while (robot.getDirection() != Direction.RIGHT ) {
+            robot.turnRight();
+        }
+    }
+
+    private void turnRobotLeft(Robot robot) {
+        while (robot.getDirection() != Direction.LEFT ) {
+            robot.turnLeft();
+        }
+    }
+
+    private void turnRobotUp(Robot robot) {
+        while (robot.getDirection() != Direction.UP ) {
+            robot.turnLeft();
+        }
+    }
+
+    private void turnRobotDown(Robot robot) {
+        while (robot.getDirection() != Direction.DOWN ) {
+            robot.turnRight();
+        }
     }
 }
+
+
