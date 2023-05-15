8b07dcc94b0d8e2afbe1d71b798200696122e28e
Roman Zatorskyy <rzbehappy@gmail.com>
2023-05-15 18:37:51 +0200

    jv-robot solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,29 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int x = robot.getX();
+        int y = robot.getY();
+
+        while (x != toX || y != toY) {
+            if (x < toX) {
+                robot.turnRight();
+                robot.stepForward();
+                x++;
+            } else if (x > toX) {
+                robot.turnLeft();
+                robot.stepForward();
+                x--;
+            } else if (y < toY) {
+                robot.stepForward();
+                y++;
+            } else if (y > toY) {
+                robot.turnRight();
+                robot.turnRight();
+                robot.stepForward();
+                y--;
+            }
+        }
     }
 }
