5b4cd007976d97fa234867dfd7e48039080e5409
Kseniia Loginova <ksenixen@gmail.com>
2023-05-16 18:31:11 +0300

    Removed code duplication

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -6,32 +6,25 @@ public class RobotRoute {
             while (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             }
-            while (robot.getX() != toX) {
-                robot.stepForward();
-            }
         } else if (toX > robot.getX()) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-            while (robot.getX() != toX) {
-                robot.stepForward();
-            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
         }
         if (toY < robot.getY()) {
             while (robot.getDirection() != Direction.DOWN) {
                 robot.turnLeft();
             }
-            while (robot.getY() != toY) {
-                robot.stepForward();
-            }
         } else if (toY > robot.getY()) {
             while (robot.getDirection() != Direction.UP) {
                 robot.turnRight();
             }
-            while (robot.getY() != toY) {
-                robot.stepForward();
-            }
         }
-
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
