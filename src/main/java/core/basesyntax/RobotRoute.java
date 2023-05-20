b63f4a7cf1fec4cd0058a2d16cbe70a75b4597fc
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-05-20 15:03:20 +0300

    refractoring code after 3 PR for 4 PR

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         //write your solution here
-
         if (robot.getX() == toX && robot.getY() == toY) {
             robot.getDirection();
         }
-
         if (robot.getX() < toX) {
             while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
