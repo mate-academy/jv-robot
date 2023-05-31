c9dac1f925e6f7debfb3690a8d4f085de02096a4
Yura Khilko <yurakhilko@gmail.com>
2023-05-31 20:47:20 +0300

    added algorithm to RobotRoute.moveRomote

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,40 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        if (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                //  turn left
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+            } else {
+                //  turn right
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnLeft();
+                }
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                //  turn down
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnLeft();
+                }
+            } else {
+                //  turn up
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnLeft();
+                }
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
+
     }
 }
