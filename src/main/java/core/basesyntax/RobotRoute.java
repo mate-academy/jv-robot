296822730e858f5cff5f707a5d9d9526a0306f9e
Hlib Bykovskyi <minexgleb@gmail.com>
2023-06-02 02:56:10 +0300

    add moverobot method implementation

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,50 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction robotDirection = robot.getDirection();
+        int posX = robot.getX();
+        int posY = robot.getY();
+        int distanceX = toX - posX;
+        int distanceY = toY - posY;
+
+        changeDirectionX(robot, distanceX);
+        for (int i = 0; i < Math.abs(distanceX); i++) {
+            robot.stepForward();
+        }
+
+        changeDirectionY(robot, distanceY);
+        for (int i = 0; i < Math.abs(distanceY); i++) {
+            robot.stepForward();
+        }
+    }
+
+    private void changeDirectionX(Robot robot, int distanceX) {
+
+        if (distanceX > 0) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+
+        if (distanceX < 0) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        }
+    }
+
+    private void changeDirectionY(Robot robot, int distanceY) {
+
+        if (distanceY > 0) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+        }
+
+        if (distanceY < 0) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        }
     }
 }
