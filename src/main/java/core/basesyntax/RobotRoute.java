626492605381c428f7ec7f05952c16fa6104eac9
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-06-06 23:55:07 +0300

    added methods moveToX() and moveToY()

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,45 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveToX(robot, toX);
+        moveToY(robot, toY);
+
+    }
+    public void moveToX(Robot robot, int toX) {
+        if(robot.getX() < toX){
+            while (!robot.getDirection().equals(Direction.RIGHT)) {
+                robot.turnRight();
+            }
+            while (robot.getX() < toX) {
+                robot.stepForward();
+            }
+        }
+        if(robot.getX() > toX){
+            while (!robot.getDirection().equals(Direction.LEFT)) {
+                robot.turnLeft();
+            }
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+        }
+    }
+    public void moveToY(Robot robot, int toY) {
+
+        if(robot.getY() < toY){
+            while (!robot.getDirection().equals(Direction.UP)) {
+                robot.turnLeft();
+            }
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        }
+        if(robot.getY() > toY){
+            while (!robot.getDirection().equals(Direction.DOWN)) {
+                robot.turnLeft();
+            }
+            while (robot.getY() > toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
