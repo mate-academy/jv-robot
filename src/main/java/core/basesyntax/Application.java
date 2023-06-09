b6782abdc25454115ad461f59551d3bc770d3307
ANDREY <andrey.kiyas@gmail.com>
2023-06-10 01:57:00 +0300

    hw-robot-route-solution-andrey-kiyas

---
+++ src/main/java/core/basesyntax/Application.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+public class Application {
+    public static void main(String[] args) {
+        Robot robot = new Robot(Direction.RIGHT, 3, -2);
+        System.out.println("Direction: " + robot.getDirection() + ", X = " + robot.getX() + ", Y = " + robot.getY());
+
+        RobotRoute.moveRobot(robot, -4, 3);
+        System.out.println("Direction: " + robot.getDirection() + ", X = " + robot.getX() + ", Y = " + robot.getY());
+    }
+}
--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        //write your solution here
+
+        int stepsByX = toX - robot.getX();
+        int stepsByY = toY - robot.getY();
+
+        Direction moveX = (stepsByX > 0) ? Direction.RIGHT : Direction.LEFT;
+        Direction moveY = (stepsByY > 0) ? Direction.UP : Direction.DOWN;
+
+        // Поворачиваем по X
+        while (robot.getDirection() != moveX) {
+            robot.turnLeft();
+        }
+
+        // Двигаем по X
+        for (int x = 1; x <= Math.abs(stepsByX); x++) {
+            robot.stepForward();
+        }
+
+        // Поворачиваем по Y
+        while (robot.getDirection() != moveY) {
+            robot.turnRight();
+        }
+
+        // Двигаем по Y
+        for (int y = 1; y <= Math.abs(stepsByY); y++) {
+            robot.stepForward();
+        }
     }
 }
