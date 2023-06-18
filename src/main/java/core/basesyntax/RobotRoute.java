57138a3bf969c4cd9e6d897d7caf09b86968fadc
I <dovgopolkir@gmail.com>
2023-06-18 21:14:05 +0300

    added changes in the class RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,23 +2,26 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        while (robot.getX() != toX || robot.getY() != toY) {
+        while (robot.getX() != toX) {
             if (robot.getX() < toX) {
                 while (robot.getDirection() != Direction.RIGHT) {
                     robot.turnRight();
                 }
                 robot.stepForward();
-            } else if (robot.getX() > toX) {
+            } else {
                 while (robot.getDirection() != Direction.LEFT) {
                     robot.turnRight();
                 }
                 robot.stepForward();
-                if (robot.getY() < toY) {
+            }
+        }
+                while (robot.getY() != toY) {
+                    if (robot.getY() < toY) {
                     while (robot.getDirection() != Direction.UP) {
                         robot.turnRight();
                     }
                     robot.stepForward();
-                } else if (robot.getY() > toY) {
+                } else {
                     while (robot.getDirection() != Direction.DOWN) {
                         robot.turnRight();
                     }
@@ -27,4 +30,4 @@ public class RobotRoute {
             }
         }
     }
-}
+
