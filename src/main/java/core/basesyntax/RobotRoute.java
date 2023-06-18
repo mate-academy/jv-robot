dc17b71f8fcb1be251dab41b72ec971e525214d4
Efim Turkovskiy <turkovskiy.efim@outlook.com>
2023-06-18 14:28:51 +0300

    deletedSpace

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class RobotRoute {
-
     public void moveRobot(Robot robot, int toX, int toY) {
         if (robot.getX() < toX) {
             while (robot.getDirection() != Direction.RIGHT) {
