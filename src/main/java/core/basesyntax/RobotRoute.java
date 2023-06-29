6d4201e1a274496ce06305999306e0e5264d99cb
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-06-29 15:36:39 +0300

    added an implemetation of moveRobot method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,32 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        if (robot.getX() > toX) {
+            changeDirection(robot, Direction.LEFT);
+            robot.stepForward();
+            moveRobot(robot, toX, toY);
+        } else if (robot.getX() < toX) {
+            changeDirection(robot, Direction.RIGHT);
+            robot.stepForward();
+            moveRobot(robot, toX, toY);
+        }
+
+        if (robot.getY() > toY) {
+            changeDirection(robot, Direction.DOWN);
+            robot.stepForward();
+            moveRobot(robot, toX, toY);
+        } else if (robot.getY() < toY) {
+            changeDirection(robot, Direction.UP);
+            robot.stepForward();
+            moveRobot(robot, toX, toY);
+        }
+    }
+
+    private void changeDirection(Robot robot, Direction direction) {
+        if (!robot.getDirection().equals(direction)) {
+            robot.turnRight();
+            changeDirection(robot, direction);
+        }
     }
 }
