c7ba01496d4c0748ef9b59299c8f3074669a1f41
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2023-07-06 13:34:13 +0200

    create steps for robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,52 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+        Direction currentDirection = robot.getDirection();
+
+        if (currentX > toX) {
+            while (currentDirection != Direction.LEFT) {
+                robot.turnLeft();
+                currentDirection = robot.getDirection();
+            }
+            while (currentX != toX) {
+                robot.stepForward();
+                currentX = robot.getX();
+            }
+        }
+
+        if (currentX < toX) {
+            while (currentDirection != Direction.RIGHT) {
+                robot.turnRight();
+                currentDirection = robot.getDirection();
+            }
+            while (currentX != toX) {
+                robot.stepForward();
+                currentX = robot.getX();
+            }
+        }
+
+        if (currentY > toY) {
+            while (currentDirection != Direction.DOWN) {
+                robot.turnLeft();
+                currentDirection = robot.getDirection();
+            }
+            while (currentY != toY) {
+                robot.stepForward();
+                currentY = robot.getY();
+            }
+        }
+
+        if (currentY < toY) {
+            while (currentDirection != Direction.UP) {
+                robot.turnRight();
+                currentDirection = robot.getDirection();
+            }
+            while (currentY != toY) {
+                robot.stepForward();
+                currentY = robot.getY();
+            }
+        }
     }
 }
