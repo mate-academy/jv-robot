23fca0b931173f01b40df2fcb8173d55685159b8
karma.misha <karma.misha@gmail.com>
2023-07-19 23:28:21 +0300

    implement method moveRobot(); added moveX(),moveY(),determineDeractionAxisX(),determineDeractionAxisY()

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,55 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveX(robot, toX);
+        moveY(robot, toY);
+    }
+
+    private Direction determineDeractionAxisX(Robot robot, int expectedX) {
+        if (expectedX > robot.getX()) {
+            return Direction.RIGHT;
+        } else {
+            return Direction.LEFT;
+        }
+    }
+
+    private Direction determineDeractionAxisY(Robot robot, int expectedY) {
+        if (expectedY > robot.getY()) {
+            return Direction.UP;
+        } else {
+            return Direction.DOWN;
+        }
+    }
+
+    private void moveX(Robot robot, int toX) {
+        if (robot.getX() == toX) {
+            return;
+        }
+
+        Direction expectedDirectionX = determineDeractionAxisX(robot, toX);
+
+        while (expectedDirectionX != robot.getDirection()) {
+            robot.turnLeft();
+        }
+
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveY(Robot robot, int toY) {
+        if (robot.getY() == toY) {
+            return;
+        }
+
+        Direction expectedDirectionY = determineDeractionAxisY(robot, toY);
+
+        while (expectedDirectionY != robot.getDirection()) {
+            robot.turnLeft();
+        }
+
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
