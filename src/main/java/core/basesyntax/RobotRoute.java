200fffd692e7480f88ea34bac836748e68c5e23d
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2023-07-25 15:49:33 +0300

    fix Solution Robot Route task

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -6,23 +6,24 @@ public class RobotRoute {
           while (robot.getDirection() != Direction.RIGHT) {
               robot.turnLeft();
           }
-      } else {
-          while (robot.getDirection() != Direction.LEFT) {
-              robot.turnLeft();
-          }
+      }
+      if (robot.getX() > toX) {
+        while (robot.getDirection() != Direction.RIGHT) {
+            robot.turnRight();
+        }
       }
       while (robot.getX() != toX) {
           robot.stepForward();
       }
-
       if (robot.getY() < toY) {
-        while (robot.getDirection() != Direction.UP) {
-            robot.turnLeft();
-        }
-      } else {
-          while (robot.getDirection() != Direction.DOWN) {
+          while (robot.getDirection() != Direction.UP) {
               robot.turnLeft();
           }
+      }
+      if (robot.getY() > toY) {
+        while (robot.getDirection() != Direction.DOWN) {
+            robot.turnLeft();
+        }
       }
       while (robot.getY() != toY) {
           robot.stepForward();
