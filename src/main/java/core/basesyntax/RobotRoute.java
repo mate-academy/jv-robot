8a40ed1c047af01a3021a1f1933eda0260ebe0d9
Bohdan Maksymenko <bogdan.maksimenko.2002@gmail.com>
2023-07-29 00:14:08 +0300

    done method moveRobot to change his pozition

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,33 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getY() < toY) {
+            if (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+                continue;
+            }
+            robot.stepForward();
+        }
+        while (robot.getY() > toY) {
+            if (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+                continue;
+            }
+            robot.stepForward();
+        }
+        while (robot.getX() < toX) {
+            if (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+                continue;
+            }
+            robot.stepForward();
+        }
+        while (robot.getX() > toX) {
+            if (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+                continue;
+            }
+            robot.stepForward();
+        }
     }
 }
