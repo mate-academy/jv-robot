b5724904bbf7d0546d1e27334c28512f0e7a4b0a
katym <katy.mozharovska@gmail.com>
2023-07-28 19:50:57 +0300

    implemented moveRobot method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -11,15 +11,18 @@ public class RobotRoute {
             while(robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             }
+
         } else if (robot.getX() < toX) {
             while(robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
+
         }
 
         while(robot.getX() != toX) {
             robot.stepForward();
         }
+
     }
 
     private static void moveToY(Robot robot, int toY) {
@@ -27,14 +30,17 @@ public class RobotRoute {
             while(robot.getDirection() != Direction.DOWN) {
                 robot.turnRight();
             }
+
         } else if (robot.getY() < toY) {
             while(robot.getDirection() != Direction.UP) {
                 robot.turnRight();
             }
+
         }
 
         while(robot.getY() != toY) {
             robot.stepForward();
         }
+
     }
 }
