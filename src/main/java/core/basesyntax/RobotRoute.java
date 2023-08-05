fd10fdef825a4412ec43b7433eb0e5d9b72b2a84
Nazar <nazarmos13@gmail.com>
2023-08-05 22:08:26 +0300

    add moveRobot() to the RobotRoute class

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,50 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                if (robot.getDirection() != Direction.DOWN) {
+                    while (robot.getDirection() != Direction.LEFT) {
+                        robot.turnLeft();
+                    }
+                } else {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            } else {
+                if (robot.getDirection() != Direction.UP) {
+                    while (robot.getDirection() != Direction.RIGHT) {
+                        robot.turnLeft();
+                    }
+                } else {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            }
+
+        }
+
+        while (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                if (robot.getDirection() != Direction.RIGHT) {
+                    while (robot.getDirection() != Direction.DOWN) {
+                        robot.turnLeft();
+                    }
+                } else {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            } else {
+                if (robot.getDirection() != Direction.LEFT) {
+                    while (robot.getDirection() != Direction.UP) {
+                        robot.turnLeft();
+                    }
+                } else {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            }
+
+        }
     }
 }
