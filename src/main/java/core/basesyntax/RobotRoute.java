af3c2cb3218cdda59181daf37de2742be4bf3532
Teract10s <trs.leonov@gmail.com>
2023-08-09 09:25:07 +0300

    implemented moveRobot method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,35 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveRobotByX(robot, toX);
+        moveRobotByY(robot, toY);
+    }
+
+    private static void turnRobot (Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnRight();
+        }
+    }
+
+    private static void moveRobotByX (Robot robot, int toX) {
+        if (robot.getX() > toX) {
+            turnRobot(robot, Direction.LEFT);
+        } else {
+            turnRobot(robot, Direction.RIGHT);
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+    }
+
+    private static void moveRobotByY (Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            turnRobot(robot, Direction.DOWN);
+        } else {
+            turnRobot(robot, Direction.UP);
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
