5080ebccdb9cf75d50b92087c626e040e4f77ea4
Denis Bohusevich <bogusevich03@gmail.com>
2023-08-10 14:52:37 +0300

    make robot go home

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,37 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveRobotOnX(robot,toX);
+        moveRobotOnY(robot, toY);
+    }
+
+    private void moveRobotOnX(Robot robot, int toX) {
+        if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveRobotOnY(Robot robot, int toY) {
+        if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+        }
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
