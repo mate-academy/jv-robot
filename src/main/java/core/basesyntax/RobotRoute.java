2ad08cb52576580b395f664034ef2644a7e6883a
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-08-11 06:50:47 +0300

    Now poor baby robot can move ;)

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,28 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        boolean isGreaterX = robot.getX() > toX;
+        boolean isGreaterY = robot.getY() > toY;
+        Direction toDirection;
+
+        toDirection = isGreaterX ? Direction.LEFT : Direction.RIGHT;
+
+        while (!(toDirection.equals(robot.getDirection()))) {
+            robot.turnLeft();
+        }
+
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        toDirection = isGreaterY ? Direction.DOWN : Direction.UP;
+
+        while (!(toDirection.equals(robot.getDirection()))) {
+            robot.turnLeft();
+        }
+
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
