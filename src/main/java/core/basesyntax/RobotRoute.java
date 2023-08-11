e6ea33746480e151cb2c3a276888e9cca5511e6c
Roman K <romazan07sq@gmail.com>
2023-08-11 16:57:50 +0300

    implement task

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,25 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    private static int directionOfMovement = 0;
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        // next forward on X-axis;
+        directionOfMovement = robot.getX() > toX ?  Direction.LEFT.ordinal() : Direction.RIGHT.ordinal();
+        while (robot.getDirection().ordinal() != directionOfMovement) {
+            robot.turnRight();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        // next forward on Y-axis;
+        directionOfMovement = robot.getY() > toY ?  Direction.DOWN.ordinal() : Direction.UP.ordinal();
+        while (robot.getDirection().ordinal() != directionOfMovement) {
+            robot.turnRight();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
