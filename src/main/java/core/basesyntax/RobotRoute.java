62b939f0a4e76f686f37aad06853db11286b0ee7
Ihor Trokhymchuk <hameleon009@gmail.com>
2023-08-17 12:06:28 +0300

    fixed code style

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,31 +1,25 @@
 package core.basesyntax;
 
-import javax.management.Descriptor;
-
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         int y = robot.getY();
         int x = robot.getX();
         Direction direct = robot.getDirection();
-
         // VERTICAL STEPS
         if (toY > y) {
             while (direct != direct.UP) {
                 robot.turnRight();
                 direct = robot.getDirection();
             }
-
             while (toY != y) {
                 robot.stepForward();
                 y = robot.getY();
             }
-
         } else if (toY < y) {
             while (direct != direct.DOWN) {
                 robot.turnRight();
                 direct = robot.getDirection();
             }
-
             while (toY != y) {
                 robot.stepForward();
                 y = robot.getY();
@@ -33,28 +27,23 @@ public class RobotRoute {
         }
         //HORIZONTAL STEPS
         if (toX > x) {
-
             while (direct != direct.RIGHT) {
                 robot.turnRight();
                 direct = robot.getDirection();
             }
-
             while (x != toX) {
                 robot.stepForward();
                 x = robot.getX();
             }
-
         } else if (toX < x) {
-            while (direct != direct.LEFT){
+            while (direct != direct.LEFT) {
                 robot.turnRight();
                 direct = robot.getDirection();
             }
-
             while (x != toX) {
                 robot.stepForward();
                 x = robot.getX();
             }
         }
-
     }
 }
