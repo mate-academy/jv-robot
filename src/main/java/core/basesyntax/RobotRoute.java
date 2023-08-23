64389c9a064b1a59be6937f061803883ac05aa27
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-08-23 20:33:55 +0300

    Clean checkstyle.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -4,7 +4,7 @@ public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         int actualX = robot.getX();
         int actualY = robot.getY();
-        if(actualY == toY && actualX == toX) {
+        if (actualY == toY && actualX == toX) {
             return;
         }
         defineHorizontalDirection(robot, actualX, toX);
@@ -12,22 +12,23 @@ public class RobotRoute {
         defineVerticalDirection(robot, actualY, toY);
         moveStepsForward(robot, Math.sqrt(Math.pow(toY - actualY, 2)));
     }
-    private void moveStepsForward(Robot robot, double numberOfSteps){
-        for (int i = 0 ; i < numberOfSteps; i++) {
+
+    private void moveStepsForward(Robot robot, double numberOfSteps) {
+        for (int i = 0; i < numberOfSteps; i++) {
             robot.stepForward();
         }
     }
 
-    private void defineHorizontalDirection(Robot robot, int currentX, int toX){
-        if(currentX < toX) {
+    private void defineHorizontalDirection(Robot robot, int currentX, int toX) {
+        if (currentX < toX) {
             turnToDirection(robot, Direction.RIGHT);
         } else if (currentX > toX) {
             turnToDirection(robot, Direction.LEFT);
         }
     }
 
-    private void defineVerticalDirection(Robot robot, int currentY, int toY){
-        if(currentY < toY) {
+    private void defineVerticalDirection(Robot robot, int currentY, int toY) {
+        if (currentY < toY) {
             turnToDirection(robot, Direction.UP);
         } else if (currentY > toY) {
             turnToDirection(robot, Direction.DOWN);
@@ -35,7 +36,7 @@ public class RobotRoute {
     }
 
     private void turnToDirection(Robot robot, Direction direction) {
-        while(robot.getDirection() != direction) {
+        while (robot.getDirection() != direction) {
             robot.turnRight();
         }
     }
