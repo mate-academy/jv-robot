046edf2e2b728a841bd6577588912a92b2c26992
Oleskii Cherniienko <molekyla2008@gmail.com>
2023-08-29 13:03:46 +0300

    implementation of the robot movement method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+    protected void moveRobot(Robot robot, int toX, int toY) {
+      int deltaX = toX - robot.getX();
+      int deltaY = toY - robot.getY();
+
+      if (deltaX > 0) {
+          for (int i = 0; i < deltaX; i++) {
+              robot.turnRight();
+          }
+      } else if (deltaX < 0) {
+          for (int i = 0; i > deltaX; i--) {
+              robot.turnLeft();
+          }
+      }
+
+          for (int i = 0; i < Math.abs(deltaX); i++) {
+              robot.stepForward();
+          }
+
+      if (deltaY > 0) {
+          for(int i = 0; i < deltaY; i++) {
+              robot.turnRight();
+          }
+      } else if (deltaY < 0) {
+          for(int i = 0; i > deltaY; i--) {
+              robot.turnLeft();
+          }
+      }
+
+          for (int i = 0; i < Math.abs(deltaY); i++) {
+              robot.stepForward();
+          }
     }
 }
