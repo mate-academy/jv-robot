276562f7f83fccf824dc02aa08ad899eb3eba61c
Petro Smoliar <smp22pl@gmail.com>
2023-08-29 12:08:13 +0200

    hm - robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,63 +2,63 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    private Direction directionToX;
+    private Direction directionToY;
+
     public void moveRobot(Robot robot, int toX, int toY) {
         //write your solution here
-        Direction directionX = robot.getX() > toX ? Direction.LEFT : Direction.RIGHT;
-        Direction directionY = robot.getY() > toY ? Direction.DOWN : Direction.UP;
-        if (robot.getX() != toX) {
-            if (robot.getDirection() == directionX) {
-                while (robot.getX() != toX) {
-                    robot.stepForward();
-                }
-            } else {
-                if (robot.getDirection() == Direction.DOWN) {
-                    if (directionX == Direction.LEFT) {
-                        robot.turnRight();
-                        while (robot.getX() != toX) {
-                            robot.stepForward();
-                        }
-                    }
-                } else {
-                    while (directionX != robot.getDirection()) {
-                        robot.turnLeft();
-                    }
-                    while (robot.getX() != toX) {
-                        robot.stepForward();
-                    }
-                }
+        directionToX = robot.getX() > toX ? Direction.LEFT : Direction.RIGHT;
+        directionToY = robot.getY() > toY ? Direction.DOWN : Direction.UP;
+        if ((Math.abs(giveNumber(directionToX) - giveNumber(robot.getDirection())) != 2)
+                && ((giveNumber(directionToY) - giveNumber(robot.getDirection()) != 0))) {
+            toTurn(giveNumber(robot.getDirection()), giveNumber(directionToX), robot, directionToX);
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+            toTurn(giveNumber(robot.getDirection()), giveNumber(directionToY), robot, directionToY);
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        } else {
+            toTurn(giveNumber(robot.getDirection()), giveNumber(directionToY), robot, directionToY);
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+            toTurn(giveNumber(robot.getDirection()), giveNumber(directionToX), robot, directionToX);
+            while (robot.getX() != toX) {
+                robot.stepForward();
             }
         }
-        if (robot.getY() != toY) {
-            if (robot.getDirection() == directionY) {
-                while (robot.getY() != toY) {
-                    robot.stepForward();
-                }
-            } else {
-                if (robot.getDirection() == Direction.LEFT) {
-                    if (directionY == Direction.DOWN) {
-                        robot.turnLeft();
-                        while (robot.getY() != toY) {
-                            robot.stepForward();
-                        }
-                    } else {
-                        while (robot.getDirection() != directionY) {
-                            robot.turnRight();
-                        }
-                        while (robot.getY() != toY) {
-                            robot.stepForward();
-                        }
-                    }
+    }
 
-                } else {
-                    while (robot.getDirection() != directionY) {
-                        robot.turnRight();
-                    }
-                    while (robot.getY() != toY) {
-                        robot.stepForward();
-                    }
-                }
+    private int giveNumber(Direction direction) {
+        switch (direction) {
+            case UP:
+                return 1;
+            case RIGHT:
+                return 2;
+            case DOWN:
+                return 3;
+            case LEFT:
+                return 4;
+            default:
+                return 0;
+        }
+    }
+
+    private void toTurn(int start, int finish, Robot robot, Direction finishDirection) {
+        if ((finish - start) == 0) {
+            return;
+        }
+        if ((finish - start) == 1 || (finish - start) == -3) {
+            while (robot.getDirection() != finishDirection) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != finishDirection) {
+                robot.turnLeft();
             }
         }
+        return;
     }
 }
