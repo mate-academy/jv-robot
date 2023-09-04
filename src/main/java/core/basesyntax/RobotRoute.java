28f1f88b664da141c5e2b74c45f088508d0728ee
Illya <illios.piv@gmail.com>
2023-09-04 04:56:41 +0200

    Added ternary operator. Create method turnOnPosition. Add loop while. Implemented method moveRobot.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+
+        Direction firstRightDirection = (robot.getX() > toX) ? Direction.LEFT : Direction.RIGHT;
+        this.turnOnPosition(robot, firstRightDirection);
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        Direction secondRightDirection = (robot.getY() > toY) ? Direction.DOWN : Direction.UP;
+        this.turnOnPosition(robot, secondRightDirection);
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private void turnOnPosition(Robot robot, Direction rightDirection) {
+        Direction currentDirection = robot.getDirection();
+        if (currentDirection == Direction.RIGHT && rightDirection == Direction.DOWN
+                || currentDirection == Direction.DOWN && rightDirection == Direction.LEFT
+                || currentDirection == Direction.LEFT && rightDirection == Direction.UP
+                || currentDirection == Direction.UP && rightDirection == Direction.RIGHT) {
+            while (robot.getDirection() != rightDirection) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != rightDirection) {
+                robot.turnLeft();
+            }
+        }
     }
 }
+
