e680ee2d48591590a1982e6e35b996b0d5822fd3
nazariykolesnikoww <nazarkolesnikov536@gmail.com>
2023-09-12 11:18:49 +0300

    Help the robot to reach the position (toX, toY), change the current position and robot's direction to get to the final position, implementaion of the method moveRobot()

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,35 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+
+        while (currentY != toY || currentX != toX) {
+            if (currentX < toX) {
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentX = currentX + 1;
+            } else if (currentX > toX) {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+                currentX = currentX - 1;
+            } else if (currentY < toY) {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentY = currentY + 1;
+            } else {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentY = currentY - 1;
+            }
+        }
     }
 }
