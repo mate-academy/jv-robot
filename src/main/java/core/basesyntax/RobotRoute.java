d0d83a5246ac1e950b6dd2f630541b539a483b82
Jakub Sekowski <sekowskijakub@gmail.com>
2023-09-17 17:43:15 +0200

    First solution of jv-robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        if (toX - robot.getX() > 0) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (toX - robot.getX() != 0) {
+                robot.stepForward();
+            }
+        } else if (toX - robot.getX() < 0) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            while (toX - robot.getX() != 0) {
+                robot.stepForward();
+            }
+        }
+        if (toY - robot.getY() > 0) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            while (toY - robot.getY() != 0) {
+                robot.stepForward();
+            }
+        } else if (toY - robot.getY() < 0) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            while (toY - robot.getY() != 0) {
+                robot.stepForward();
+            }
+        }
     }
 }
