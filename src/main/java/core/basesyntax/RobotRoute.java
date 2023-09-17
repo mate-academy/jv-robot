18583c7a31c92ae2d0c54c94871d388ffdd93803
Mateusz Rostek <17rostek17@gmail.com>
2023-09-17 12:53:33 +0200

    Created 2 more methods to keep the code more concise and readable.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,7 +2,11 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
+        goToDesignatedX(robot, toX);
+        goToDesignatedY(robot, toY);
+    }
 
+    private void goToDesignatedX(Robot robot, int toX) {
         if (robot.getX() > toX) {
             while (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
@@ -16,7 +20,9 @@ public class RobotRoute {
         while (robot.getX() != toX) {
             robot.stepForward();
         }
+    }
 
+    private void goToDesignatedY(Robot robot, int toY) {
         if (robot.getY() > toY) {
             while (robot.getDirection() != Direction.DOWN) {
                 robot.turnLeft();
@@ -31,6 +37,4 @@ public class RobotRoute {
             robot.stepForward();
         }
     }
-
-
 }
