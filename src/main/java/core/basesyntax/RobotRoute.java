c961a57b9e16dc28399b178b6a701f1b74169fad
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-09-28 14:22:11 +0300

    RobotRoute beta v1.0.0

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,25 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    public static void main(String[] args) {
+        RobotRoute route = new RobotRoute();
+        Robot robot = new Robot(Direction.RIGHT,0,0);
+        route.moveRobot(robot,4,4);
+    }
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction desiredXDirection = robot.getX() > toX ? Direction.LEFT : Direction.RIGHT;
+        Direction desiredYDirection = robot.getY() > toY ? Direction.DOWN : Direction.UP;
+        while (!desiredXDirection.equals(robot.getDirection())) {
+            robot.turnLeft();
+        }
+        while (!(toX == robot.getX())) {
+            robot.stepForward();
+        }
+        while (!desiredYDirection.equals(robot.getDirection())) {
+            robot.turnRight();
+        }
+        while (!(toY == robot.getY())) {
+            robot.stepForward();
+        }
     }
 }
