62380eb5b6a1db7893b4007f2159a382ea6e3a72
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2023-10-09 16:52:23 +0300

    robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,20 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX || robot.getY() != toY) {
+            if (robot.getX() < toX) {
+                robot.turnRight();
+                robot.stepForward();
+            } else if (robot.getX() > toX) {
+                robot.turnLeft();
+                robot.stepForward();
+            } else if (robot.getY() < toY) {
+                robot.stepForward();
+            } else if (robot.getY() > toY) {
+                robot.turnLeft();
+                robot.turnLeft();
+                robot.stepForward();
+            }
+        }
     }
 }
