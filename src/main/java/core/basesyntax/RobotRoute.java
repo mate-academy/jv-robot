5af0e4f8d71e7fc09aa91588983245ffe11a028d
Eugene Korniuk <korniuk77@gmail.com>
2023-10-10 23:31:25 +0300

    fix method moveRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,28 +2,72 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        final int startPositionX = robot.getX();
-        final int startPositionY = robot.getY();
-        robot.stepForward();
-        if (robot.getX() > startPositionX && startPositionX > toX) {
-            robot.turnRight();
-            robot.turnRight();
-        } else if (robot.getX() < startPositionX && startPositionX < toX) {
-            robot.turnLeft();
-            robot.turnLeft();
+        if (robot.getX() > toX) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() < toX) {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                case UP:
+                    robot.turnRight();
+                    break;
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
         }
-        while (robot.getX() != toX) {
-            robot.stepForward();
-        }
-        if (robot.getY() > startPositionY && startPositionY > toY) {
-            robot.turnRight();
-            robot.turnRight();
-        } else if (robot.getY() < startPositionY && startPositionY < toY) {
-            robot.turnLeft();
-            robot.turnLeft();
-        }
-        while (robot.getY() != toY) {
-            robot.stepForward();
+        if (robot.getY() > toY) {
+            switch (robot.getDirection()) {
+                case UP:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        } else if (robot.getY() < toY) {
+            switch (robot.getDirection()) {
+                case DOWN:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+
         }
     }
 }
