07ae4be23cebf337cb183efe253789be9cbd0fd5
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2023-10-10 08:37:34 +0300

    change the code

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,20 +2,31 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        while (robot.getX() != toX || robot.getY() != toY) {
+        while (robot.getX() != toX) {
             if (robot.getX() < toX) {
-                robot.turnRight();
-                robot.stepForward();
-            } else if (robot.getX() > toX) {
-                robot.turnLeft();
-                robot.stepForward();
-            } else if (robot.getY() < toY) {
-                robot.stepForward();
-            } else if (robot.getY() > toY) {
-                robot.turnLeft();
-                robot.turnLeft();
-                robot.stepForward();
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+            } else {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnRight();
+                }
             }
+            robot.stepForward();
+        }
+
+        while (robot.getY() != toY) {
+            if (robot.getY() < toY) {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+            } else {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnRight();
+                }
+            }
+            robot.stepForward();
         }
     }
 }
+
