49bd94186f650321b9414de1e0a179b90b900960
Eugene Korniuk <korniuk77@gmail.com>
2023-10-10 22:19:47 +0300

    fix some style mistakes

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,19 +2,19 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      int startPositionX = robot.getX();
-      int startPositionY = robot.getY();
-      robot.stepForward();
-      if (robot.getX() > startPositionX && startPositionX > toX) {
-          robot.turnRight();
-          robot.turnRight();
-      } else if (robot.getX() < startPositionX && startPositionX < toX) {
-          robot.turnLeft();
-          robot.turnLeft();
-      }
-      while (robot.getX() != toX) {
+        int startPositionX = robot.getX();
+        int startPositionY = robot.getY();
+        robot.stepForward();
+        if (robot.getX() > startPositionX && startPositionX > toX) {
+            robot.turnRight();
+            robot.turnRight();
+        } else if (robot.getX() < startPositionX && startPositionX < toX) {
+            robot.turnLeft();
+            robot.turnLeft();
+        }
+        while (robot.getX() != toX) {
           robot.stepForward();
-      }
+        }
         if (robot.getY() > startPositionY && startPositionY > toY) {
             robot.turnRight();
             robot.turnRight();
