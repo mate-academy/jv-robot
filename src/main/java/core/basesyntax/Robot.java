1c287e27f19d0225017708b898beb469c5e3e895
Snizhana Sosiedova <snejik1111@gmail.com>
2023-10-14 00:32:13 +0300

    Implemented the logic in the moveRobot method of the RobotRoute class.

--- src/main/java/core/basesyntax/Robot.java
+++ src/main/java/core/basesyntax/Robot.java
@@ -79,4 +79,30 @@ public class Robot {
                 break;
         }
     }
+
+    public void stepForward(int stepsToMake) {
+        for (int i = 1; i <= stepsToMake; i++) {
+            this.stepForward();
+        }
+    }
+
+    public void changeDirection(Direction targetDirection) {
+        Direction currentDirection = this.getDirection();
+        boolean needMoveToTheRight = needMoveToTheRight(currentDirection, targetDirection);
+        while (currentDirection != targetDirection) {
+            if (needMoveToTheRight) {
+                this.turnRight();
+            } else {
+                this.turnLeft();
+            }
+            currentDirection = this.getDirection();
+        }
+    }
+
+    private boolean needMoveToTheRight(Direction currentDirection, Direction targetDirection) {
+        return (currentDirection == Direction.DOWN && targetDirection == Direction.LEFT)
+                || (currentDirection == Direction.LEFT && targetDirection == Direction.UP)
+                || (currentDirection == Direction.UP && targetDirection == Direction.RIGHT)
+                || (currentDirection == Direction.RIGHT && targetDirection == Direction.DOWN);
+    }
 }
