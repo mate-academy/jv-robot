520177762360cb11af0ca78ff0d89a70a6442662
Slava_Moiseev <qualityengineerinkom@gmail.com>
2023-10-16 20:45:48 +0300

    Robot return tot home

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,49 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX < robot.getX()) {
+            if (robot.getDirection() != Direction.LEFT) {
+                do {
+                    robot.turnLeft();
+                } while (robot.getDirection() != Direction.LEFT);
+            }
+            do {
+                robot.stepForward();
+            } while (toX != robot.getX());
+        }
+        if (toX > robot.getX()) {
+            if (robot.getDirection() != Direction.RIGHT) {
+                do {
+                    robot.turnRight();
+                } while (robot.getDirection() != Direction.RIGHT);
+            }
+            do {
+                robot.stepForward();
+            } while (toX != robot.getX());
+        }
+        if (toX == robot.getX() && toY == robot.getY()) {
+            return;
+        }
+
+        if (robot.getY() < toY) {
+            if (robot.getDirection() != Direction.UP) {
+                do {
+                    robot.turnLeft();
+                } while (robot.getDirection() != Direction.UP);
+            }
+            do {
+                robot.stepForward();
+            } while (toY != robot.getY());
+        }
+        if (robot.getY() > toY) {
+            if (robot.getDirection() != Direction.DOWN) {
+                do {
+                    robot.turnLeft();
+                } while (robot.getDirection() != Direction.DOWN);
+                do {
+                    robot.stepForward();
+                } while (toY != robot.getY());
+            }
+        }
     }
 }
