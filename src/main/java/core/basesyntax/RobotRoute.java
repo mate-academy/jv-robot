9d6735ce5c8b8b27a71267c144f3131b46ad411d
Bohdan Kost <kostbogdan@icloud.com>
2023-10-22 14:09:06 +0300

    'RobotRoute' method implementation

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,15 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getDirection() != Direction.RIGHT) {
+            robot.turnRight();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        robot.turnLeft();
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
