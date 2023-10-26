50834265addb45ccc379486b03486ff522f8caef
Eugene Bormotov <bormotovjenya2510@gmail.com>
2023-10-26 20:50:28 +0200

    I completed the task according to the conditions. I hope it's correct.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    public static void main(String[] args) {
+
+    }
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnLeft();
+            }
+            while (toX != robot.getX()) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getX() <= toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+            while (toX != robot.getX()) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getY() < toY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            while (toY != robot.getY()) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getY() >= toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+            while (toY != robot.getY()) {
+                robot.stepForward();
+            }
+        }
     }
 }
