ae7d47615a212cb8d9718326388adc62150a5e14
MaksOsypchuk <Promaks_ua@icloud.com>
2023-10-26 12:15:29 +0300

    Implemented the movement of the robot to the starting point using the if and whail operators. But displayed the start and end points on the screen.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,41 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //go x
+        while (toX != robot.getX()) {
+            if (toX < robot.getX()) {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+            } else {
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            }
+        }
+        //go y
+        while (toY != robot.getY()) {
+            if (toY < robot.getY()) {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+            } else {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Robot robot = new Robot(Direction.UP, 4, 7);
+        RobotRoute robotRoute = new RobotRoute();
+        System.out.println(robot.getX() + " " + robot.getY() + " " + robot.getDirection());
+        robotRoute.moveRobot(robot, 1, 1);
+        System.out.println(robot.getX() + " " + robot.getY() + " " + robot.getDirection());
     }
 }
