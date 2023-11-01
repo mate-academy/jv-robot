3b1763226dcb841d54ef2948facfd8fcc88a25d8
Bohdan <bohdan.g.54@gmail.com>
2023-11-01 03:56:13 +0200

    initial commit

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int deltaX = toX - robot.getX();
+        int deltaY = toY - robot.getY();
+
+        if (deltaX > 0) {
+            turnToDirection(robot, Direction.RIGHT);
+            moveForward(robot, deltaX);
+        } else if (deltaX < 0) {
+            turnToDirection(robot, Direction.LEFT);
+            moveForward(robot, -deltaX);
+        }
+
+        if (deltaY > 0) {
+            turnToDirection(robot, Direction.UP);
+            moveForward(robot, deltaY);
+        } else if (deltaY < 0) {
+            turnToDirection(robot, Direction.DOWN);
+            moveForward(robot, -deltaY);
+        }
     }
+
+    private void turnToDirection(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            robot.turnRight();
+        }
+    }
+
+    private void moveForward(Robot robot, int steps) {
+        for (int i = 0; i < steps; i++) {
+            robot.stepForward();
+        }
+    }
+
 }
