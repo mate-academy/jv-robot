f0a94f19b24c887b5f3b91158fd4cad1fb09e86a
Tetiana Khrestina <khrestinatatiana@gmail.com>
2023-11-08 20:13:34 +0200

    Implement the logic in the moveRobot method of the RobotRoute class to navigate the robot from its current position to the target position

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,47 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // Отримуємо поточні координати та напрямок робота
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+        Direction currentDirection = robot.getDirection();
+
+        // Визначаємо напрямок руху по X (вліво або вправо)
+        if (currentX < toX) {
+            while (currentDirection != Direction.RIGHT) {
+                robot.turnRight();
+                currentDirection = robot.getDirection();
+            }
+        } else if (currentX > toX) {
+            while (currentDirection != Direction.LEFT) {
+                robot.turnLeft();
+                currentDirection = robot.getDirection();
+            }
+        }
+
+        // Рухаємося до цільової координати X
+        while (currentX != toX) {
+            robot.stepForward();
+            currentX = robot.getX();
+        }
+
+        // Визначаємо напрямок руху по Y (вгору або вниз)
+        if (currentY < toY) {
+            while (currentDirection != Direction.UP) {
+                robot.turnLeft();
+                currentDirection = robot.getDirection();
+            }
+        } else if (currentY > toY) {
+            while (currentDirection != Direction.DOWN) {
+                robot.turnRight();
+                currentDirection = robot.getDirection();
+            }
+        }
+
+        // Рухаємося до цільової координати Y
+        while (currentY != toY) {
+            robot.stepForward();
+            currentY = robot.getY();
+        }
     }
 }
