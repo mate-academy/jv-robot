b39e263dc53433b8814c9e5689c1954ed16c3c87
artem <artemsa223@gmail.com>
2023-11-12 16:15:18 +0000

    add method moveRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,38 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Robot robot1 = robot;
+        if (toX < 0) {
+            while (robot1.getDirection() != Direction.LEFT) {
+                robot1.turnLeft();
+            }
+            while (robot1.getX() != toX) {
+                robot1.stepForward();
+            }
+        }
+        if (toX > 0) {
+            while (robot1.getDirection() != Direction.RIGHT) {
+                robot1.turnRight();
+            }
+            while (robot1.getX() != toX) {
+                robot1.stepForward();
+            }
+        }
+        if (toY < 0) {
+            while (robot1.getDirection() != Direction.DOWN) {
+                robot1.turnLeft();
+            }
+            while (robot1.getY() != toY) {
+                robot1.stepForward();
+            }
+        }
+        if (toY > 0) {
+            while (robot1.getDirection() != Direction.UP) {
+                robot1.turnRight();
+            }
+            while (robot1.getY() != toY) {
+                robot1.stepForward();
+            }
+        }
     }
 }
