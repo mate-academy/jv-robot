602b50a7a3faa610b1ff217b3c47e77b9d03f95d
Kinereiko <kinereiko@gmail.com>
2023-11-20 16:43:21 +0200

    implement RobotRoute method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,96 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() != toX) {
+            setDirectionX(robot, toX);
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() != toY) {
+            setDirectionY(robot, toY);
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    public void setDirectionX(Robot robot, int x) {
+        Direction direction = robot.getDirection();
+        if (robot.getX() < x) {
+            if (direction != Direction.RIGHT) {
+                switch (direction) {
+                    case UP:
+                        robot.turnRight();
+                        break;
+                    case DOWN:
+                        robot.turnLeft();
+                        break;
+                    case LEFT:
+                        robot.turnRight();
+                        robot.turnRight();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } else {
+            if (robot.getDirection() != Direction.LEFT) {
+                switch (direction) {
+                    case UP:
+                        robot.turnLeft();
+                        break;
+                    case DOWN:
+                        robot.turnRight();
+                        break;
+                    case RIGHT:
+                        robot.turnLeft();
+                        robot.turnLeft();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    public void setDirectionY(Robot robot, int y) {
+        Direction direction = robot.getDirection();
+        if (robot.getY() < y) {
+            if (direction != Direction.UP) {
+                switch (direction) {
+                    case LEFT:
+                        robot.turnRight();
+                        break;
+                    case RIGHT:
+                        robot.turnLeft();
+                        break;
+                    case DOWN:
+                        robot.turnRight();
+                        robot.turnRight();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } else {
+            if (direction != Direction.DOWN) {
+                switch (direction) {
+                    case LEFT:
+                        robot.turnLeft();
+                        break;
+                    case RIGHT:
+                        robot.turnRight();
+                        break;
+                    case UP:
+                        robot.turnLeft();
+                        robot.turnLeft();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
