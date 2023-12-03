c65d8c123759820e864aa564c8dbdc26d0a87d89
skillfuldev <chernonog.evgeniy@gmail.com>
2023-12-03 11:10:52 +0200

    added_hw-jv-robot-solution_separated_methods

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,69 +3,85 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         if (robot.getX() > toX) {
-            for (int i = robot.getX(); i > toX; i--) {
-                if (robot.getDirection() == Direction.RIGHT) {
-                    robot.turnLeft();
-                    robot.turnLeft();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.UP) {
-                    robot.turnLeft();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.LEFT) {
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.DOWN) {
-                    robot.turnRight();
-                    robot.stepForward();
-                }
-            }
+            moveToXWhenXLessThanCurrentX(robot, toX);
         } else {
-            for (int i = robot.getX(); i < toX; i++) {
-                if (robot.getDirection() == Direction.LEFT) {
-                    robot.turnRight();
-                    robot.turnRight();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.UP) {
-                    robot.turnRight();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.RIGHT) {
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.DOWN) {
-                    robot.turnLeft();
-                    robot.stepForward();
-                }
-            }
+            moveToXWhenXGreatThanCurrentX(robot, toX);
         }
         if (robot.getY() > toY) {
-            for (int i = robot.getY(); i > toY; i--) {
-                if (robot.getDirection() == Direction.RIGHT) {
-                    robot.turnRight();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.UP) {
-                    robot.turnRight();
-                    robot.turnRight();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.LEFT) {
-                    robot.turnLeft();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.DOWN) {
-                    robot.stepForward();
-                }
-            }
+            moveToYWhenYlessThanCurrentY(robot, toY);
         } else {
-            for (int i = robot.getY(); i < toY; i++) {
-                if (robot.getDirection() == Direction.LEFT) {
-                    robot.turnRight();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.UP) {
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.RIGHT) {
-                    robot.turnLeft();
-                    robot.stepForward();
-                } else if (robot.getDirection() == Direction.DOWN) {
-                    robot.turnLeft();
-                    robot.turnLeft();
-                    robot.stepForward();
-                }
+            moveToYWhenYGreatThanCurrentY(robot, toY);
+        }
+    }
+
+    private static void moveToYWhenYGreatThanCurrentY(Robot robot, int toY) {
+        for (int i = robot.getY(); i < toY; i++) {
+            if (robot.getDirection() == Direction.LEFT) {
+                robot.turnRight();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.UP) {
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.RIGHT) {
+                robot.turnLeft();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.DOWN) {
+                robot.turnLeft();
+                robot.turnLeft();
+                robot.stepForward();
+            }
+        }
+    }
+
+    private static void moveToYWhenYlessThanCurrentY(Robot robot, int toY) {
+        for (int i = robot.getY(); i > toY; i--) {
+            if (robot.getDirection() == Direction.RIGHT) {
+                robot.turnRight();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.UP) {
+                robot.turnRight();
+                robot.turnRight();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.LEFT) {
+                robot.turnLeft();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.DOWN) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    private static void moveToXWhenXGreatThanCurrentX(Robot robot, int toX) {
+        for (int i = robot.getX(); i < toX; i++) {
+            if (robot.getDirection() == Direction.LEFT) {
+                robot.turnRight();
+                robot.turnRight();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.UP) {
+                robot.turnRight();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.RIGHT) {
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.DOWN) {
+                robot.turnLeft();
+                robot.stepForward();
+            }
+        }
+    }
+
+    private static void moveToXWhenXLessThanCurrentX(Robot robot, int toX) {
+        for (int i = robot.getX(); i > toX; i--) {
+            if (robot.getDirection() == Direction.RIGHT) {
+                robot.turnLeft();
+                robot.turnLeft();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.UP) {
+                robot.turnLeft();
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.LEFT) {
+                robot.stepForward();
+            } else if (robot.getDirection() == Direction.DOWN) {
+                robot.turnRight();
+                robot.stepForward();
             }
         }
     }
