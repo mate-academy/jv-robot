968d5f73044eed5d68ef88023853a23293adf627
Orest 40th <student2528@bmet.org.ua>
2023-12-03 03:46:44 +0200

    implemented robotroute with fixes

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,8 +2,12 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
         int initialX = robot.getX();
+        if (robot.getX() == toX && robot.getY() != toY) {
+            //reset the robot whack it a bit out of place
+            robot.turnLeft();
+        }
         robot.stepForward();
         //totheLEEEFTt
         if (robot.getX() < toX) {
@@ -17,8 +21,7 @@ public class RobotRoute {
                     robot.turnLeft();
                     robot.stepForward();
                     robot.getX();
-                }
-                while (robot.getX() < initialX);
+                } while (robot.getX() < initialX);
                 while (robot.getX() != toX) {
                     robot.stepForward();
                     robot.getX();
@@ -38,8 +41,7 @@ public class RobotRoute {
                     robot.turnLeft();
                     robot.stepForward();
                     robot.getX();
-                }
-                while (robot.getX() > initialX);
+                } while (robot.getX() > initialX);
                 while (robot.getX() != toX) {
                     robot.stepForward();
                     robot.getX();
@@ -47,43 +49,39 @@ public class RobotRoute {
             }
             moveRobotAlongYRight(robot, toY);
         }
-        robot.getDirection();
     }
+
     public void moveRobotAlongYRight(Robot robot, int toY) {
         if (robot.getY() > toY) {
             robot.turnLeft();
-            while (robot.getY() > toY ) {
+            while (robot.getY() > toY) {
                 robot.stepForward();
                 robot.getY();
             }
         }
         if (robot.getY() < toY) {
             robot.turnRight();
-            while (robot.getY() < toY ) {
+            while (robot.getY() < toY) {
                 robot.stepForward();
                 robot.getY();
             }
         }
-
     }
+
     public void moveRobotAlongYLeft(Robot robot, int toY) {
         if (robot.getY() > toY) {
             robot.turnRight();
-            while (robot.getY() > toY ) {
+            while (robot.getY() > toY) {
                 robot.stepForward();
                 robot.getY();
             }
         }
         if (robot.getY() < toY) {
             robot.turnLeft();
-            while (robot.getY() < toY ) {
+            while (robot.getY() < toY) {
                 robot.stepForward();
                 robot.getY();
             }
         }
-
     }
-    //public void moveRobotAlongXLeft(Robot robot, int toY) {
-
-    //}
 }
