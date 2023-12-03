5d54d26ae8e197735794f913a7bffd729d640469
Orest 40th <student2528@bmet.org.ua>
2023-12-03 02:22:32 +0200

    roughly sketched the move method in  RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,87 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        int initialX = robot.getX();
+        robot.stepForward();
+        //totheLEEEFTt
+        if (robot.getX() < toX) {
+            if (robot.getX() > initialX) {
+                while (robot.getX() != toX) {
+                    robot.stepForward();
+                    robot.getX();
+                }
+            } else {
+                do {
+                    robot.turnLeft();
+                    robot.stepForward();
+                    robot.getX();
+                }
+                while (robot.getX() < initialX);
+                while (robot.getX() != toX) {
+                    robot.stepForward();
+                    robot.getX();
+                }
+            }
+            moveRobotAlongYLeft(robot, toY);
+        }
+        //ttheRIGHT
+        if (robot.getX() > toX) {
+            if (robot.getX() < initialX) {
+                while (robot.getX() != toX) {
+                    robot.stepForward();
+                    robot.getX();
+                }
+            } else {
+                do {
+                    robot.turnLeft();
+                    robot.stepForward();
+                    robot.getX();
+                }
+                while (robot.getX() > initialX);
+                while (robot.getX() != toX) {
+                    robot.stepForward();
+                    robot.getX();
+                }
+            }
+            moveRobotAlongYRight(robot, toY);
+        }
+        robot.getDirection();
     }
+    public void moveRobotAlongYRight(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            robot.turnLeft();
+            while (robot.getY() > toY ) {
+                robot.stepForward();
+                robot.getY();
+            }
+        }
+        if (robot.getY() < toY) {
+            robot.turnRight();
+            while (robot.getY() < toY ) {
+                robot.stepForward();
+                robot.getY();
+            }
+        }
+
+    }
+    public void moveRobotAlongYLeft(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            robot.turnRight();
+            while (robot.getY() > toY ) {
+                robot.stepForward();
+                robot.getY();
+            }
+        }
+        if (robot.getY() < toY) {
+            robot.turnLeft();
+            while (robot.getY() < toY ) {
+                robot.stepForward();
+                robot.getY();
+            }
+        }
+
+    }
+    //public void moveRobotAlongXLeft(Robot robot, int toY) {
+
+    //}
 }
