7809bc5d1d9e29612a9e9143d3a65f21e972eab1
Yevhen <shymeiko.zenya@gmail.com>
2023-12-10 16:09:31 +0200

    implementRobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,107 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveAlongX(robot, toX);
+        moveAlongY(robot, toY);
+    }
+
+    private void moveAlongX(Robot robot, int toX) {
+        if (toX > robot.getX()) {
+            changeDirectionToRight(robot);
+        } else {
+            changeDirectionToLeft(robot);
+        }
+
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+    }
+
+    private void moveAlongY(Robot robot, int toY) {
+        if (toY > robot.getY()) {
+            changeDirectionToUp(robot);
+        } else {
+            changeDirectionToDown(robot);
+        }
+
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private void changeDirectionToUp(Robot robot) {
+        switch (robot.getDirection()) {
+            case UP:
+                break;
+            case DOWN:
+                robot.turnLeft();
+                robot.turnLeft();
+                break;
+            case LEFT:
+                robot.turnRight();
+                break;
+            case RIGHT:
+                robot.turnLeft();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void changeDirectionToDown(Robot robot) {
+        switch (robot.getDirection()) {
+            case UP:
+                robot.turnLeft();
+                robot.turnLeft();
+                break;
+            case DOWN:
+                break;
+            case LEFT:
+                robot.turnLeft();
+                break;
+            case RIGHT:
+                robot.turnRight();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void changeDirectionToLeft(Robot robot) {
+        switch (robot.getDirection()) {
+            case UP:
+                robot.turnLeft();
+                break;
+            case DOWN:
+                robot.turnRight();
+                break;
+            case LEFT:
+                break;
+            case RIGHT:
+                robot.turnRight();
+                robot.turnRight();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void changeDirectionToRight(Robot robot) {
+        switch (robot.getDirection()) {
+            case UP:
+                robot.turnRight();
+                break;
+            case DOWN:
+                robot.turnLeft();
+                break;
+            case LEFT:
+                robot.turnRight();
+                robot.turnRight();
+                break;
+            case RIGHT:
+                break;
+            default:
+                break;
+        }
     }
 }
