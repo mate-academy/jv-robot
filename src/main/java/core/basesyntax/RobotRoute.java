792fa41ef858f43986d61f701c74e43d62c87bf9
Artem <artem.kozakevych@gmail.com>
2023-12-18 00:35:19 +0200

    release RobotRoute class

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,24 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        while (robot.getX() > toX && robot.getDirection() == Direction.RIGHT) {
+            robot.turnLeft();
+        }
+        while (robot.getX() < toX && robot.getDirection() == Direction.LEFT) {
+            robot.turnRight();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        while (robot.getY() > toY && robot.getDirection() == Direction.UP) {
+            robot.turnLeft();
+        }
+        while (robot.getY() < toY && robot.getDirection() == Direction.DOWN) {
+            robot.turnRight();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
+
