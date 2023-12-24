68025dcdf11c098d8e6d92481b46f3317b44efa1
Soqiable <sooqiable@gmail.com>
2023-12-24 12:57:02 +0200

    [jv-robot]: home-work solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //  determination of X coordinate
+        if (toX < robot.getX()) {
+            do {
+                robot.turnLeft();
+            } while (robot.getDirection() != Direction.LEFT);
+        } else {
+            do {
+                robot.turnRight();
+            } while (robot.getDirection() != Direction.RIGHT);
+        }
+
+        int amountStepsX = Math.abs(toX - robot.getX());
+        for (int i = 1; i <= amountStepsX; i++) {
+            robot.stepForward();
+        }
+
+        // determination of Y coordinate
+        if (toY < robot.getY()) {
+            do {
+                robot.turnLeft();
+            } while (robot.getDirection() != Direction.DOWN);
+        } else {
+            do {
+                robot.turnRight();
+            } while (robot.getDirection() != Direction.UP);
+        }
+
+        int amountStepsY = Math.abs(toY - robot.getY());
+        for (int j = 1; j <= amountStepsY; j++) {
+            robot.stepForward();
+        }
     }
 }
