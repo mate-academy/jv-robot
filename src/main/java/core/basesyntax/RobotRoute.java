dc0d08d7f3ff8913e21e3c1172bad53174477aab
Vitaliy <vitaliyufc@gmail.com>
2024-01-04 20:29:30 +0300

    Solution for robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,27 +1,34 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
+    private void moveHorizontally(Robot robot, int toX) {
         int currentX = robot.getX();
-        Direction targetDirection1 = (currentX < toX) ? Direction.RIGHT : Direction.LEFT;
-        while (robot.getX() != toX && robot.getDirection() != targetDirection1) {
-            if (targetDirection1 == Direction.RIGHT) {
+        Direction targetDirection = (currentX < toX) ? Direction.RIGHT : Direction.LEFT;
+        while (robot.getX() != toX && robot.getDirection() != targetDirection) {
+            if (targetDirection == Direction.RIGHT) {
                 robot.turnRight();
             } else {
                 robot.turnLeft();
             }
             robot.stepForward();
         }
+    }
 
+    private void moveVertically(Robot robot, int toY) {
         int currentY = robot.getY();
-        Direction targetDirection2 = (currentY < toY) ? Direction.UP : Direction.DOWN;
-        while (robot.getY() != toY && robot.getDirection() != targetDirection2) {
-            if (targetDirection2 == Direction.RIGHT) {
+        Direction targetDirection = (currentY < toY) ? Direction.UP : Direction.DOWN;
+        while (robot.getY() != toY && robot.getDirection() != targetDirection) {
+            if (targetDirection == Direction.RIGHT) {
                 robot.turnRight();
             } else {
                 robot.turnLeft();
             }
             robot.stepForward();
         }
+    }
+
+    public void moveRobot(Robot robot, int toX, int toY) {
+        moveHorizontally(robot, toX);
+        moveVertically(robot, toY);
     }
 }
