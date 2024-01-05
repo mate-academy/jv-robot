d4cfff044e3f951a691bada7839f292749b55c9b
Bohdan Buhrak <bohdan0410@gmail.com>
2024-01-05 22:26:29 +0200

    task solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -4,9 +4,7 @@ public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         //write your solution here
 
-        //write your solution here
         int deltaX = toX - robot.getX();
-        int deltaY = toY - robot.getY();
 
         while (deltaX > 0 && robot.getDirection() != Direction.RIGHT) {
             robot.turnLeft();
@@ -20,6 +18,8 @@ public class RobotRoute {
             robot.stepForward();
             deltaX = toX - robot.getX();
         }
+
+        int deltaY = toY - robot.getY();
 
         while (deltaY > 0 && robot.getDirection() != Direction.UP) {
             robot.turnRight();
