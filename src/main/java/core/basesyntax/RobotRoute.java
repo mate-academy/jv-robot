32c2f7f488dc3a75e9614a586a2fc0886ffb89c9
Dariia <daradyradara@gmail.com>
2024-01-11 15:38:27 +0200

    Logic has been implemented in RobotRoute.java.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,26 @@
 package core.basesyntax;
 
+import javax.swing.plaf.basic.BasicArrowButton;
+
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX || robot.getY() != toY) {
+            while (robot.getDirection() != getDirectionToTarget(robot, toX, toY)) {
+                robot.turnRight();
+            }
+            robot.stepForward();
+        }
+    }
+
+    private Direction getDirectionToTarget(Robot robot, int toX, int toY) {
+        if (toY > robot.getY()) {
+            return Direction.UP;
+        } else if (toY < robot.getY()) {
+            return Direction.DOWN;
+        } else if (toX > robot.getX()) {
+            return Direction.RIGHT;
+        } else {
+            return Direction.LEFT;
+        }
     }
 }
