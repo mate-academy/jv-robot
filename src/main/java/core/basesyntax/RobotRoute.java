6c25e6c4fcc9a24c9446140d8fc37f9c7467a416
Artem Akymenko <akimenko.artem@gmail.com>
2024-01-21 15:45:44 +0200

    Implemented the logic in the moveRobot method of the RobotRoute class

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    // Method has less than 30 lines so I decided not to divide this method.
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX || robot.getY() != toY) {
+            boolean isTurnRight;
+            switch (robot.getDirection()) {
+                case UP -> {
+                    robot.stepForward();
+                    isTurnRight = toX > robot.getX();
+                }
+                case DOWN -> {
+                    robot.stepForward();
+                    isTurnRight = toX < robot.getX();
+                }
+                case LEFT -> {
+                    robot.stepForward();
+                    isTurnRight = toY > robot.getY();
+                }
+                case RIGHT -> {
+                    robot.stepForward();
+                    isTurnRight = toY < robot.getY();
+                }
+                default -> isTurnRight = false;
+            }
+            if (isTurnRight) {
+                robot.turnRight();
+            } else {
+                robot.turnLeft();
+            }
+        }
     }
 }
+
