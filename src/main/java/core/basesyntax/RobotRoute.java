b28a868c0e12d49e7070f368ad5e4ab4829e86f2
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-01-24 14:37:56 +0200

    main example

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,33 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        //write your solution here
+        int robotX = robot.getX();
+        int robotY = robot.getY();
+
+        // To equal X coords
+        if (robotX < toX) {
+            robot.turnRight();
+        } else if (robotX > toX) {
+            robot.turnLeft();
+        }
+        while (robotX != toX) {
+            robot.stepForward();
+        }
+
+        // To equal Y coords
+        if (robotY < toY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+        } else if (robotY > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+        }
+        while (robotY != toY) {
+            robot.stepForward();
+        }
     }
 }
