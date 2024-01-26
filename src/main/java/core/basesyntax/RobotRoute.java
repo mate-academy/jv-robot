2bc10d37949db75ccb15be53412e23c1f6ddfc8b
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-01-26 12:58:35 +0200

    implemented pv moveRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,27 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currrentX = robot.getX();
+        int currentY = robot.getY();
+        int deltaX = toX - robot.getX();
+        while (deltaX > 0 && robot.getDirection() != Direction.RIGHT) {
+            robot.turnRight();
+        }
+        while (deltaX < 0 && robot.getDirection() != Direction.LEFT) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        int deltaY = toY - robot.getY();
+        while (deltaY > 0 && robot.getDirection() != Direction.UP) {
+            robot.turnRight();
+        }
+        while (deltaY < 0 && robot.getDirection() != Direction.DOWN) {
+            robot.turnLeft();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
