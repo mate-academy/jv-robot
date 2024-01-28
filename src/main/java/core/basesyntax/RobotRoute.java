e283e429a7f4c5694fcdb2e3a8616783d8c09dab
Kacper <kacperkukula97@gmail.com>
2024-01-28 02:30:17 +0100

    First Solution, added logic to moveRobot() and two methods to face the correct way

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,40 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+
+        faceCorrectWayToX(robot.getX(), toX, robot);
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        faceCorrectWayToY(robot.getY(), toY, robot);
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+
+    }
+
+    private void faceCorrectWayToX(int currentPos, int toPos, Robot robot) {
+        if (currentPos > toPos) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+        } else if (currentPos < toPos) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+        }
+    }
+
+    private void faceCorrectWayToY(int currentPos, int toPos, Robot robot) {
+        if (currentPos > toPos) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+        } else if (currentPos < toPos) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+        }
     }
 }
