7a96be106a7fef5f29c0f50e522b5f3d0facb6ac
Myroslav Stefanchuk <stefanchykmyroslav@gmail.com>
2024-01-31 16:55:30 +0100

    added implementation for the method moveRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,27 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+
+        int stepsX = toX - currentX;
+        int stepsY = toY - currentY;
+
+        moveInDirection(robot, stepsX, Direction.RIGHT, Direction.LEFT);
+        moveInDirection(robot, stepsY, Direction.UP, Direction.DOWN);
+    }
+
+    private void moveInDirection(Robot robot, int steps, Direction positive, Direction negative) {
+        while (robot.getDirection() != getDirectionForSteps(steps, positive, negative)) {
+            robot.turnRight();
+        }
+
+        for (int i = 0; i < Math.abs(steps); i++) {
+            robot.stepForward();
+        }
+    }
+
+    private Direction getDirectionForSteps(int steps, Direction positive, Direction negative) {
+        return (steps > 0) ? positive : negative;
     }
 }
