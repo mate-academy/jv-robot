8908c803d72591795be5cc7b53a460a85b918952
Patryk Tyl <tyl.pat@wp.pl>
2024-02-07 19:05:17 +0100

    getting rid of the infinite loop

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
     public void moveRobot(Robot robot, int toX, int toY) {
         int currentX = robot.getX();
         int currentY = robot.getY();
@@ -8,28 +9,22 @@ public class RobotRoute {
         Direction directionX = currentX < toX ? Direction.RIGHT : Direction.LEFT;
         Direction directionY = currentY < toY ? Direction.UP : Direction.DOWN;
 
-        while (robot.getDirection() != directionX) {
-            if (robot.getDirection() == Direction.UP || robot.getDirection() == Direction.DOWN) {
-                robot.turnLeft();
-            } else {
-                robot.turnRight();
+        while (currentX != toX || currentY != toY) {
+            if (currentX != toX) {
+                while (robot.getDirection() != directionX) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentX = robot.getX();
             }
-        }
 
-        while (robot.getX() != toX) {
-            robot.stepForward();
-        }
-
-        while (robot.getDirection() != directionY) {
-            if (robot.getDirection() == Direction.RIGHT || robot.getDirection() == Direction.LEFT) {
-                robot.turnLeft();
-            } else {
-                robot.turnRight();
+            if (currentY != toY) {
+                while (robot.getDirection() != directionY) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentY = robot.getY();
             }
-        }
-
-        while (robot.getY() != toY) {
-            robot.stepForward();
         }
     }
 }
