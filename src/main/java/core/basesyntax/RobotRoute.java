3f1586b41c7fff634fc27804dbf57dc5c2c33101
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-02-11 17:10:56 +0200

    refactored

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,31 +2,38 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        int startDistanceX = Math.abs(robot.getX()) + Math.abs(toX);
-        int startDistanceY = Math.abs(robot.getY()) + Math.abs(toY);
+        robotReachX(robot, toX);
+        robotReachY(robot, toY);
+    }
+
+    private void robotReachX(Robot robot, int destinationX) {
+        int startDistanceX = Math.abs(robot.getX()) + Math.abs(destinationX);
         while (!robot.getDirection().equals(Direction.RIGHT)) {
             robot.turnRight();
         }
         robot.stepForward();
-        if (Math.abs(robot.getX()) + Math.abs(toX) > startDistanceX) {
+        if (Math.abs(robot.getX()) + Math.abs(destinationX) > startDistanceX) {
             while (!robot.getDirection().equals(Direction.LEFT)) {
                 robot.turnRight();
             }
         }
-        while (robot.getX() != toX) {
+        while (robot.getX() != destinationX) {
             robot.stepForward();
         }
+    }
 
+    private void robotReachY(Robot robot, int destinationY) {
+        int startDistanceY = Math.abs(robot.getY()) + Math.abs(destinationY);
         while (!robot.getDirection().equals(Direction.UP)) {
             robot.turnRight();
         }
         robot.stepForward();
-        if (Math.abs(robot.getY()) + Math.abs(toY) > startDistanceY) {
+        if (Math.abs(robot.getY()) + Math.abs(destinationY) > startDistanceY) {
             while (!robot.getDirection().equals(Direction.DOWN)) {
                 robot.turnRight();
             }
         }
-        while (robot.getY() != toY) {
+        while (robot.getY() != destinationY) {
             robot.stepForward();
         }
     }
