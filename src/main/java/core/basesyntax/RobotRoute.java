18aca92b2133edb5faa0a47e5327b3e6a4220266
Shr1mpa <aminakrevetka@gmail.com>
2024-02-12 10:37:37 +0200

    solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -3,5 +3,40 @@ package core.basesyntax;
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
       //write your solution here
+        reachX(robot, toX);
+        reachY(robot, toY);
+
+
     }
+    private void reachX(Robot robot, int toX) {
+        int distanceX = Math.abs(robot.getX() + Math.abs(toX));
+        while (!robot.getDirection().equals(Direction.RIGHT)) {
+            robot.turnRight();
+        }
+        robot.stepForward();
+        if(Math.abs(robot.getX() + Math.abs(toX)) > distanceX){
+            while (!robot.getDirection().equals(Direction.LEFT)){
+                robot.turnLeft();
+            }
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+    }
+    private void reachY(Robot robot, int toY){
+        int distanceY = Math.abs(robot.getY() + Math.abs(toY));
+        while (!robot.getDirection().equals(Direction.UP)) {
+            robot.turnRight();
+        }
+        robot.stepForward();
+        if(Math.abs(robot.getY() + Math.abs(toY)) > distanceY){
+            while (!robot.getDirection().equals(Direction.DOWN)){
+                robot.turnLeft();
+            }
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
 }
