925893664cdc98211a424c901703f037276e20aa
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-09 00:07:20 +0200

    Added implementation of moving robot to target coordinates

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,29 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX || robot.getY() != toY) {
+            if (robot.getX() != toX) {
+                moveAxis(robot, toX, true);
+            }
+            if (robot.getY() != toY) {
+                moveAxis(robot, toY, false);
+            }
+        }
+    }
+
+    private void moveAxis(Robot rob, int target, boolean isAxisX) {
+        Direction targetDirection = this.getTargetDirection(rob, target, isAxisX);
+        this.turnRobotToRightDirection(rob, targetDirection);
+        rob.stepForward();
+    }
+
+    private Direction getTargetDirection(Robot rob, int target, boolean isAxisX) {
+        return isAxisX ? rob.getX() > target ? Direction.LEFT : Direction.RIGHT : rob.getY() > target ? Direction.DOWN : Direction.UP;
+    }
+
+    private void turnRobotToRightDirection(Robot rob, Direction targetDirection) {
+        while (rob.getDirection() != targetDirection) {
+            rob.turnRight();
+        }
     }
 }
