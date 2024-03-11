7cdac694c266082186216c82ff58ebe7dc9223a7
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-03-11 09:49:54 +0200

    maybe yes

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,67 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() < toX) {
+            if (robot.getDirection() == Direction.DOWN) {
+                robot.turnRight();
+            }
+            if (robot.getDirection() == Direction.LEFT) {
+                robot.turnRight();
+            }
+            if (robot.getDirection() == Direction.RIGHT) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+
+        }
+
+        if (robot.getX() > toX) {
+            if (robot.getDirection() == Direction.UP) {
+                robot.turnRight();
+            }
+            if (robot.getDirection() == Direction.LEFT) {
+                robot.turnLeft();
+            }
+            if (robot.getDirection() == Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+
+        }
+
+        if (robot.getY() > toY) {
+            if (robot.getDirection() == Direction.UP) {
+                robot.turnLeft();
+            }
+
+            if (robot.getDirection() == Direction.RIGHT) {
+                robot.turnLeft();
+            }
+
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+
+        }
+
+        if (robot.getY() < toY) {
+            if (robot.getDirection() == Direction.DOWN) {
+                robot.turnLeft();
+            }
+
+            if (robot.getDirection() == Direction.UP) {
+                robot.turnRight();
+            }
+
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+
+        }
+
     }
 }
