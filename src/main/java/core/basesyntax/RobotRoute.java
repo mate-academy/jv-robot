8d7969af18717f8228a55f0ea30635db6baefb0b
Anastasiia-Rokytska <anastasiia.rokytska.knm.2019@lpnu.ua>
2024-03-11 14:14:51 +0200

    implement method moveRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,25 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getY() < toY && !robot.getDirection().equals(Direction.UP)) {
+            robot.turnLeft();
+        }
+        while (robot.getY() > toY && !robot.getDirection().equals(Direction.DOWN)) {
+            robot.turnLeft();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+
+        while (robot.getX() < toX && !robot.getDirection().equals(Direction.LEFT)) {
+            robot.turnLeft();
+        }
+        while (robot.getX() > toX && !robot.getDirection().equals(Direction.RIGHT)) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
     }
 }
