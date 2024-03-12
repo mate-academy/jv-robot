88db4a6e5a66d281541d14668b1b68c2ca40ab46
Andrey Chertykovtsev <chertiav@gmail.com>
2024-03-12 16:41:46 +0100

    Implement the logic in the `moveRobot` method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,49 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    private static void moveRobotByDirection(Robot robot, int toX, int toY) {
+        if (robot.getDirection() == Direction.UP || robot.getDirection() == Direction.DOWN) {
+            do {
+                robot.stepForward();
+            } while (robot.getY() != toY);
+        } else {
+            do {
+                robot.stepForward();
+            } while (robot.getX() != toX);
+        }
+    }
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX > 0 && toY > 0 && robot.getDirection() == Direction.UP
+                || toX < 0 && toY > 0 && robot.getDirection() == Direction.LEFT
+                || toX > 0 && toY < 0 && robot.getDirection() == Direction.RIGHT
+                || toX < 0 && toY < 0 && robot.getDirection() == Direction.DOWN) {
+            moveRobotByDirection(robot, toX, toY);
+            robot.turnRight();
+            moveRobotByDirection(robot, toX, toY);
+        } else if (toX > 0 && toY > 0 && robot.getDirection() == Direction.DOWN
+                || toX < 0 && toY > 0 && robot.getDirection() == Direction.RIGHT
+                || toX > 0 && toY < 0 && robot.getDirection() == Direction.LEFT
+                || toX < 0 && toY < 0 && robot.getDirection() == Direction.UP) {
+            robot.turnLeft();
+            moveRobotByDirection(robot, toX, toY);
+            robot.turnLeft();
+            moveRobotByDirection(robot, toX, toY);
+        } else if (toX > 0 && toY > 0 && robot.getDirection() == Direction.LEFT
+                || toX < 0 && toY > 0 && robot.getDirection() == Direction.DOWN
+                || toX > 0 && toY < 0 && robot.getDirection() == Direction.UP
+                || toX < 0 && toY < 0 && robot.getDirection() == Direction.RIGHT) {
+            robot.turnRight();
+            moveRobotByDirection(robot, toX, toY);
+            robot.turnRight();
+            moveRobotByDirection(robot, toX, toY);
+        } else if (toX > 0 && toY > 0 && robot.getDirection() == Direction.RIGHT
+                || toX < 0 && toY > 0 && robot.getDirection() == Direction.UP
+                || toX > 0 && toY < 0 && robot.getDirection() == Direction.DOWN
+                || toX < 0 && toY < 0 && robot.getDirection() == Direction.LEFT) {
+            moveRobotByDirection(robot, toX, toY);
+            robot.turnLeft();
+            moveRobotByDirection(robot, toX, toY);
+        }
     }
 }
