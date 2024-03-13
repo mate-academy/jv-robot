4f3e99d50ea6d35a6052c1f8d8068356495aaa2e
Stepan <oleksiukstepann@gmail.com>
2024-03-13 14:32:19 +0200

    Implemented the logic in the moveRobot method of the RobotRoute class: · create and implement method moveRobot · create and implement method setDirectionToY and setDirectionToX · create and implement method turnToDirection

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,38 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        setDirectionToY(robot, toY);
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+        setDirectionToX(robot, toX);
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
     }
+
+    private void setDirectionToY(Robot robot, int toY) {
+        if (robot.getY() > toY) {
+            turnToDirection(robot, Direction.DOWN);
+        } else {
+            turnToDirection(robot, Direction.UP);
+        }
+    }
+
+    private void setDirectionToX(Robot robot, int toX) {
+        if (robot.getX() > toX) {
+            turnToDirection(robot, Direction.LEFT);
+        } else {
+            turnToDirection(robot, Direction.RIGHT);
+        }
+    }
+
+    private void turnToDirection(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnRight();
+        }
+    }
+
 }
