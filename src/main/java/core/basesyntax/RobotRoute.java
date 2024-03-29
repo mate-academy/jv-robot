ed018c7f980c6fe2cf8ba89867728a245fa0f5b9
Andrii Datko <a.p.datko@gmail.com>
2024-03-29 13:44:00 +0200

    Implemented the moveRobot method.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,49 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() > toX) {
+            while (!robot.getDirection().equals(Direction.LEFT)) {
+                robot.turnLeft();
+                robot.getDirection();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+                robot.getX();
+            }
+        }
+        if (robot.getY() > toY) {
+            while (!robot.getDirection().equals(Direction.DOWN)) {
+                robot.turnLeft();
+                robot.getDirection();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+                robot.getY();
+            }
+        }
+        if (robot.getX() < toX) {
+            while (!robot.getDirection().equals(Direction.RIGHT)) {
+                robot.turnRight();
+                robot.getDirection();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+                robot.getX();
+            }
+        }
+        if (robot.getY() < toY) {
+            while (!robot.getDirection().equals(Direction.UP)) {
+                robot.turnLeft();
+                robot.getDirection();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+                robot.getY();
+            }
+        }
+        if (robot.getX() == toX && robot.getY() == toY) {
+            System.out.println("The robot has reached its destination.");
+        }
     }
 }
+
