7157f6b40389aa456e7e5b3dd89e04f74962adf7
Sofiia Boiko <boikosofiiawork@gmail.com>
2024-04-17 19:24:00 +0300

    add robot mover

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int x = robot.getX();
+        int y = robot.getY();
+
+        rotate(robot, x, toX, Direction.RIGHT, Direction.LEFT);
+
+        while (x != toX) {
+            robot.stepForward();
+            x = robot.getX();
+        }
+
+        rotate(robot, y, toY, Direction.UP, Direction.DOWN);
+
+        while (y != toY) {
+            robot.stepForward();
+            y = robot.getY();
+        }
+    }
+
+    private void rotate(Robot robot, int currentCoordinate, int targetCoordinate,
+                        Direction positiveDirection, Direction negativeDirection) {
+
+        if (currentCoordinate < targetCoordinate) {
+            while (robot.getDirection() != positiveDirection) {
+                robot.turnRight();
+            }
+        } else if (currentCoordinate > targetCoordinate) {
+            while (robot.getDirection() != negativeDirection) {
+                robot.turnLeft();
+            }
+        }
+
     }
 }
