619d74a0cc9381ca3cf8ca604145784ca951a00d
Ivan Demchenko <experywow@gmail.com>
2024-04-24 16:35:04 +0100

    Implementation of moveRobot method via 2 while loops,

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,39 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+        while (currentX != toX) {
+            if (currentX < toX) {
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentX++;
+            }
+            if (currentX > toX) {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+                currentX--;
+            }
+        }
+        while (currentY != toY) {
+            if (currentY < toY) {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentY++;
+            }
+            if (currentY > toY) {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+                currentY--;
+            }
+        }
     }
 }
