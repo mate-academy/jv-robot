09b8cddd8ee94fa7a67a7d6681f2b28b5f571ab7
Anna-Sofia Halas <annasofiia.halas@gmail.com>
2024-04-29 12:44:27 +0300

    fixes

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,36 +2,30 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        int currentX = robot.getX();
-        int currentY = robot.getY();
-
-        int diffX = toX - currentX;
-        int diffY = toY - currentY;
-
-        Direction targetDirectionX = diffX > 0 ? Direction.RIGHT : Direction.LEFT;
-        Direction targetDirectionY = diffY > 0 ? Direction.UP : Direction.DOWN;
-
-        while (robot.getDirection() != targetDirectionX) {
-            if (robot.getDirection() == Direction.UP || robot.getDirection() == Direction.DOWN) {
-                robot.turnRight();
-            } else {
-                robot.turnLeft();
-            }
-        }
-
+        //write your solution here
         while (robot.getX() != toX) {
+            if (robot.getX() < toX) {
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+            } else {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnRight();
+                }
+            }
             robot.stepForward();
         }
 
-        while (robot.getDirection() != targetDirectionY) {
-            if (robot.getDirection() == Direction.RIGHT || robot.getDirection() == Direction.LEFT) {
-                robot.turnRight();
-            } else {
-                robot.turnLeft();
-            }
-        }
-
         while (robot.getY() != toY) {
+            if (robot.getY() < toY) {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+            } else {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnRight();
+                }
+            }
             robot.stepForward();
         }
     }
