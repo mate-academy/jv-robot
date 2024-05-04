5e2f52d18b12b5cca166fe3ab7d33f3fd2f2d63c
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-05-04 19:07:37 +0200

    Solved jv-robot task, (passed all tests)

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,28 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveTo(robot, toX, robot.getX(), Direction.LEFT, Direction.RIGHT);
+        moveTo(robot, toY, robot.getY(), Direction.DOWN, Direction.UP);
+    }
+
+    private void moveTo(Robot robot, int target, int current,
+                        Direction leftDirection, Direction rightDirection) {
+        while (current != target) {
+            if (current < target) {
+                if (robot.getDirection() != rightDirection) {
+                    robot.turnRight();
+                } else {
+                    robot.stepForward();
+                    current++;
+                }
+            } else {
+                if (robot.getDirection() != leftDirection) {
+                    robot.turnLeft();
+                } else {
+                    robot.stepForward();
+                    current--;
+                }
+            }
+        }
     }
 }
