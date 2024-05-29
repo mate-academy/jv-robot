eb08920ac3a20cb77fae5636858725f8edaa7040
Panchenko Dmytro <panchenko0795@gmail.com>
2024-05-29 23:37:46 +0300

    Implemented logic in the `RobotRoute` class that navigates the robot from its current position to the target position

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,37 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                findRightDirection(robot, Direction.LEFT);
+            } else {
+                findRightDirection(robot, Direction.RIGHT);
+            }
+            robot.stepForward();
+        }
+        while (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                findRightDirection(robot, Direction.DOWN);
+            } else {
+                findRightDirection(robot, Direction.UP);
+            }
+            robot.stepForward();
+        }
+    }
+
+    private void findRightDirection(Robot robot, Direction neededDirection) {
+        if (robot.getDirection() != neededDirection) {
+            if ((neededDirection == Direction.LEFT && robot.getDirection() == Direction.DOWN)
+                    || (neededDirection == Direction.UP && robot.getDirection() == Direction.LEFT)
+                    || (neededDirection == Direction.RIGHT && robot.getDirection() == Direction.UP)
+                    || (neededDirection == Direction.DOWN
+                    && robot.getDirection() == Direction.RIGHT)) {
+                robot.turnRight();
+            } else {
+                while (robot.getDirection() != neededDirection) {
+                    robot.turnLeft();
+                }
+            }
+        }
     }
 }
