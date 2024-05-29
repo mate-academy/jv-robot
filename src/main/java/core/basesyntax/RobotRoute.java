72a4514f8262809fe34c90f58a6e216cf9161749
Oleksii <obabych1@stu.vistula.edu.pl>
2024-05-29 19:18:25 +0200

    init commit for homework

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,28 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX) {
+            if (robot.getX() < toX) {
+                changeCurrentPosition(robot, Direction.RIGHT);
+            } else {
+                changeCurrentPosition(robot, Direction.LEFT);
+            }
+            robot.stepForward();
+        }
+
+        while (robot.getY() != toY) {
+            if (robot.getY() < toY) {
+                changeCurrentPosition(robot, Direction.UP);
+            } else {
+                changeCurrentPosition(robot, Direction.DOWN);
+            }
+            robot.stepForward();
+        }
+    }
+
+    private void changeCurrentPosition(Robot robot, Direction direction) {
+        if (robot.getDirection() != direction) {
+            robot.turnRight();
+        }
     }
 }
