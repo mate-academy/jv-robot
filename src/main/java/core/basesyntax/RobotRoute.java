00df6c2a9e2a87c4adea0eda34efe0e436abbbbe
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-06-17 00:37:19 +0300

    Implemented method RoborRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
 public class RobotRoute {
+    private Robot robot;
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (toX > robot.getX()) {
+                robot.stepForward();
+            }
+        } else if (toX < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.stepForward();
+            }
+        }
+        if (toY > robot.getY()) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+            while (toY > robot.getY()) {
+                robot.stepForward();
+            }
+        } else if (toY < robot.getY()) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+            while (toY < robot.getY()) {
+                robot.stepForward();
+            }
+        }
     }
 }
