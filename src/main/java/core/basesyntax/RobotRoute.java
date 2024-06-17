32eebcd93111aeaea4ceee3467f6a7d489b7fd7a
Bohdan <111230201+FireFalcons@users.noreply.github.com>
2024-06-17 21:15:55 +0300

    Formatid text

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,21 +2,21 @@ package core.basesyntax;
 
 public class RobotRoute {
     public static void moveRobot(Robot robot, int toX, int toY) {
-        while (robot.getX() != toX || robot.getY() != toY){
-            if (robot.getX() < toX){
-                while (robot.getDirection() != Direction.RIGHT){
+        while (robot.getX() != toX || robot.getY() != toY) {
+            if (robot.getX() < toX) {
+                while (robot.getDirection() != Direction.RIGHT) {
                     robot.turnRight();
                 }
             } else if (robot.getX() > toX) {
-                while (robot.getDirection() != Direction.LEFT){
+                while (robot.getDirection() != Direction.LEFT) {
                     robot.turnLeft();
                 }
-            } else if (robot.getY() < toY){
-                while (robot.getDirection() != Direction.UP){
+            } else if (robot.getY() < toY) {
+                while (robot.getDirection() != Direction.UP) {
                     robot.turnRight();
                 }
             } else {
-                while (robot.getDirection() != Direction.DOWN){
+                while (robot.getDirection() != Direction.DOWN) {
                     robot.turnLeft();
                 }
             }
