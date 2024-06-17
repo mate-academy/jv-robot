95629b6f226f5a1b1a1a51a9ab4ec0d3a2dfbea2
Bohdan <111230201+FireFalcons@users.noreply.github.com>
2024-06-17 15:18:01 +0300

    Update RobotRoute.java

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
 public class RobotRoute {
-    public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+    public static void moveRobot (Robot robot, int toX, int toY) {
+        /* цей метод приймає Robot- екземпляр. Метод повинен переміщати робота з його поточного
+         положення до цільового, враховуючи його поточний напрямок і оновлюючи напрямок за потреби*/
+        // toX і toY - координати дому
+        while (robot.getX() != toX || robot.getY() != toY){
+            if (robot.getX() < toX){
+                while (robot.getDirection() != Direction.RIGHT){
+                    robot.turnRight();
+                }
+            } else if (robot.getX()>toX) {
+                while (robot.getDirection() != Direction.LEFT){
+                    robot.turnLeft();
+                }
+            } else if (robot.getY() < toY){
+                while (robot.getDirection() != Direction.UP){
+                    robot.turnRight();
+                }
+            } else {
+                while (robot.getDirection() != Direction.DOWN){
+                    robot.turnLeft();
+                }
+            }
+            robot.stepForward();
+        }
+    }
+
+    public static void main(String[] args) {
+        Robot robot = new Robot(0,0,Direction.RIGHT);
+        moveRobot(robot, 5,6);
+        System.out.println("Robot`s final position: (" + robot.getX() + ", " + robot.getY() + ")");
     }
 }
