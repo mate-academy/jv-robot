a41fd28f93a534673a79e488dc2807ff51ed78e0
rainett <96131229+rainett@users.noreply.github.com>
2024-08-03 14:48:29 +0300

    implemented moveRobot method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,52 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                faceLeft(robot);
+            } else {
+                faceRight(robot);
+            }
+            robot.stepForward();
+        }
+
+        while (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                faceDown(robot);
+            } else {
+                faceUp(robot);
+            }
+            robot.stepForward();
+        }
+    }
+
+    private void faceUp(Robot robot) {
+        faceLeft(robot);
+        robot.turnRight();
+    }
+
+    private void faceDown(Robot robot) {
+        faceLeft(robot);
+        robot.turnLeft();
+    }
+
+    private void faceRight(Robot robot) {
+        faceLeft(robot);
+        robot.turnLeft();
+        robot.turnLeft();
+    }
+
+    private void faceLeft(Robot robot) {
+        switch (robot.getDirection()) {
+            case UP -> robot.turnLeft();
+            case RIGHT -> {
+                robot.turnLeft();
+                robot.turnLeft();
+            }
+            case DOWN -> robot.turnRight();
+            default -> {
+
+            }
+        }
     }
 }
