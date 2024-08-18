0e3d07f7cf9470ec260dee73e3e4fb4fc9418a80
oksana-feshchenko <foksanv@gmail.com>
2024-08-18 12:38:41 +0300

    add RobotRoute implementation

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,22 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // handle x first
+        while (robot.getDirection() != (toX < robot.getX() ? Direction.LEFT : Direction.RIGHT)) {
+            robot.turnRight();
+        }
+
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+
+        // now y
+        while (robot.getDirection() != (toY < robot.getY() ? Direction.UP : Direction.DOWN)) {
+            robot.turnRight();
+        }
+
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
     }
 }
