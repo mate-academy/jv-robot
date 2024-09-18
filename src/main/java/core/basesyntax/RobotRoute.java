c5c6617e5bd90dd9b89f298dcc6a5466c521d873
Oleh Ridchenko <olehridchenko@gmail.com>
2024-09-18 12:22:33 +0300

    Implement the logic in the moveRobot method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
+    private void turnToTarget(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnLeft();
+        }
+    }
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+
+        while (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                turnToTarget(robot, Direction.LEFT);
+            } else {
+                turnToTarget(robot, Direction.RIGHT);
+            }
+            robot.stepForward();
+        }
+
+        while (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                turnToTarget(robot, Direction.DOWN);;
+            } else {
+                turnToTarget(robot, Direction.UP);;
+            }
+            robot.stepForward();
+        }
     }
 }
