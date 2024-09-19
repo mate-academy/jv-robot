b5137b8aec83692da217023d35f223b5fb5c6b5b
Pawel <capiukpawel@gmail.com>
2024-09-19 18:47:07 +0200

    fixes

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,34 +2,33 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-
-        moveInXDirection(robot, toX);
-        moveInYDirection(robot, toY);
-
-    }
-        private void moveInXDirection(Robot robot, int toX) {
-            while (robot.getX() != toX) {
-                if(robot.getX() < toX) {
-                    turnRobotDirection(robot, Direction.RIGHT);
-                } else {
-                    turnRobotDirection(robot, Direction.LEFT);
-                }
-                robot.stepForward();
-        }
-    }
-        private void moveInYDirection(Robot robot, int toY) {
-            while (robot.getY() != toY) {
-                if (robot.getY() < toY) {
-                    turnRobotDirection(robot, Direction.UP);
-                } else {
-                    turnRobotDirection(robot, Direction.DOWN);
-                }
-                robot.stepForward();
+        //moving robot based on X axis
+        while (robot.getX() != toX) {
+            if (robot.getX() < toX) {
+                turnRobot(robot, Direction.RIGHT);
+            } else if (robot.getX() > toX) {
+                turnRobot(robot, Direction.LEFT);
             }
+            robot.stepForward();
         }
-        private void turnRobotDirection (Robot robot, Direction targetDirection){
-            while (robot.getDirection() != targetDirection){
+        //moving robot based on Y axis
+        while (robot.getY() != toY) {
+            if (robot.getY() < toY) {
+                turnRobot(robot, Direction.UP);
+            } else if (robot.getY() > toY) {
+                turnRobot(robot, Direction.DOWN);
+            }
+            robot.stepForward();
+        }
+    }
+
+    public void turnRobot(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            if (targetDirection == Direction.RIGHT) {
                 robot.turnRight();
-                }
+            } else {
+                robot.turnLeft();
             }
         }
+    }
+}
