38999db813397058a8b8118c82fa8e27617d3e6c
kupnovytskyi <kupnovytskyinazar@gmail.com>
2024-09-19 16:37:26 +0300

    first solution

--- src/main/java/core/basesyntax/Robot.java
+++ src/main/java/core/basesyntax/Robot.java
@@ -12,6 +12,7 @@ public class Robot {
     }
 
     public Direction getDirection() {
+
         return direction;
     }
 
--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,67 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentTox = robot.getX();
+        int currentToy = robot.getY();
+        Direction currentDirection = robot.getDirection();
+        currentDirection = moveHorizontally(robot, toX, currentTox, currentDirection);
+        moveVertically(robot, toY, currentToy, currentDirection);
+    }
+
+    private Direction moveHorizontally(Robot robot, int toX, int currentTox, Direction direction) {
+        if (toX > currentTox) {
+            if (direction != Direction.RIGHT) {
+                while (direction != Direction.RIGHT) {
+                    robot.turnRight();
+                    direction = robot.getDirection();
+                }
+            }
+            while (toX > currentTox) {
+                robot.stepForward();
+                currentTox++;
+            }
+        } else if (toX < currentTox) {
+            if (direction != Direction.LEFT) {
+                while (direction != Direction.LEFT) {
+                    robot.turnLeft();
+                    direction = robot.getDirection();
+                }
+            }
+            while (toX < currentTox) {
+                robot.stepForward();
+                currentTox--;
+            }
+        }
+        return direction;
+    }
+
+    private void moveVertically(Robot robot, int toY, int currentToy, Direction currentDirection) {
+        if (toY > currentToy) {
+            if (currentDirection != Direction.UP) {
+                direction(robot, Direction.UP);
+            }
+            while (currentToy < toY) {
+                robot.stepForward();
+                currentToy++;
+            }
+        } else if (toY < currentToy) {
+            if (currentDirection != Direction.DOWN) {
+                direction(robot, Direction.DOWN);
+            }
+            while (currentToy > toY) {
+                robot.stepForward();
+                currentToy--;
+            }
+        }
+    }
+
+    private void direction(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            if (robot.getDirection() != direction) {
+                robot.turnRight();
+            } else {
+                robot.turnLeft();
+            }
+        }
     }
 }
