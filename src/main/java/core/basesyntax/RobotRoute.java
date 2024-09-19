cef1dc749ba7f3c3ebf8aa690b9b412cc0cc42a9
Pawel <capiukpawel@gmail.com>
2024-09-19 18:29:49 +0200

    First commit

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,34 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+
+        moveInXDirection(robot, toX);
+        moveInYDirection(robot, toY);
+
     }
-}
+        private void moveInXDirection(Robot robot, int toX) {
+            while (robot.getX() != toX) {
+                if(robot.getX() < toX) {
+                    turnRobotDirection(robot, Direction.RIGHT);
+                } else {
+                    turnRobotDirection(robot, Direction.LEFT);
+                }
+                robot.stepForward();
+        }
+    }
+        private void moveInYDirection(Robot robot, int toY) {
+            while (robot.getY() != toY) {
+                if (robot.getY() < toY) {
+                    turnRobotDirection(robot, Direction.UP);
+                } else {
+                    turnRobotDirection(robot, Direction.DOWN);
+                }
+                robot.stepForward();
+            }
+        }
+        private void turnRobotDirection (Robot robot, Direction targetDirection){
+            while (robot.getDirection() != targetDirection){
+                robot.turnRight();
+                }
+            }
+        }
