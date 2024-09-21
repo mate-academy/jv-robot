a94cf7c2813bbf6b0b34613fbf17895292b671d3
Rostyslav Novak <dbrayn306@gmail.com>
2024-09-21 13:37:23 +0300

    hot fix

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -7,26 +7,30 @@ public class RobotRoute {
             switch (robot.getDirection()) {
                 case UP:
                     if (toY > 0 || toY > robot.getY()) {
-                        while (toY > robot.getY())
+                        while (toY > robot.getY()) {
                             robot.stepForward();
+                        }
                     }
                     robot.turnRight();
                 case RIGHT:
                     if (toX > 0 || toX > robot.getX()) {
-                        while (toX > robot.getX())
+                        while (toX > robot.getX()) {
                             robot.stepForward();
+                        }
                     }
                     robot.turnRight();
                 case DOWN:
                     if (toY < 0 || toY < robot.getY()) {
-                        while (toY < robot.getY())
+                        while (toY < robot.getY()) {
                             robot.stepForward();
+                        }
                     }
                     robot.turnRight();
                 case LEFT:
                     if (toX < 0 || toX < robot.getX()) {
-                        while (toX < robot.getX())
+                        while (toX < robot.getX()) {
                             robot.stepForward();
+                        }
                     }
                     robot.turnRight();
             }
