2adb99287beb6d8f59d990069844a7eb1ca837f5
Yuliia Kudrynska <ykudrynska@gmail.com>
2024-09-26 22:31:56 +0300

    Implementing robot moving functionality

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,240 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        switch (robot.getDirection()) {
+            case UP:
+                moveIfDirectionUP(robot, toX, toY);
+                break;
+            case DOWN:
+                moveIfDirectionDown(robot, toX, toY);
+                break;
+            case LEFT:
+                moveIfDirectionLeft(robot, toX, toY);
+                break;
+            case RIGHT:
+                moveIfDirectionRight(robot, toX, toY);
+                break;
+        }
+    }
+    public void moveIfDirectionUP(Robot robot, int toX, int toY) {
+        if (robot.getX() == toX && robot.getY() < toY) {
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() == toX && robot.getY() > toY) {
+            robot.turnRight();
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() == toY) {
+            robot.turnRight();
+            goFwdX(robot, toX);
+        }
+
+        if (robot.getX() > toX && robot.getY() == toY) {
+            robot.turnLeft();
+            goBackX(robot, toX);
+        }
+
+        if (robot.getX() < toX && robot.getY() < toY) {
+            robot.turnRight();
+            goFwdX(robot, toX);
+            robot.turnLeft();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() > toY) {
+            robot.turnLeft();
+            goBackX(robot, toX);
+            robot.turnLeft();
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() < toY) {
+            robot.turnLeft();
+            goBackX(robot, toX);
+            robot.turnRight();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() > toY) {
+            robot.turnRight();
+            goFwdX(robot, toX);
+            robot.turnRight();
+            goBackY(robot, toY);
+        }
+    }
+
+    public void moveIfDirectionDown(Robot robot, int toX, int toY) {
+        if (robot.getX() == toX && robot.getY() < toY) {
+            robot.turnRight();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() == toX && robot.getY() > toY) {
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() == toY) {
+            robot.turnLeft();
+            goFwdX(robot, toX);
+        }
+
+        if (robot.getX() > toX && robot.getY() == toY) {
+            robot.turnRight();
+            goBackX(robot, toX);
+        }
+
+        if (robot.getX() < toX && robot.getY() < toY) {
+            robot.turnLeft();
+            goFwdX(robot, toX);
+            robot.turnLeft();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() > toY) {
+            robot.turnRight();
+            goBackX(robot, toX);
+            robot.turnLeft();
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() < toY) {
+            robot.turnRight();
+            goBackX(robot, toX);
+            robot.turnRight();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() > toY) {
+            robot.turnLeft();
+            goFwdX(robot, toX);
+            robot.turnRight();
+            goBackY(robot, toY);
+        }
+    }
+
+    public void moveIfDirectionLeft(Robot robot, int toX, int toY) {
+        if (robot.getX() == toX && robot.getY() < toY) {
+            robot.turnRight();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() == toX && robot.getY() > toY) {
+            robot.turnLeft();
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() == toY) {
+            robot.turnRight();
+            robot.turnRight();
+            goFwdX(robot, toX);
+        }
+
+        if (robot.getX() > toX && robot.getY() == toY) {
+            goBackX(robot, toX);
+        }
+
+        if (robot.getX() < toX && robot.getY() < toY) {
+            robot.turnRight();
+            robot.turnRight();
+            goFwdX(robot, toX);
+            robot.turnLeft();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() > toY) {
+            goBackX(robot, toX);
+            robot.turnLeft();
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() < toY) {
+            goBackX(robot, toX);
+            robot.turnRight();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() > toY) {
+            robot.turnLeft();
+            robot.turnLeft();
+            goFwdX(robot, toX);
+            robot.turnRight();
+            goBackY(robot, toY);
+        }
+    }
+
+    public void moveIfDirectionRight(Robot robot, int toX, int toY) {
+        if (robot.getX() == toX && robot.getY() < toY) {
+            robot.turnLeft();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() == toX && robot.getY() > toY) {
+            robot.turnRight();
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() == toY) {
+            goFwdX(robot, toX);
+        }
+
+        if (robot.getX() > toX && robot.getY() == toY) {
+            robot.turnLeft();
+            robot.turnLeft();
+            goBackX(robot, toX);
+        }
+
+        if (robot.getX() < toX && robot.getY() < toY) {
+            goFwdX(robot, toX);
+            robot.turnLeft();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() > toY) {
+            robot.turnLeft();
+            robot.turnLeft();
+            goBackX(robot, toX);
+            robot.turnLeft();
+            goBackY(robot, toY);
+        }
+
+        if (robot.getX() > toX && robot.getY() < toY) {
+            robot.turnLeft();
+            robot.turnLeft();
+            goBackX(robot, toX);
+            robot.turnRight();
+            goFwdY(robot, toY);
+        }
+
+        if (robot.getX() < toX && robot.getY() > toY) {
+            goFwdX(robot, toX);
+            robot.turnRight();
+            goBackY(robot, toY);
+        }
+    }
+
+    private void goBackX(Robot robot, int toX) {
+        for (int i = robot.getX(); i > toX; i--) {
+            robot.stepForward();
+        }
+    }
+
+    private void goFwdX(Robot robot, int toX) {
+        for (int i = robot.getX(); i < toX; i++) {
+            robot.stepForward();
+        }
+    }
+
+    private void goBackY(Robot robot, int toY) {
+        for (int j = robot.getY(); j > toY; j--) {
+            robot.stepForward();
+        }
+    }
+
+    private void goFwdY(Robot robot, int toY) {
+        for (int j = robot.getY(); j < toY; j++) {
+            robot.stepForward();
+        }
     }
 }
