26449821cb016477fd7b34c4862b04f8edbcd51e
Mihail <mishadyakivnich@gmail.com>
2024-10-31 19:21:01 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,19 +2,19 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      Direction dirX = (robot.getX() - toX > 0 ? Direction.RIGHT : Direction.LEFT);
-      Direction dirY = (robot.getY() - toY > 0 ? Direction.UP : Direction.DOWN);
-      while (robot.getDirection() != dirX) {
-          robot.turnRight();
-      }
-      while (robot.getDirection() != dirY) {
-          robot.turnLeft();
-      }
-      while (robot.getX() != toX) {
-          robot.stepForward();
-      }
-      while (robot.getY() != toY) {
-          robot.stepForward();
-      }
+        Direction dirX = (robot.getX() - toX > 0 ? Direction.RIGHT : Direction.LEFT);
+        Direction dirY = (robot.getY() - toY > 0 ? Direction.UP : Direction.DOWN);
+        while (robot.getDirection() != dirX) {
+            robot.turnRight();
+        }
+        while (robot.getDirection() != dirY) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
