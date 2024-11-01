5e2f920cc0c4ca54174773ee360f195da857a7ba
Mihail <mishadyakivnich@gmail.com>
2024-11-01 14:47:53 +0200

    Merge remote-tracking branch 'origin/hw-solution' into hw-solution
    # Conflicts:
    #	src/main/java/core/basesyntax/RobotRoute.java

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,19 +2,19 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      Direction dirX = (robot.getX() - toX > 0) ? Direction.RIGHT : Direction.LEFT;
-      Direction dirY = (robot.getY() - toY > 0) ? Direction.UP : Direction.DOWN;
+        Direction dirX = (robot.getX() - toX > 0) ? Direction.RIGHT : Direction.LEFT;
+        Direction dirY = (robot.getY() - toY > 0) ? Direction.UP : Direction.DOWN;
         while (robot.getDirection() != dirX) {
-          robot.turnRight();
+            robot.turnRight();
         }
         while (robot.getX() != toX) {
-          robot.stepForward();
+            robot.stepForward();
         }
         while (robot.getDirection() != dirY) {
             robot.turnLeft();
         }
         while (robot.getY() != toY) {
-          robot.stepForward();
+            robot.stepForward();
         }
     }
 }
