de9aa2bfc58cf500265a8ad7b74bab95556a6239
FroGitHub <andrey8102310@gmail.com>
2024-12-26 17:44:56 +0200

    add RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,31 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
+    private static void rotateRobotTo(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnLeft();
+        }
+    }
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here -3 -2 -1 0 1 2 3
+        if (robot.getX() < toX) {
+            rotateRobotTo(robot, Direction.RIGHT);
+        } else {
+            rotateRobotTo(robot, Direction.LEFT);
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        if (robot.getY() < toY) {
+            rotateRobotTo(robot, Direction.UP);
+        } else {
+            rotateRobotTo(robot, Direction.DOWN);
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
