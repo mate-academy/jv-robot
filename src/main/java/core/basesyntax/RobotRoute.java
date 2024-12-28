f5ab5f046719c4d8c7fc835dc10e40eaa7b2b2a6
ivanlavrinenko <lavrinenkoivan51@gmail.com>
2024-12-28 15:40:16 +0100

    imtendation levels corrected

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,22 +2,22 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      int distanceX = toX - robot.getX();
-      int distanceY = toY - robot.getY();
+        int distanceX = toX - robot.getX();
+        int distanceY = toY - robot.getY();
 
-      switch (robot.getDirection()) {
-          case UP:
-              robot.turnRight();
-              break;
-          case DOWN:
-              robot.turnLeft();
-              break;
-          case LEFT:
-              robot.turnRight();
-              robot.turnRight();
-              break;
-          case RIGHT:
-              break;
+        switch (robot.getDirection()) {
+            case UP:
+                robot.turnRight();
+                break;
+            case DOWN:
+                robot.turnLeft();
+                break;
+            case LEFT:
+                robot.turnRight();
+                robot.turnRight();
+                break;
+            case RIGHT:
+                break;
       }
 
       if (distanceX < 0) {
