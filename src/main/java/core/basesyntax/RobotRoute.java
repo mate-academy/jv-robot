53b4328074f6199597a41da7b3b6e9e99c9e4e5d
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-01-16 19:10:16 +0200

    initisl commit -2

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -9,35 +9,29 @@ public class RobotRoute {
             int deltaX = toX - currentX;
             int deltaY = toY - currentY;
 
-            // Determine which way to turn and move
             if (deltaX > 0) {
-                // Robot needs to move right
                 if (robot.getDirection() != Direction.RIGHT) {
                     robot.turnRight();
                 }
             } else if (deltaX < 0) {
-                // Robot needs to move left
+
                 if (robot.getDirection() != Direction.LEFT) {
                     robot.turnLeft();
                 }
             }
 
             if (deltaY > 0) {
-                // Robot needs to move up
                 if (robot.getDirection() != Direction.UP) {
                     robot.turnRight();
                 }
             } else if (deltaY < 0) {
-                // Robot needs to move down
                 if (robot.getDirection() != Direction.DOWN) {
                     robot.turnLeft();
                 }
             }
 
-            // Move the robot one step forward
             robot.stepForward();
 
-            // Update current coordinates
             currentX = robot.getX();
             currentY = robot.getY();
         }
