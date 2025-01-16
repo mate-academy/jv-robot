60bdb3d12b52c1e6f0d9b761dc1c6ef47077f55c
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-01-16 19:08:11 +0200

    initial commit

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,44 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+
+        while (currentX != toX || currentY != toY) {
+            int deltaX = toX - currentX;
+            int deltaY = toY - currentY;
+
+            // Determine which way to turn and move
+            if (deltaX > 0) {
+                // Robot needs to move right
+                if (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+            } else if (deltaX < 0) {
+                // Robot needs to move left
+                if (robot.getDirection() != Direction.LEFT) {
+                    robot.turnLeft();
+                }
+            }
+
+            if (deltaY > 0) {
+                // Robot needs to move up
+                if (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+            } else if (deltaY < 0) {
+                // Robot needs to move down
+                if (robot.getDirection() != Direction.DOWN) {
+                    robot.turnLeft();
+                }
+            }
+
+            // Move the robot one step forward
+            robot.stepForward();
+
+            // Update current coordinates
+            currentX = robot.getX();
+            currentY = robot.getY();
+        }
     }
 }
