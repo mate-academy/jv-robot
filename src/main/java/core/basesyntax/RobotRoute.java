b9df06e973c99b3bd8fb585470ebe6a686badbaf
HSlawinski <hubi.slawinski@gmail.com>
2025-01-31 22:06:45 +0100

    RobotRoute implement fix

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -5,19 +5,27 @@ public class RobotRoute {
         //PORUSZANIE SIE PO OSI X
         if (robot.getX() < toX) {
            robot.turnRight();
-           while (robot.getDirection() == Direction.RIGHT) robot.stepForward();
+           while (robot.getDirection() == Direction.RIGHT) {
+               robot.stepForward();
+           }
         }
-        if (robot.getX() > toX){
+        if (robot.getX() > toX) {
             robot.turnLeft();
-            while (robot.getDirection() == Direction.LEFT) robot.stepForward();
+            while (robot.getDirection() == Direction.LEFT) {
+                robot.stepForward();
+            }
         }
         if (robot.getY() < toY) {
             robot.turnRight();
-            while (robot.getDirection() == Direction.UP) robot.stepForward();
+            while (robot.getDirection() == Direction.UP) {
+                robot.stepForward();
+            }
         }
         if (robot.getY() > toY) {
             robot.turnRight();
-            while (robot.getDirection() == Direction.DOWN) robot.stepForward();
+            while (robot.getDirection() == Direction.DOWN) {
+                robot.stepForward();
+            }
         }
         if (robot.getY() == toY && robot.getX() == toX) {
             System.out.println("You are in right place");
