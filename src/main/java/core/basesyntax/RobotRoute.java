9973c76556a9ba3b3bda7aaba1441a97db8ee641
HSlawinski <hubi.slawinski@gmail.com>
2025-01-31 22:01:09 +0100

    RobotRoute implement

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,25 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //PORUSZANIE SIE PO OSI X
+        if (robot.getX() < toX) {
+           robot.turnRight();
+           while (robot.getDirection() == Direction.RIGHT) robot.stepForward();
+        }
+        if (robot.getX() > toX){
+            robot.turnLeft();
+            while (robot.getDirection() == Direction.LEFT) robot.stepForward();
+        }
+        if (robot.getY() < toY) {
+            robot.turnRight();
+            while (robot.getDirection() == Direction.UP) robot.stepForward();
+        }
+        if (robot.getY() > toY) {
+            robot.turnRight();
+            while (robot.getDirection() == Direction.DOWN) robot.stepForward();
+        }
+        if (robot.getY() == toY && robot.getX() == toX) {
+            System.out.println("You are in right place");
+        }
     }
 }
