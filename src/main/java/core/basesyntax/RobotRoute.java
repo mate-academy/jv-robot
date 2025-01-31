5ea04b03b20ca358e44a37b6bc63adc182c4eb38
HSlawinski <hubi.slawinski@gmail.com>
2025-01-31 22:16:01 +0100

    RobotRoute implement fix

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,33 +2,35 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        //PORUSZANIE SIE PO OSI X
         if (robot.getX() < toX) {
-           robot.turnRight();
-           while (robot.getDirection() == Direction.RIGHT) {
-               robot.stepForward();
-           }
-        }
-        if (robot.getX() > toX) {
-            robot.turnLeft();
-            while (robot.getDirection() == Direction.LEFT) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (robot.getX() < toX) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+            while (robot.getX() > toX) {
                 robot.stepForward();
             }
         }
         if (robot.getY() < toY) {
-            robot.turnRight();
-            while (robot.getDirection() == Direction.UP) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            while (robot.getY() < toY) {
                 robot.stepForward();
             }
-        }
-        if (robot.getY() > toY) {
-            robot.turnRight();
-            while (robot.getDirection() == Direction.DOWN) {
+        } else if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+            while (robot.getY() > toY) {
                 robot.stepForward();
             }
-        }
-        if (robot.getY() == toY && robot.getX() == toX) {
-            System.out.println("You are in right place");
         }
     }
 }
