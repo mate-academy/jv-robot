7017056a5f12ad8946706dd7934f82d7239e8428
Mateusz <mateusz.kiryk@o2.pl>
2025-02-13 10:49:18 +0100

    Third solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
-public class main {
-    public static void Main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         Robot robot = new Robot(Direction.RIGHT, 0, 0);
         RobotRoute robotRoute = new RobotRoute();
         robotRoute.moveRobot(robot, 3, 2);
