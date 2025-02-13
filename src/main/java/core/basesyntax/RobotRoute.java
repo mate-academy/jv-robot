52cd9dac8a44a9e35d929795070c76df5f26522a
Mateusz <mateusz.kiryk@o2.pl>
2025-02-13 10:46:05 +0100

    Second solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -5,20 +5,16 @@ public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
         while (robot.getX() != toX) {
             if (robot.getX() < toX) {
-
                 while (robot.getDirection() != Direction.RIGHT) {
                     robot.turnRight();
                 }
             } else {
-
                 while (robot.getDirection() != Direction.LEFT) {
                     robot.turnLeft();
                 }
             }
             robot.stepForward();
         }
-
-
         while (robot.getY() != toY) {
             if (robot.getY() < toY) {
 
--- src/main/java/core/basesyntax/main.java
+++ src/main/java/core/basesyntax/main.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class main {
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         Robot robot = new Robot(Direction.RIGHT, 0, 0);
         RobotRoute robotRoute = new RobotRoute();
         robotRoute.moveRobot(robot, 3, 2);
