18f9f9813aa4ee568917709f6c27d92b3ef8e5cb
Roman Zaharii <romazagariy2003@gmail.com>
2025-02-23 12:50:02 +0200

    fixed Checkstyle issues

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -7,47 +7,36 @@ public class RobotRoute {
         if (differenceX >= 0 && differenceY >= 0) {
             if (robot.getDirection() == Direction.UP) {
                 robot.turnRight();
-            }
-            else if (robot.getDirection() == Direction.DOWN) {
+            } else if (robot.getDirection() == Direction.DOWN) {
                 robot.turnLeft();
-            }
-            else if (robot.getDirection() == Direction.RIGHT) {
+            } else if (robot.getDirection() == Direction.RIGHT) {
                 robot.turnRight();
                 robot.turnRight();
             }
-        }
-        else if (differenceX <= 0 && differenceY <= 0) {
+        } else if (differenceX <= 0 && differenceY <= 0) {
             if (robot.getDirection() == Direction.UP) {
                 robot.turnLeft();
-            }
-            else if (robot.getDirection() == Direction.DOWN) {
+            } else if (robot.getDirection() == Direction.DOWN) {
                 robot.turnRight();
-            }
-            else if (robot.getDirection() == Direction.RIGHT) {
+            } else if (robot.getDirection() == Direction.RIGHT) {
                 robot.turnRight();
                 robot.turnRight();
             }
-        }
-        else if (differenceX >= 0 && differenceY <= 0) {
+        } else if (differenceX >= 0 && differenceY <= 0) {
             if (robot.getDirection() == Direction.UP) {
                 robot.turnRight();
-            }
-            else if (robot.getDirection() == Direction.DOWN) {
+            } else if (robot.getDirection() == Direction.DOWN) {
                 robot.turnLeft();
-            }
-            else if (robot.getDirection() == Direction.LEFT) {
+            } else if (robot.getDirection() == Direction.LEFT) {
                 robot.turnRight();
                 robot.turnRight();
             }
-        }
-        else if (differenceX <= 0 && differenceY >= 0) {
+        } else if (differenceX <= 0 && differenceY >= 0) {
             if (robot.getDirection() == Direction.UP) {
                 robot.turnLeft();
-            }
-            else if (robot.getDirection() == Direction.DOWN) {
+            } else if (robot.getDirection() == Direction.DOWN) {
                 robot.turnRight();
-            }
-            else if (robot.getDirection() == Direction.RIGHT) {
+            } else if (robot.getDirection() == Direction.RIGHT) {
                 robot.turnRight();
                 robot.turnRight();
             }
@@ -55,11 +44,10 @@ public class RobotRoute {
         while (robot.getX() != toX) {
             robot.stepForward();
         }
-        if ((robot.getDirection() == Direction.LEFT && differenceY >= 0) ||
-                (robot.getDirection() == Direction.RIGHT && differenceY <= 0 )) {
+        if ((robot.getDirection() == Direction.LEFT && differenceY >= 0)
+                || (robot.getDirection() == Direction.RIGHT && differenceY <= 0)) {
             robot.turnRight();
-        }
-        else {
+        } else {
             robot.turnLeft();
         }
         while (robot.getY() != toY) {
