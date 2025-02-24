3bbb8a2bc12387389cd639f6b14bf9d216e4876d
Vershynin Kostia <bigvini@gmail.com>
2025-02-24 12:26:51 +0200

    implement method RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,39 +2,33 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-        int x = robot.getX();
-        int y = robot.getY();
 
-        if (x < toX) {
-            while (robot.getDirection() == Direction.RIGHT) {
+        if (robot.getX() < toX) {
+            while (robot.getDirection() != Direction.RIGHT) {
                 robot.turnRight();
             }
-            while (robot.getX() == toX) {
-                robot.stepForward();
-            }
-        } else if (x > toX) {
-            while (robot.getDirection() == Direction.LEFT) {
+        } else if (robot.getX() > toX) {
+            while (robot.getDirection() != Direction.LEFT) {
                 robot.turnRight();
-            }
-            while (robot.getX() == toX) {
-                robot.stepForward();
             }
         }
 
-        if (y < toY) {
-            while (robot.getDirection() == Direction.UP) {
+        while (robot.getX() == toX) {
+            robot.stepForward();
+        }
+
+        if (robot.getY() < toY) {
+            while (robot.getDirection() != Direction.UP) {
                 robot.turnRight();
             }
-            while (robot.getY() == toY) {
-                robot.stepForward();
-            }
-        } else if (y > toY) {
+        } else if (robot.getY() > toY) {
             while (robot.getDirection() == Direction.DOWN) {
                 robot.turnRight();
             }
-            while (robot.getY() == toY) {
-                robot.stepForward();
-            }
+        }
+
+        while (robot.getY() != toY) {
+            robot.stepForward();
         }
     }
 }
