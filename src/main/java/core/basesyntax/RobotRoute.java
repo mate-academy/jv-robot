7627ac21023bc20a4cd6c393e67531465775880e
Denis Basarab <denya2007basarab@gmail.com>
2025-02-26 17:54:34 +0200

    added correct cod to method moveRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,20 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        String leftOrRight = (robot.getX() > toX) ? Direction.LEFT.name() : Direction.RIGHT.name();
+        while (leftOrRight != robot.getDirection().name()) {
+            robot.turnLeft();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        String upOrDown = (robot.getY() > toY) ? Direction.DOWN.name() : Direction.UP.name();
+        while (upOrDown != robot.getDirection().name()) {
+            robot.turnLeft();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
