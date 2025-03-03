b0162e165d2f59f1edb9b5a52184f8941c86fbec
Vlad Loran <vladloran17@gmail.com>
2025-03-03 06:43:12 +0000

    Finished task and checkstyle

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,87 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        if (robot.getX() > toX) {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                case UP:
+                    robot.turnLeft();
+                    break;
+                case DOWN:
+                    robot.turnRight();
+                    break;
+                default:
+                    break;
+            }
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+        } else {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    break;
+                case UP:
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+            while (robot.getX() < toX) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getY() < toY) {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    robot.turnRight();
+                    break;
+                case RIGHT:
+                    robot.turnLeft();
+                    break;
+                case UP:
+                    break;
+                case DOWN:
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    break;
+                default:
+                    break;
+            }
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        } else {
+            switch (robot.getDirection()) {
+                case LEFT:
+                    robot.turnLeft();
+                    break;
+                case RIGHT:
+                    robot.turnRight();
+                    break;
+                case UP:
+                    robot.turnRight();
+                    robot.turnRight();
+                    break;
+                case DOWN:
+                    break;
+                default:
+                    break;
+            }
+            while (robot.getY() > toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
