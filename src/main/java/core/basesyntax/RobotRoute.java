e433402b4ef9ca688845c8edb0756bb21363b9cf
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-03-09 22:32:33 +0200

    implemented MoveRobot method logic

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,29 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int x = robot.getX();
+        int y = robot.getY();
+        if (toX != x) {
+            Direction targetX = (toX > x) ? Direction.RIGHT : Direction.LEFT;
+            changeRobotDirection(robot, targetX);
+            while (toX != x) {
+                robot.stepForward();
+                x = robot.getX();
+            }
+        }
+        if (toY != y) {
+            Direction targetY = (toY > y) ? Direction.UP : Direction.DOWN;
+            changeRobotDirection(robot, targetY);
+            while (toY != y) {
+                robot.stepForward();
+                y = robot.getY();
+            }
+        }
+    }
+
+    public static void changeRobotDirection(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            robot.turnRight();
+        }
     }
 }
