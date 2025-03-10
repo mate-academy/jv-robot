55615de669ea13562b60de9cba04162dc0528e3c
BoichukMaryna <marisha.boychuk97@gmail.com>
2025-03-10 10:50:04 +0000

    added RobotTest class

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,26 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        while (robot.getX() != toX || robot.getY() != toY) {
+            if (robot.getX() < toX) {
+                setDirection(robot, Direction.RIGHT);
+                robot.stepForward();
+            } else if (robot.getX() > toX) {
+                setDirection(robot, Direction.LEFT);
+                robot.stepForward();
+            } else if (robot.getY() < toY) {
+                setDirection(robot, Direction.UP);
+                robot.stepForward();
+            } else if (robot.getY() > toY) {
+                setDirection(robot, Direction.DOWN);
+                robot.stepForward();
+            }
+        }
+    }
+
+    private void setDirection(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            robot.turnRight();
+        }
     }
 }
