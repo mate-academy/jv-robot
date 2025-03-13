82bcdeaceb9fa929c4f17da22aca202a0b32d5c1
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-03-13 21:56:02 +0100

    fixed

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax;
-
-public class Main {
-    public static void main(String[] args) {
-        Robot robot = new Robot(Direction.RIGHT, 5,5);
-        robot.moveRobot(robot, 7, 7);
-    }
-}
