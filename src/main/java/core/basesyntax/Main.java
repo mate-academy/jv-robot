3be7c697e26fda8186c5cf246ae2c31d73754c06
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-03-13 21:48:04 +0100

    Solved task

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,8 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        Robot robot = new Robot(Direction.RIGHT, 5,5);
+        robot.moveRobot(robot, 7, 7);
+    }
+}
--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,48 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
-    }
+
+      if(robot.getX() < toX) {
+          while (robot.getDirection() != Direction.RIGHT) {
+              robot.turnRight();
+          }
+          while (robot.getX()!= toX) {
+              robot.stepForward();
+          }
+      } else if (robot.getX() > toX) {
+          while (robot.getDirection() != Direction.LEFT) {
+              robot.turnLeft();
+          }
+          while (robot.getX() != toX) {
+              robot.stepForward();
+          }
+
+      } else {
+              System.out.println("Robot is on the coordinate X");
+
+      }
+
+        if(robot.getY() < toY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        } else if (robot.getY() > toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+
+        } else {
+            System.out.println("Robot is on the coordinate Y");
+
+        }
+
+
+
+}
 }
