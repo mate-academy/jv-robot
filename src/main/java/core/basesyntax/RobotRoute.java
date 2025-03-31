38fde9b75f83489237c667b38771b046da139a98
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-03-31 22:11:56 +0200

    Implement the logic in the moveRobot method of the RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,75 @@
 package core.basesyntax;
 
+
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        switch (robot.getDirection()) {
+            case UP:
+                if (toY > robot.getY()) {
+                    while (robot.getY() < toY) {
+                        robot.stepForward();
+                    }
+                } else if (toY < robot.getY()) {
+
+                    robot.turnRight();
+                    robot.turnRight();
+                    while (robot.getY() > toY) {
+                        robot.stepForward();
+                    }
+                }
+                break;
+                
+            case DOWN:
+                if (toY < robot.getY()) {
+                    while (robot.getY() > toY) {
+                        robot.stepForward();
+                    }
+                } else if (toY > robot.getY()) {
+
+                    robot.turnRight();
+                    robot.turnRight();
+                    while (robot.getY() < toY) {
+                        robot.stepForward();
+                    }
+                }
+                break;
+
+            case LEFT:
+                if (toX < robot.getX()) {
+                    while (robot.getX() > toX) {
+                        robot.stepForward();
+                    }
+                } else if (toX > robot.getX()) {
+
+                    robot.turnRight();
+                    while (robot.getX() < toX) {
+                        robot.stepForward();
+                    }
+
+
+                }
+                break;
+
+            case RIGHT:
+                if (toX > robot.getX()) {
+
+                    while (robot.getX() < toX) {
+                        robot.stepForward();
+                    }
+                } else if (toX < robot.getX()) {
+
+                    robot.turnLeft();
+                    robot.turnLeft();
+                    while (robot.getX() > toX) {
+                        robot.stepForward();
+                    }
+                }
+                break;
+
+        }
+
+        
+
     }
 }
