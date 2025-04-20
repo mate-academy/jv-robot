54312d63f94da9e5ac9166b6dff3d1acce5e38d8
Vladislav Balitskiy <vladislav.balitskiy20.01@gmail.com>
2025-04-20 14:47:43 +0200

    get needed direction

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,37 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+
+        if (toX > currentX) {
+            rotateTo(robot, Direction.RIGHT);
+            while (robot.getX() < toX) {
+                robot.stepForward();
+            }
+        } else if (toX < currentX) {
+            rotateTo(robot, Direction.LEFT);
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+        }
+
+        if (toY > currentY) {
+            rotateTo(robot, Direction.UP);
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        } else if (toY < currentY) {
+            rotateTo(robot, Direction.DOWN);
+            while (robot.getY() > toY) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    private void rotateTo(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            robot.turnRight();
+        }
     }
 }
