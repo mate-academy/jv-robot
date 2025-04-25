a2338132bd9b6c47afcb371dd1d866b324eac21a
Anton Stiazhkin <rudana089@gmail.com>
2025-04-25 20:31:40 +0300

    realize moveRobot() method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,27 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int deltaX = toX - robot.getX();
+        int deltaY = toY - robot.getY();
+
+        if (deltaX != 0) {
+            Direction desiredXDir = deltaX > 0 ? Direction.RIGHT : Direction.LEFT;
+            while (robot.getDirection() != desiredXDir) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < Math.abs(deltaX); i++) {
+                robot.stepForward();
+            }
+        }
+
+        if (deltaY != 0) {
+            Direction desiredYDir = deltaY > 0 ? Direction.UP : Direction.DOWN;
+            while (robot.getDirection() != desiredYDir) {
+                robot.turnRight();
+            }
+            for (int i = 0; i < Math.abs(deltaY); i++) {
+                robot.stepForward();
+            }
+        }
     }
 }
