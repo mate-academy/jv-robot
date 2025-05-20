7d80d981ac300daa930780bdedc099ebb85391e4
Szymon Twardowski <szytwardowski@gmail.com>
2025-05-20 13:44:20 +0200

    robot2

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -7,36 +7,34 @@ public class RobotRoute {
         int currentY = robot.getY();
 
         if (toX > currentX) {
-        rotateTo(robot, Direction.RIGHT);
-
-        for (int i = 0; i < toX-currentX; i++) {
-            robot.stepForward();
-        }
-    } else if (toX < currentX) {
+            rotateTo(robot, Direction.RIGHT);
+            for (int i = 0; i < toX - currentX; i++) {
+                robot.stepForward();
+            }
+        } else if (toX < currentX) {
             rotateTo(robot, Direction.LEFT);
-
             for (int i = 0; i < currentX - toX; i++) {
                 robot.stepForward();
             }
         }
+
         if (toY > currentY) {
             rotateTo(robot, Direction.UP);
             for (int i = 0; i < toY - currentY; i++) {
                 robot.stepForward();
             }
         } else if (toY < currentY) {
-            rotateTo(robot,Direction.DOWN);
+            rotateTo(robot, Direction.DOWN);
             for (int i = 0; i < currentY - toY; i++) {
                 robot.stepForward();
             }
-
         }
     }
+
     private void rotateTo(Robot robot, Direction targetDirection) {
         while (robot.getDirection() != targetDirection) {
             robot.turnRight();
-
         }
     }
-
 }
+git
